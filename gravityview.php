7b912b6fc562696a303ff58a06430f699c5c82a4
Luis Godinho <luis@gomo.pt>
2014-05-09 00:10:38 +0100

    Fix #79 less default widget active areas but with hook!

--- gravityview.php
+++ gravityview.php
@@ -318,11 +318,13 @@ final class GravityView_Plugin {
 	 * @return array definition for default widget areas
 	 */
 	public static function get_default_widget_areas() {
-		return array(
-			array( '1-1' => array( array( 'areaid' => 'top', 'title' => __('Full Width Top', 'gravity-view' ) , 'subtitle' => '' ) ) ),
+		$default_areas = array(
+			//array( '1-1' => array( array( 'areaid' => 'top', 'title' => __('Full Width Top', 'gravity-view' ) , 'subtitle' => '' ) ) ),
 			array( '1-2' => array( array( 'areaid' => 'left', 'title' => __('Left', 'gravity-view') , 'subtitle' => '' ) ), '2-2' => array( array( 'areaid' => 'right', 'title' => __('Right', 'gravity-view') , 'subtitle' => '' ) ) ),
-			array( '1-1' => array( 	array( 'areaid' => 'bottom', 'title' => __('Full Width Bottom', 'gravity-view') , 'subtitle' => '' ) ) )
+			//array( '1-1' => array( 	array( 'areaid' => 'bottom', 'title' => __('Full Width Bottom', 'gravity-view') , 'subtitle' => '' ) ) )
 		);
+
+		return apply_filters( 'gravityview_widget_active_areas', $default_areas );
 	}
 
 	/**
