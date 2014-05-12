85a7f45c476b3113b3240d193e1c9f17a498650a
Zack Katz <zack@katzwebservices.com>
2014-05-12 11:10:32 -0600

    Convert paragraphs to divs, move image to be ready to float

--- templates/list-body.php
+++ templates/list-body.php
@@ -22,32 +22,47 @@ foreach( $this->entries as $entry ) :
 			<?php if( !empty(  $this->fields['directory_list-title'] ) ):
 				$i = 0;
 				foreach( $this->fields['directory_list-title'] as $field ) :
-
 					if( $i == 0 ): ?>
-						<h3 class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></h3>
+						<h3 class="<?php echo gv_class( $field ); ?>"><?php echo gv_value( $entry, $field ); ?></h3>
 					<?php else: ?>
-						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
+						<div class="<?php echo gv_class( $field ); ?>"><?php echo wpautop(gv_value( $entry, $field )); ?></div>
 					<?php endif;
 					$i++; ?>
 				<?php endforeach; ?>
-			<?php endif; ?>
+			<?php endif;
 
+			if( !empty(  $this->fields['directory_list-subtitle'] ) ):
+
+			?>
+			<div class="gv-list-view-subtitle"> <?php
+				foreach( $this->fields['directory_list-subtitle'] as $field ) :
+			?>
+				<h4 class="<?php echo gv_class( $field ); ?>"><?php echo gv_value( $entry, $field ); ?></h4>
+			<?php endforeach; ?>
+			</div>
+			<?php endif; ?>
 		</div>
 
 		<div class="gv-list-view-content">
-
-			<div class="gv-list-view-content-description">
-				<?php if( !empty(  $this->fields['directory_list-description'] ) ):
-					foreach( $this->fields['directory_list-description'] as $field ) : ?>
-						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
-					<?php endforeach; ?>
-				<?php endif; ?>
-			</div>
 
 			<div class="gv-list-view-content-image">
 				<?php if( !empty(  $this->fields['directory_list-image'] ) ):
 					foreach( $this->fields['directory_list-image'] as $field ) : ?>
 						<?php echo gv_value( $entry, $field ); ?>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			</div>
+
+			<div class="gv-list-view-content-description">
+				<?php if( !empty(  $this->fields['directory_list-description'] ) ):
+					foreach( $this->fields['directory_list-description'] as $field ) : ?>
+						<div class="<?php echo gv_class( $field ); ?>"><?php
+
+							if($label = gv_label( $field )) { echo '<h4>'.esc_html( $label ).'</h4>'; }
+
+							echo wpautop(gv_value( $entry, $field ));
+						?>
+						</div>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</div>
