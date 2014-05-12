ceb3fd974719b5e7f5af7d0713eb4a23a9be5387
Zack Katz <zack@katzwebservices.com>
2014-05-12 13:13:49 -0600

    Hide footer if no footer fields
    Also update indenting for easier visuals

--- templates/list-single.php
+++ templates/list-single.php
@@ -3,23 +3,23 @@
 
 	<?php foreach( $this->entries as $entry ) : ?>
 
-		<div id="gv_list_<?php echo $entry['id']; ?>" class="">
+	<div id="gv_list_<?php echo $entry['id']; ?>" class="">
 
-			<div class="gv-list-view-title">
+		<div class="gv-list-view-title">
 
-				<?php if( !empty( $this->fields['single_list-title'] ) ):
-					$i = 0;
-					foreach( $this->fields['single_list-title'] as $field ) :
-						if( $i == 0 ): ?>
-							<h3 class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></h3>
-						<?php else: ?>
-							<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
-						<?php endif;
-						$i++; ?>
-					<?php endforeach; ?>
-				<?php endif; ?>
+			<?php if( !empty( $this->fields['single_list-title'] ) ):
+				$i = 0;
+				foreach( $this->fields['single_list-title'] as $field ) :
+					if( $i == 0 ): ?>
+						<h3 class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></h3>
+					<?php else: ?>
+						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
+					<?php endif;
+					$i++; ?>
+				<?php endforeach; ?>
+			<?php endif; ?>
 
-			</div>
+		</div>
 
 		<div class="gv-list-view-content">
 
@@ -49,19 +49,20 @@
 
 		</div>
 
+		<?php if( !empty(  $this->fields['single_list-footer'] ) ): ?>
+
 		<div class="gv-list-view-footer">
 
-			<?php if( !empty(  $this->fields['single_list-footer'] ) ): ?>
-				<ul>
-					<?php foreach( $this->fields['single_list-footer'] as $field ) : ?>
-						<li class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></li>
-					<?php endforeach; ?>
-				</ul>
-			<?php endif; ?>
+			<ul>
+				<?php foreach( $this->fields['single_list-footer'] as $field ) : ?>
+					<li class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></li>
+				<?php endforeach; ?>
+			</ul>
 
 		</div>
+		<?php endif; ?>
 
-		</div>
+	</div>
 
 	<?php endforeach; ?>
 
