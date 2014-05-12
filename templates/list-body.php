cb152f1e437719c63f0fec23de44cbdcf61a59ce
Luis Godinho <luis@gomo.pt>
2014-05-12 11:14:11 +0100

    #88 now with the correct field names.

--- templates/list-body.php
+++ templates/list-body.php
@@ -19,9 +19,9 @@ foreach( $this->entries as $entry ) :
 
 		<div class="gv-list-view-title">
 
-			<?php if( !empty(  $this->fields['list-title'] ) ):
+			<?php if( !empty(  $this->fields['directory_list-title'] ) ):
 				$i = 0;
-				foreach( $this->fields['list-title'] as $field ) :
+				foreach( $this->fields['directory_list-title'] as $field ) :
 
 					if( $i == 0 ): ?>
 						<h3 class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></h3>
@@ -37,24 +37,24 @@ foreach( $this->entries as $entry ) :
 		<div class="gv-list-view-content">
 
 			<div class="gv-list-view-content-description">
-				<?php if( !empty(  $this->fields['list-content-description'] ) ):
-					foreach( $this->fields['list-content-description'] as $field ) : ?>
+				<?php if( !empty(  $this->fields['directory_list-content-description'] ) ):
+					foreach( $this->fields['directory_list-content-description'] as $field ) : ?>
 						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</div>
 
 			<div class="gv-list-view-content-image">
-				<?php if( !empty(  $this->fields['list-content-image'] ) ):
-					foreach( $this->fields['list-content-image'] as $field ) : ?>
+				<?php if( !empty(  $this->fields['directory_list-content-image'] ) ):
+					foreach( $this->fields['directory_list-content-image'] as $field ) : ?>
 						<?php echo gv_value( $entry, $field ); ?>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</div>
 
 			<div class="gv-list-view-content-attributes">
-				<?php if( !empty(  $this->fields['list-content-attributes'] ) ):
-					foreach( $this->fields['list-content-attributes'] as $field ) : ?>
+				<?php if( !empty(  $this->fields['directory_list-content-attributes'] ) ):
+					foreach( $this->fields['directory_list-content-attributes'] as $field ) : ?>
 						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
 					<?php endforeach; ?>
 				<?php endif; ?>
@@ -64,9 +64,9 @@ foreach( $this->entries as $entry ) :
 
 		<div class="gv-list-view-footer">
 
-			<?php if( !empty(  $this->fields['list-footer'] ) ): ?>
+			<?php if( !empty(  $this->fields['directory_list-footer'] ) ): ?>
 				<ul>
-					<?php foreach( $this->fields['list-footer'] as $field ) : ?>
+					<?php foreach( $this->fields['directory_list-footer'] as $field ) : ?>
 						<li class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></li>
 					<?php endforeach; ?>
 				</ul>
--- templates/list-single.php
+++ templates/list-single.php
@@ -7,9 +7,9 @@
 
 			<div class="gv-list-view-title">
 
-				<?php if( !empty( $this->fields['single-list-title'] ) ):
+				<?php if( !empty( $this->fields['single_list-title'] ) ):
 					$i = 0;
-					foreach( $this->fields['single-list-title'] as $field ) :
+					foreach( $this->fields['single_list-title'] as $field ) :
 						if( $i == 0 ): ?>
 							<h3 class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></h3>
 						<?php else: ?>
@@ -24,24 +24,24 @@
 		<div class="gv-list-view-content">
 
 			<div class="gv-list-view-content-description">
-				<?php if( !empty(  $this->fields['single-list-description'] ) ):
-					foreach( $this->fields['single-list-description'] as $field ) : ?>
+				<?php if( !empty(  $this->fields['single_list-description'] ) ):
+					foreach( $this->fields['single_list-description'] as $field ) : ?>
 						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</div>
 
 			<div class="gv-list-view-content-image">
-				<?php if( !empty(  $this->fields['single-list-image'] ) ):
-					foreach( $this->fields['single-list-image'] as $field ) : ?>
+				<?php if( !empty(  $this->fields['single_list-image'] ) ):
+					foreach( $this->fields['single_list-image'] as $field ) : ?>
 						<?php echo gv_value( $entry, $field ); ?>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</div>
 
 			<div class="gv-list-view-content-attributes">
-				<?php if( !empty(  $this->fields['single-list-attributes'] ) ):
-					foreach( $this->fields['single-list-attributes'] as $field ) : ?>
+				<?php if( !empty(  $this->fields['single_list-attributes'] ) ):
+					foreach( $this->fields['single_list-attributes'] as $field ) : ?>
 						<p class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></p>
 					<?php endforeach; ?>
 				<?php endif; ?>
@@ -51,9 +51,9 @@
 
 		<div class="gv-list-view-footer">
 
-			<?php if( !empty(  $this->fields['single-list-footer'] ) ): ?>
+			<?php if( !empty(  $this->fields['single_list-footer'] ) ): ?>
 				<ul>
-					<?php foreach( $this->fields['single-list-footer'] as $field ) : ?>
+					<?php foreach( $this->fields['single_list-footer'] as $field ) : ?>
 						<li class="<?php echo gv_class( $field ); ?>"><?php echo esc_html( gv_label( $field ) ); ?><?php echo gv_value( $entry, $field ); ?></li>
 					<?php endforeach; ?>
 				</ul>
