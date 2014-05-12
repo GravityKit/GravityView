4e2249509b3e3503cf7f6ddf531afb2b6a88ba78
Zack Katz <zack@katzwebservices.com>
2014-05-12 11:10:07 -0600

    Have the file upload show an image if it exists

--- templates/fields/fileupload.php
+++ templates/fields/fileupload.php
@@ -1,32 +1,13 @@
 <?php
+/**
+ * Display the fileupload field type
+ *
+ * @package GravityView
+ */
 
 global $gravityview_view;
 
 extract( $gravityview_view->__get('field_data') );
-
-
-/*@list($url, $title, $caption, $description) = $valueArray;
-
-$size = '';
-if(!empty($url)){
-
-	$src = $url;
-	$size = @getimagesize($src);
-	$img = "<img src='$src' {$size[3]}/>";
-	$target = '';
-	 $img = array(
-	 	'src' => $src,
-	 	'size' => $size,
-	 	'title' => $title,
-	 	'caption' => $caption,
-	 	'description' => $description,
-	 	'url' => esc_attr($url),
-	 	'code' => isset($size[3]) ? "<img src='$src' {$size[3]} />" : "<img src='$src' />"
-	 );
-	 echo $img['code'];
-	$lightboxclass = '';
-	$output = "<a href='{$url}'{$target}{$lightboxclass}>{$img['code']}</a>";
-}*/
 
 $output = "";
 if(!empty($value)){
@@ -34,17 +15,41 @@ if(!empty($value)){
     $file_paths = rgar($field,"multipleFiles") ? json_decode($value) : array($value);
 
     foreach($file_paths as $file_path){
-        $info = pathinfo($file_path);
-        if(GFCommon::is_ssl() && strpos($file_path, "http:") !== false ){
-            $file_path = str_replace("http:", "https:", $file_path);
-        }
-        $file_path = esc_attr(str_replace(" ", "%20", $file_path));
 
-        $output_arr[] = $format == "text" ? $file_path . PHP_EOL: "<li><a href='$file_path' target='_blank' title='" . __("Click to view", "gravityforms") . "'>" . $info["basename"] . "</a></li>";
+    	// Is this an image?
+		$image = new GravityView_Image(array(
+			'src' => $file_path,
+			'class' => 'gv-image gv-field-id-'.$field_settings['id'],
+			'alt' => $field_settings['label']
+		));
+
+		$image_html = $image->html();
+
+		// If so, use it!
+    	if(!empty($image_html)) {
+    		$content = $image;
+    	} else {
+    		// Otherwise, get a link
+	        $info = pathinfo($file_path);
+	        if(GFCommon::is_ssl() && strpos($file_path, "http:") !== false ){
+	            $file_path = str_replace("http:", "https:", $file_path);
+	        }
+	        $file_path = esc_attr(str_replace(" ", "%20", $file_path));
+	        $content = $info["basename"];
+	    }
+
+	    $text_format = $file_path . PHP_EOL;
+
+	    $html_format = "<li><a href='$file_path' target='_blank' title='" . __("Click to view", "gravityforms") . "'>" . $content . "</a></li>";
+
+	    $output_arr[] = ($format == "text" ? $text_format : $html_format);
+
     }
+
     $output = join(PHP_EOL, $output_arr);
   }
-$output = empty($output) || $format == "text" ? $output : sprintf("<ul>%s</ul>", $output);
+
+$output = empty($output) || $format == "text" ? $output : sprintf("<ul class='gv-field-file-uploads'>%s</ul>", $output);
 
 echo $output;
 
