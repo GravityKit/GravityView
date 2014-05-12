0b817c3af613a2a45a44b4da7b16f3273527c9ff
Zack Katz <zack@katzwebservices.com>
2014-05-12 16:21:27 -0600

    Improve support for multiple search forms
    If there are multiple views on a page, `id`s wouldn't work as well.
    This adds view IDs.

--- templates/widget-search.php
+++ templates/widget-search.php
@@ -7,11 +7,14 @@
 
 global $gravityview_view;
 
+$view_id = $gravityview_view->view_id;
+
 ?>
 
 <form class="gv-widget-search" method="get" action="">
 
 	<?php
+
 		// search filters (fields)
 		echo $gravityview_view->__get('search_fields');
 	?>
@@ -21,18 +24,18 @@ global $gravityview_view;
 
 		<?php if( $gravityview_view->__get('search_free') ): ?>
 			<div class="gv-search">
-				<label for="gv_search"><?php esc_html_e( 'Search Entries:', 'gravity-view' ); ?></label>
-				<input type="text" name="gv_search" id="gv_search" value="<?php echo $gravityview_view->__get('curr_search'); ?>" />
+				<label for="gv_search_<?php echo $view_id; ?>"><?php esc_html_e( 'Search Entries:', 'gravity-view' ); ?></label>
+				<input type="text" name="gv_search" id="gv_search_<?php echo $view_id; ?>" value="<?php echo $gravityview_view->__get('curr_search'); ?>" />
 			</div>
 		<?php endif; ?>
 
 		<?php if( $gravityview_view->__get('search_date') ): ?>
-
-			<label for="gv_start_date"><?php esc_html_e('Filter by date:', 'gravity-view' ); ?></label>
-			<input name="gv_start" id="gv_start_date" type="text" class="<?php echo esc_html($gravityview_view->__get('datepicker_class')); ?>" placeholder="<?php esc_attr_e('Start date', 'gravity-view' ); ?>" value="<?php echo $gravityview_view->__get('curr_start'); ?>">
-			<input name="gv_end" id="gv_end_date" type="text" class="<?php echo  esc_html($gravityview_view->__get('datepicker_class')); ?>" placeholder="<?php esc_attr_e('End date', 'gravity-view' ); ?>" value="<?php echo $gravityview_view->__get('curr_end'); ?>">
-
+			<div class="gv-search-date">
+				<label for="gv_start_date_<?php echo $view_id; ?>"><?php esc_html_e('Filter by date:', 'gravity-view' ); ?></label>
+				<input name="gv_start" id="gv_start_date_<?php echo $view_id; ?>" type="text" class="<?php echo esc_html($gravityview_view->__get('datepicker_class')); ?>" placeholder="<?php esc_attr_e('Start date', 'gravity-view' ); ?>" value="<?php echo $gravityview_view->__get('curr_start'); ?>">
+				<input name="gv_end" id="gv_end_date_<?php echo $view_id; ?>" type="text" class="<?php echo  esc_html($gravityview_view->__get('datepicker_class')); ?>" placeholder="<?php esc_attr_e('End date', 'gravity-view' ); ?>" value="<?php echo $gravityview_view->__get('curr_end'); ?>">
+			</div>
 		<?php endif; ?>
-		<input type="submit" class="button" id="gv_search_button" value="<?php esc_attr_e( 'Search', 'gravity-view' ); ?>" />
+		<input type="submit" class="button gv_search_button" id="gv_search_button_<?php echo $view_id; ?>" value="<?php esc_attr_e( 'Search', 'gravity-view' ); ?>" />
 	</div>
 </form>
