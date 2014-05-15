438e958acc73555fddbc28106ebba2c1c1198632
Zack Katz <zack@katzwebservices.com>
2014-05-15 11:57:47 -0600

    Show Views in Nav menu
    Yes, please!

--- gravityview.php
+++ gravityview.php
@@ -246,7 +246,7 @@ final class GravityView_Plugin {
 			'public'              => true,
 			'show_ui'             => true,
 			'show_in_menu'        => true,
-			'show_in_nav_menus'   => false,
+			'show_in_nav_menus'   => true,
 			'show_in_admin_bar'   => true,
 			'menu_position'       => 15,
 			'menu_icon'           => '',
