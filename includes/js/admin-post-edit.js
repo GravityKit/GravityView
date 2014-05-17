0b906b6a92c1aacfefbc12c4e84aca6669d5c7f2
Luis Godinho <luis@gomo.pt>
2014-05-17 12:17:26 +0100

    cleaning

--- includes/js/admin-post-edit.js
+++ includes/js/admin-post-edit.js
@@ -16,14 +16,11 @@
 	function InsertViewShortcode(){
 		var view_id = $("#gravityview_view_id").val();
 
-		if( view_id == "" ) {
+		if( view_id === '' ) {
 			alert( gvGlobals.alert_1 );
 			$("#gravityview_view_id").focus();
 			return false;
 		}
-
-		//'page_size' => '', 'sort_field' => '', 'sort_direction' => 'ASC', 'start_date' => '', 'end_date' => ''
-
 
 		var shortcode = '[gravityview id="' + view_id +'"';
 
@@ -35,18 +32,18 @@
 
 		// sorting
 		var sort_field = $("#gravityview_sort_field").val();
-		if( '' != sort_field ) {
+		if( '' !== sort_field ) {
 			var sort_direction = $("#gravityview_sort_direction").val();
 			shortcode += ' sort_field="' + sort_field + '"' + ' sort_direction="' + sort_direction + '"';
 		}
 
 		// date filtering
 		var start_date = $("#gravityview_start_date").val();
-		if( '' != start_date ) {
+		if( '' !== start_date ) {
 			shortcode += ' start_date="' + start_date + '"';
 		}
 		var end_date = $("#gravityview_end_date").val();
-		if( '' != end_date ) {
+		if( '' !== end_date ) {
 			shortcode += ' end_date="' + end_date + '"';
 		}
 
@@ -74,7 +71,7 @@
 				action: 'gv_sortable_fields',
 				viewid: $(this).val(),
 				nonce: gvGlobals.nonce,
-			}
+			};
 
 			$.post( gvGlobals.ajaxurl, data, function( response ) {
 				if( response ) {
