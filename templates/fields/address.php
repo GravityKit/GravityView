64603c16f43f0b2538dd7c94fc816ed02a6b7b4f
Zack Katz <zack@katzwebservices.com>
2014-06-18 19:55:43 -0600

    Field values into function; Fix names
    * Created new `gravityview_get_field_value()` function in class-api.php.
    * Fix name display

--- templates/fields/address.php
+++ templates/fields/address.php
@@ -9,20 +9,4 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-$output = '';
-
-if( floatval( $field_id ) === floor( floatval( $field_id ) ) ) {
-	// For the complete field value
-	$output = $display_value;
-} else {
-	// For part of the field value
-	$entry_keys = array_keys( $entry );
-	foreach( $entry_keys as $input_key ) {
-		if( is_numeric( $input_key ) && floatval( $input_key ) === floatval( $field_id ) ) {
-			$output = $entry[ $input_key ];
-			break;
-		}
-	}
-}
-
-echo $output;
+echo gravityview_get_field_value( $entry, $field_id, $display_value );
--- templates/fields/checkbox.php
+++ templates/fields/checkbox.php
@@ -4,23 +4,10 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-$output = '';
-
-if( floatval( $field_id ) === floor( floatval( $field_id ) ) ) {
-	// For the complete field value
-	$output = $display_value;
+if( in_array( $field['type'], array( 'radio', 'checkbox' ) ) && !empty( $entry[ $input_key ] ) ) {
+	$output = apply_filters( 'gravityview_field_tick', '<span class="dashicons dashicons-yes"></span>', $entry, $field);
 } else {
-	// For part of the field value
-	$entry_keys = array_keys( $entry );
-	foreach( $entry_keys as $input_key ) {
-		if( is_numeric( $input_key ) && floatval( $input_key ) === floatval( $field_id ) ) {
-			if( in_array( $field['type'], array( 'radio', 'checkbox' ) ) && !empty( $entry[ $input_key ] ) ) {
-				$output = apply_filters( 'gravityview_field_tick', '<span class="dashicons dashicons-yes"></span>', $entry, $field);
-			} else {
-				$output = $entry[ $input_key ];
-			}
-		}
-	}
+	$output = gravityview_get_field_value( $entry, $field_id, $display_value );
 }
 
 echo $output;
