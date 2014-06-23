1a81a7f8f1767941fa3458383ec0ee90ce9152bd
Luis Godinho <luis@gomo.pt>
2014-06-23 12:28:30 +0100

    Fixed: PHP Notice:  Undefined variable: input_key in /plugins/GravityView/templates/fields/checkbox.php on line 7

--- templates/fields/checkbox.php
+++ templates/fields/checkbox.php
@@ -4,7 +4,7 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-if( in_array( $field['type'], array( 'radio', 'checkbox' ) ) && !empty( $entry[ $input_key ] ) ) {
+if( in_array( $field['type'], array( 'radio', 'checkbox' ) ) && !empty( $entry[ $field_id ] ) ) {
 	$output = apply_filters( 'gravityview_field_tick', '<span class="dashicons dashicons-yes"></span>', $entry, $field);
 } else {
 	$output = gravityview_get_field_value( $entry, $field_id, $display_value );
