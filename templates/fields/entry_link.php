5fc57d6e0c7b9961b14c300369ce04d6131bd7d4
Zack Katz <zack@katzwebservices.com>
2014-06-27 08:07:53 -0600

    Fix empty custom link text

--- templates/fields/entry_link.php
+++ templates/fields/entry_link.php
@@ -4,7 +4,9 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-$output = apply_filters( 'gravityview_entry_link', GravityView_API::replace_variables( $field_settings['entry_link_text'], $form, $entry ) );
+$link_text = empty( $field_settings['entry_link_text'] ) ? __('View Details', 'gravity-view') : $field_settings['entry_link_text'];
+
+$output = apply_filters( 'gravityview_entry_link', GravityView_API::replace_variables( $link_text, $form, $entry ) );
 
 $href = GravityView_API::entry_link( $entry, $field );
 $output = '<a href="'. $href .'">'. $output . '</a>';
