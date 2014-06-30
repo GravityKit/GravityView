9564d03e8ba375760635b8aba39025aae9a15501
Luis Godinho <luis@gomo.pt>
2014-06-30 17:43:05 +0100

    FixedHeader & FixedColumns adding styles and scripts

--- includes/extensions/datatables/includes/class-datatables-data.php
+++ includes/extensions/datatables/includes/class-datatables-data.php
@@ -36,6 +36,10 @@ class GV_Extension_DataTables_Data {
 		add_action( 'gravityview_datatables_scripts_styles', array( $this, 'scroller_add_scripts' ), 10, 3 );
 		add_filter( 'gravityview_datatables_js_options', array( $this, 'scroller_add_config' ), 10, 3 );
 
+		// extensions - FixedHeader & FixedColumns
+		add_action( 'gravityview_datatables_scripts_styles', array( $this, 'fixedheadercolumns_add_scripts' ), 10, 3 );
+		add_filter( 'gravityview_datatables_js_options', array( $this, 'fixedheadercolumns_add_config' ), 10, 3 );
+
 	}
 
 	/**
@@ -415,7 +419,26 @@ class GV_Extension_DataTables_Data {
 	}
 
 
+	/** FixedHeader & FixedColumns */
 
+	/**
+	 * Inject FixedHeader & FixedColumns Scripts and Styles if needed
+	 */
+	function fixedheadercolumns_add_scripts( $dt_config, $view_id, $post ) {
+
+		$settings = get_post_meta( $view_id, '_gravityview_datatables_settings', true );
+
+		if( !empty( $settings['fixedheader'] ) ) {
+			wp_enqueue_script( 'gv-dt-fixedheader', apply_filters( 'gravityview_dt_fixedheader_script_src', '//cdn.datatables.net/fixedheader/2.1.1/js/dataTables.fixedHeader.min.js' ), array( 'jquery', 'gv-datatables' ), GV_Extension_DataTables::version, true );
+			wp_enqueue_style( 'gv-dt_fixedheader_style', apply_filters( 'gravityview_dt_fixedheader_style_src', '//cdn.datatables.net/fixedheader/2.1.1/css/dataTables.fixedHeader.css' ), array('gv-datatables_style'), GV_Extension_DataTables::version, 'all' );
+		}
+
+		if( !empty( $settings['fixedcolumns'] ) ) {
+			wp_enqueue_script( 'gv-dt-fixedcolumns', apply_filters( 'gravityview_dt_fixedcolumns_script_src', '//cdn.datatables.net/fixedcolumns/3.0.1/js/dataTables.fixedColumns.min.js' ), array( 'jquery', 'gv-datatables' ), GV_Extension_DataTables::version, true );
+			wp_enqueue_style( 'gv-dt_fixedcolumns_style', apply_filters( 'gravityview_dt_fixedcolumns_style_src', '//cdn.datatables.net/fixedcolumns/3.0.1/css/dataTables.fixedColumns.css' ), array('gv-datatables_style'), GV_Extension_DataTables::version, 'all' );
+		}
+
+	}
 
 } // end class
 new GV_Extension_DataTables_Data;
