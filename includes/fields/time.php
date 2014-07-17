bf3833315a7ecc194949489823ff691127241961
Zack Katz <zack@katzwebservices.com>
2014-07-17 17:04:47 -0600

    Add Time field
    It's of the essence.

---
+++ includes/fields/time.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Add custom options for date fields
+ */
+class GravityView_Field_Time extends GravityView_Field {
+
+	var $name = 'time';
+
+	function field_options( $field_options, $template_id = '', $field_id = '', $context = '', $input_type = '' ) {
+
+		add_filter('gravityview_date_format', array( $this, 'date_format' ) );
+		$this->add_field_support('date_display', $field_options );
+		remove_filter('gravityview_date_format', array( $this, 'date_format' ) );
+
+		return $field_options;
+	}
+
+	function date_format() {
+		return 'h:iA';
+	}
+
+}
+
+new GravityView_Field_Time;
--- templates/fields/time.php
+++ templates/fields/time.php
@@ -4,7 +4,7 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-if( !empty( $field_settings ) && !empty( $field_settings['date_display'] ) && !empty( $value )) {
+if( !empty( $field_settings ) && !empty( $field_settings['date_display'] ) && !empty( $value ) ) {
 
 	// If there is a custom PHP date format passed via the date_display setting,
 	// use PHP's date format
