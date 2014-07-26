bb65745c31e1b8d2805928f02fb15991ed057b98
Zack Katz <zack@katzwebservices.com>
2014-07-26 00:31:48 -0600

    Version Bump

--- gravityview.php
+++ gravityview.php
@@ -14,7 +14,7 @@
  * Plugin Name:       	GravityView
  * Plugin URI:        	http://gravityview.co
  * Description:       	Create directories based on a Gravity Forms form, insert them using a shortcode, and modify how they output.
- * Version:          	1.1
+ * Version:          	1.1.1
  * Author:            	Katz Web Services, Inc.
  * Author URI:        	http://www.katzwebservices.com
  * Text Domain:       	gravity-view
@@ -64,7 +64,7 @@ if( is_admin() ) {
  */
 final class GravityView_Plugin {
 
-	const version = '1.1';
+	const version = '1.1.1';
 
 	public static $theInstance;
 
