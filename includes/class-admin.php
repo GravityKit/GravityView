ec78bc477547d2f7969ad1679e83655174b50af1
Luis Godinho <luis@gomo.pt>
2014-09-12 22:21:24 +0100

    include the new classes

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -48,6 +48,8 @@ class GravityView_Admin {
 	 */
 	public function backend_actions() {
 
+		include_once( GRAVITYVIEW_DIR . 'includes/admin/class.field.type.php' );
+		include_once( GRAVITYVIEW_DIR . 'includes/admin/class.render.settings.php' );
 		include_once( GRAVITYVIEW_DIR .'includes/class-admin-label.php' );
 		include_once( GRAVITYVIEW_DIR .'includes/class-admin-views.php' );
 		include_once( GRAVITYVIEW_DIR .'includes/class-admin-welcome.php' );
