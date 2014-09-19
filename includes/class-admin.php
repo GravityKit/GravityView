0e8c25ae3a1f75fb4aa02c9bcc3dfd1839d355f5
Luis Godinho <luis@gomo.pt>
2014-09-19 20:09:34 +0100

    Migration class - seamless beauty

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -17,6 +17,10 @@ class GravityView_Admin {
 			return;
 		}
 
+		// Migrate Class
+		require_once( GRAVITYVIEW_DIR . 'includes/class-migrate.php' );
+
+
 		require_once( GFCommon::get_base_path() . '/tooltips.php' );
 
 		require_once( GRAVITYVIEW_DIR . 'includes/admin/metaboxes.php' );
