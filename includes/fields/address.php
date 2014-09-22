78d7c12b197e3b5fafd70c9b42ada76ca98379bb
Zack Katz <zack@katzwebservices.com>
2014-09-21 21:52:51 -0600

    Add support for hiding Map It link
    Added: Option to disable "Map It" link for the full Address field.
    	- New `gravityview_get_map_link()` function with
    `gravityview_map_link` filter. To learn how to modify the map link,
    [refer to this how-to
    article](https://gravityview.co/support/documentation/201608159)
    	- The "Map It" string is now translatable

---
+++ includes/fields/address.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Add custom options for address fields
+ */
+class GravityView_Field_Address extends GravityView_Field {
+
+	var $name = 'address';
+
+	function field_options( $field_options, $template_id = '', $field_id = '', $context = '', $input_type = '' ) {
+
+		// If this is NOT the full address field, return default options.
+		if( floor( $field_id ) !== floatval( $field_id ) ) {
+			return $field_options;
+		}
+
+		$add_options = array();
+
+		$add_options['show_map_link'] = array(
+			'type' => 'checkbox',
+			'label' => __( 'Show Map Link:', 'gravity-view' ),
+			'desc' => __('Display a "Map It" link below the address', 'gravity-view'),
+			'default' => true,
+			'merge_tags' => false,
+		);
+
+		return $add_options + $field_options;
+	}
+
+}
+
+new GravityView_Field_Address;
--- templates/fields/address.php
+++ templates/fields/address.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Address field output
+ * Address field output, with "Map It" link removed
  *
  * @group GravityView
  */
@@ -9,4 +9,26 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-echo gravityview_get_field_value( $entry, $field_id, $display_value );
+// If it's the full address
+if( floor( $field_id ) === floatval( $field_id ) ) {
+
+	// Use Gravity Forms' method to get the full address.
+	// Pass the `text` parameter so the map link isn't added like when passing `html`
+	$value_with_newline = GFCommon::get_lead_field_display( $field, $value, "", false, 'text' );
+
+	// Add map link if it's not set (default, back compat) or if it's set to yes
+	if( !isset( $field_settings['show_map_link'] ) || !empty( $field_settings['show_map_link'] ) ){
+
+		// Add the map link as another line
+	    $value_with_newline .= "\n" . gravityview_get_map_link( $value_with_newline );
+
+	}
+
+	// Full address without the "Map It" link
+	echo str_replace("\n", '<br />', $value_with_newline );
+
+} else {
+
+	echo gravityview_get_field_value( $entry, $field_id, $display_value );
+
+}
