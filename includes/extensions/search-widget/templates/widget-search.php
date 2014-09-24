4c7168e08a0c3f687058e95fedd72bc682f68909
Luis Godinho <luis@gomo.pt>
2014-09-24 18:33:14 +0100

    load search button only if needed

--- includes/extensions/search-widget/templates/widget-search.php
+++ includes/extensions/search-widget/templates/widget-search.php
@@ -10,6 +10,8 @@ global $gravityview_view, $wp_rewrite;
 $view_id = $gravityview_view->view_id;
 $search_class = gravityview_sanitize_html_class( apply_filters( 'gravityview_search_class', 'gv-search-'.$gravityview_view->search_layout ) );
 
+$has_inputs = false;
+
 ?>
 
 <form class="gv-widget-search <?php echo $search_class; ?>" method="get" action="<?php echo esc_url( add_query_arg( array() ) ); ?>">
@@ -20,6 +22,11 @@ $search_class = gravityview_sanitize_html_class( apply_filters( 'gravityview_sea
 	foreach( $this->search_fields as $search_field ) {
 		$gravityview_view->search_field = $search_field;
 		$this->render( 'search-field', $search_field['input'] );
+
+		// show/hide the search button if there are input type fields
+		if( !$has_inputs &&  $search_field['input'] != 'link' ) {
+			$has_inputs = true;
+		}
 	}
 
 	do_action( 'gravityview_search_widget_fields_after' );
@@ -37,14 +44,16 @@ $search_class = gravityview_sanitize_html_class( apply_filters( 'gravityview_sea
 
 	?>
 
-	<div class="gv-search-box gv-search-box-submit">
-		<?php
+	<?php if( $has_inputs ) : ?>
+		<div class="gv-search-box gv-search-box-submit">
+			<?php
 
-		// Support default permalink structure
-		if( !empty( $_GET['gravityview'] ) && false === $wp_rewrite->using_index_permalinks() ) {
-			echo '<input type="hidden" name="gravityview" value="'.esc_attr( $_GET['gravityview'] ).'" />';
-		}
-		?>
-		<input type="submit" class="button gv-search-button" id="gv_search_button_<?php echo $view_id; ?>" value="<?php esc_attr_e( 'Search', 'gravity-view' ); ?>" />
-	</div>
+			// Support default permalink structure
+			if( !empty( $_GET['gravityview'] ) && false === $wp_rewrite->using_index_permalinks() ) {
+				echo '<input type="hidden" name="gravityview" value="'.esc_attr( $_GET['gravityview'] ).'" />';
+			}
+			?>
+			<input type="submit" class="button gv-search-button" id="gv_search_button_<?php echo $view_id; ?>" value="<?php esc_attr_e( 'Search', 'gravity-view' ); ?>" />
+		</div>
+	<?php endif; ?>
 </form>
