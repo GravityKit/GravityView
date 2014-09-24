f1fb65a05686390af13c905dc85810b84f21d1c5
Luis Godinho <luis@gomo.pt>
2014-09-24 16:19:41 +0100

    new filter hook for label and link separator

--- includes/extensions/search-widget/templates/search-field-link.php
+++ includes/extensions/search-widget/templates/search-field-link.php
@@ -9,15 +9,18 @@ global $gravityview_view;
 $view_id = $gravityview_view->view_id;
 $search_field = $gravityview_view->search_field;
 
+$links_label = apply_filters( 'gravityview/extension/search/links_label', __( 'Show only:', 'gravity-view' ) );
+$links_sep = apply_filters( 'gravityview/extension/search/links_sep', '&nbsp;|&nbsp;' );
 ?>
 
 <div class="gv-search-box">
 
-	<p class=""><?php esc_html_e( 'Show only:', 'gravity-view' ); ?>
+	<p class="gv-search-box-links">
+		<?php echo esc_html( $links_label ); ?>
 
 		<?php foreach( $search_field['choices'] as $k => $choice ) :
 
-			if( $k != 0 ) { echo '&nbsp;|&nbsp;'; }?>
+			if( $k != 0 ) { echo esc_html( $links_sep ); }?>
 
 			<a href="<?php echo esc_url( add_query_arg( array( $search_field['name'] => $choice['value'] ) ) ); ?>">
 				<?php echo esc_html( $choice['text'] ); ?>
