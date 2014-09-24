179d0c5bc5d2f3d957b7d825172da709339f7f61
Luis Godinho <luis@gomo.pt>
2014-09-24 15:38:59 +0100

    Fix Radio id + Label.for attr

--- includes/extensions/search-widget/templates/search-field-radio.php
+++ includes/extensions/search-widget/templates/search-field-radio.php
@@ -11,12 +11,12 @@ $search_field = $gravityview_view->search_field;
 
 ?>
 <div class="gv-search-box">
-	<ul>
+
 	<?php foreach( $search_field['choices'] as $choice ) : ?>
-		<li>
-			<input type="radio" name="<?php echo esc_attr( $search_field['name'] ); ?>" value="<?php echo esc_attr( $choice['value'] ); ?>" id="search-box-<?php echo esc_attr( $search_field['name'] ); ?>" <?php checked( $choice['value'], $search_field['value'], true, 'checked' ); ?>>
-			<label for="search-box-<?php echo esc_attr( $search_field['name'] ); ?>"><?php echo esc_html( $choice['text'] ); ?></label>
-		</li>
+
+			<input type="radio" name="<?php echo esc_attr( $search_field['name'] ); ?>" value="<?php echo esc_attr( $choice['value'] ); ?>" id="search-box-<?php echo sanitize_html_class( $search_field['name'].$choice['value'].$choice['text'] ); ?>" <?php checked( $choice['value'], $search_field['value'], true ); ?>>
+			<label for="search-box-<?php echo sanitize_html_class( $search_field['name'].$choice['value'].$choice['text'] ); ?>"><?php echo esc_html( $choice['text'] ); ?></label>
+
 	<?php endforeach; ?>
-	</ul>
+
 </div>
