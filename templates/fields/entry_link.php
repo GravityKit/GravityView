f0a8fd30abfa40fb10a48c6572e45fa3802e0ac0
Zack Katz <zack@katzwebservices.com>
2014-09-28 17:06:15 -0600

    Make sure not to render entry link in single context

--- templates/fields/entry_link.php
+++ templates/fields/entry_link.php
@@ -4,6 +4,9 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
+// Don't show on single entry
+if( $gravityview_view->context === 'single' ) { return; }
+
 $link_text = empty( $field_settings['entry_link_text'] ) ? __('View Details', 'gravity-view') : $field_settings['entry_link_text'];
 
 $output = apply_filters( 'gravityview_entry_link', GravityView_API::replace_variables( $link_text, $form, $entry ) );
