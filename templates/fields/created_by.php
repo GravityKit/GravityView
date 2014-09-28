250b3d9689566c00336e6134e5d126aa4975e0e0
Zack Katz <zack@katzwebservices.com>
2014-09-28 16:31:19 -0600

    Fix unset index issue with name display

--- templates/fields/created_by.php
+++ templates/fields/created_by.php
@@ -17,6 +17,7 @@ if( empty( $value ) ) {
 // Get the user data for the passed User ID
 $User = get_userdata($value);
 
-// Display the user data, based on the settings
-// `id`, `username`, or `display_name`
-echo $User->{$field_settings['name_display']};
+// Display the user data, based on the settings `id`, `username`, or `display_name`
+$name_display = empty( $field_settings['name_display'] ) ? 'display_name' : $field_settings['name_display'];
+
+echo $User->{$name_display};
