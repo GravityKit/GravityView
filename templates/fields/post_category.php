ce7334bfc9562df0a6c62716cf0337b5cc9b05b3
Zack Katz <zack@katzwebservices.com>
2014-10-27 09:14:28 -0600

    Live post data fix
     PHP error when displaying Post Content fields using Live Data for a
    post that no longer is published.

--- templates/fields/post_category.php
+++ templates/fields/post_category.php
@@ -5,7 +5,15 @@ global $gravityview_view;
 extract( $gravityview_view->field_data );
 
 if( !empty( $field_settings['dynamic_data'] ) ) {
-	echo gravityview_get_the_term_list( $entry['post_id'], $field_settings['link_to_term'], 'category');
+
+	$term_list = gravityview_get_the_term_list( $entry['post_id'], $field_settings['link_to_term'], 'category');
+
+	if( empty( $term_list ) ) {
+		do_action('gravityview_log_debug', 'Dynamic data for post #'.$entry['post_id'].' doesnt exist.' );
+	}
+
+	echo $term_list;
+
 } else {
 
 	if( empty( $field_settings['link_to_term'] ) ) {
