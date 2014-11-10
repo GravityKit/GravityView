6c79687c5762dc971ee78a2b20425c28093121be
Luis Godinho <luis@gomo.pt>
2014-11-10 18:58:38 +0000

    edit entry detail content
    - and merge field options with form field properties

--- includes/extensions/edit-entry/class-gv-gfentrydetail.php
+++ includes/extensions/edit-entry/class-gv-gfentrydetail.php
@@ -66,15 +66,33 @@ class GV_GFEntryDetail {
 
                     foreach( $fields as $field ){
 
-                        $field_id = $field['id'];
+                        $td_id = "field_" . $form_id . "_" . $field['id'];
+                        $value = RGFormsModel::get_lead_field_value( $lead, $field );
+                        $label = esc_html( GFCommon::get_label( $field ) );
+                        $input = GV_GFCommon::get_field_input( $field, $value, $lead['id'], $form_id );
+                        $error_class = rgget( 'failed_validation', $field ) ? "gfield_error" : "";
+
+                        $validation_message = ( rgget('failed_validation', $field ) && !empty( $field['validation_message'] ) ) ? sprintf("<div class='gfield_description validation_message'>%s</div>", $field['validation_message'] ) : '';
+
+                        if( rgar( $field, 'descriptionPlacement') == 'above' ) {
+                            $input = $validation_message . $input;
+                        } else {
+                            $input = $input . $validation_message;
+                        }
+
+                        //Add required indicator
+                        $required = ( $field['isRequired'] == 1 ) ? '<span class="required">*</span>' : '';
+
+                        // custom class as defined on field details
+                        $custom_class = empty( $field['gvCustomClass'] ) ? '' : ' class="'. esc_attr( $field['gvCustomClass'] ) .'"';
 
                         switch( RGFormsModel::get_input_type( $field ) ){
 
                             case 'section' :
                                 ?>
-                                <tr valign="top">
+                                <tr valign="top"<?php echo $custom_class; ?>>
                                         <td class="detail-view">
-                                                <div style="margin-bottom:10px; border-bottom:1px dotted #ccc;"><h2 class="detail_gsection_title"><?php echo esc_html( GFCommon::get_label( $field ) ) ?></h2></div>
+                                                <div style="margin-bottom:10px; border-bottom:1px dotted #ccc;"><h2 class="detail_gsection_title"><?php echo $label; ?></h2></div>
                                         </td>
                                 </tr>
                                 <?php
@@ -88,12 +106,15 @@ class GV_GFEntryDetail {
                             break;
 
                             default :
-                                $value = RGFormsModel::get_lead_field_value( $lead, $field );
-                                $td_id = 'field_' . $form_id . '_' . $field_id;
 
-                                $content = "<tr valign='top'><td class='detail-view' id='{$td_id}'><label class='detail-label'>" . esc_html(GFCommon::get_label($field)) . "</label>" . GFCommon::get_field_input($field, $value, $lead["id"]) . "</td></tr>";
+                                $content =
+                                    '<tr valign="top"'. $custom_class .'>
+                                        <td class="detail-view '.$error_class.'" id="'. $td_id .'">
+                                            <label class="detail-label">' . $label . $required . '</label>' . $input . '
+                                        </td>
+                                    </tr>';
 
-                                $content = apply_filters( "gform_field_content", $content, $field, $value, $lead["id"], $form["id"] );
+                                $content = apply_filters( 'gravityview_edit_entry_field_content', $content, $field, $value, $lead['id'], $form['id'] );
 
                                 echo $content;
                             break;
@@ -104,8 +125,8 @@ class GV_GFEntryDetail {
                 </table>
                 <br/>
                 <div class="gform_footer">
-                        <input type="hidden" name="gform_unique_id" value="" />
-                        <input type="hidden" name="gform_uploaded_files" id='gform_uploaded_files_<?php echo $form_id; ?>' value="" />
+                    <input type="hidden" name="gform_unique_id" value="" />
+                    <input type="hidden" name="gform_uploaded_files" id='gform_uploaded_files_<?php echo $form_id; ?>' value="" />
                 </div>
             </div>
         </div>
@@ -134,10 +155,8 @@ class GV_GFEntryDetail {
 
                 if( $prop['id'] == $field['id'] ){
 
-                    $properties = array_merge( $prop, $field );
-
-                    if( !self::hide_field_check_conditions( $properties ) ) {
-                       $edit_fields[] = $field;
+                    if( self::is_field_editable( $prop ) ) {
+                       $edit_fields[] = self::merge_field_properties( $field, $prop );
                     }
 
                 } elseif( !empty( $field['inputs'] ) ){
@@ -148,10 +167,8 @@ class GV_GFEntryDetail {
 
                         if( $prop['id'] == $input['id'] ){
 
-                            $properties = array_merge( $prop, $input );
-
-                            if( !self::hide_field_check_conditions( $properties ) ) {
-                                $edit_fields[] = $field;
+                            if( self::is_field_editable( $prop ) ) {
+                                $edit_fields[] = self::merge_field_properties( $field, $prop );
                             }
 
                         }
@@ -176,12 +193,36 @@ class GV_GFEntryDetail {
      * @param array $properties
      * @return true (field should be hidden) or false (field should be presented)
      */
-    static private function hide_field_check_conditions( $properties ) {
+    static private function is_field_editable( $properties ) {
 
-        if( $properties['allow_edit'] != 1 || !current_user_can( $properties['allow_edit_cap'] ) ) {
+        if( current_user_can( $properties['allow_edit_cap'] ) ) {
             return true;
         }
 
         return false;
     }
+
+    /**
+     * Override GF Form field properties with the ones defined on the View
+     * @param  [type] $field GF Form field object
+     * @param  [type] $prop  GV field options
+     * @return array
+     */
+    static private function merge_field_properties( $field, $prop ) {
+
+        if( empty( $prop['show_label'] ) ) {
+            $field['label'] = '';
+        } elseif ( !empty( $prop['custom_label'] ) ) {
+            $field['label'] = $prop['custom_label'];
+        }
+
+        if( !empty( $prop['custom_class'] ) ) {
+             $field['gvCustomClass'] = gravityview_sanitize_html_class( $prop['custom_class'] );
+        }
+
+        return $field;
+
+    }
+
+
 }
