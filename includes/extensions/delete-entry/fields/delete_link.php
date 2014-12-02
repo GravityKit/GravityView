19299f70af780521192c4b99a37a6ada4a5a4acd
Luis Godinho <luis@gomo.pt>
2014-12-02 17:03:42 +0000

    when showing link, check delete caps

--- includes/extensions/delete-entry/fields/delete_link.php
+++ includes/extensions/delete-entry/fields/delete_link.php
@@ -4,8 +4,8 @@ global $gravityview_view;
 
 extract( $gravityview_view->field_data );
 
-// Only show the link to logged-in users.
-if( !GravityView_Edit_Entry::check_user_cap_edit_entry( $entry ) ) {
+// Only show the link to logged-in users with the rigth caps.
+if( !GravityView_Delete_Entry::check_user_cap_delete_entry( $entry, $field_settings ) ) {
 	return;
 }
 
@@ -13,7 +13,7 @@ $link_text = empty( $field_settings['delete_link'] ) ? __('Delete Entry', 'gravi
 
 $output = apply_filters( 'gravityview_entry_link', GravityView_API::replace_variables( $link_text, $form, $entry ) );
 
-$href = GravityView_Delete_Entry::get_delete_link( $entry, $field );
+$href = GravityView_Delete_Entry::get_delete_link( $entry );
 
 $onclick = GravityView_Delete_Entry::get_confirm_dialog();
 
