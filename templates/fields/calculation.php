4405daa5f3f775c03816fdabeaac373b9336e29e
Zack Katz <zack@katzwebservices.com>
2015-02-18 02:10:12 -0700

    And calculation field, too

---
+++ templates/fields/calculation.php
@@ -0,0 +1,7 @@
+<?php
+
+$gravityview_view = GravityView_View::getInstance();
+
+extract( $gravityview_view->getCurrentField() );
+
+echo gravityview_get_field_value( $entry, $field_id, $display_value );
