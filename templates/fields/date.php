81197669385ef3237aeba8da57f427283fca3a00
Zack Katz <zack@katzwebservices.com>
2015-02-25 12:46:07 -0700

    Don't show date field value if set to Unix Epoch (1/1/1970)
    Also adds a filter to undo this (`gravityview/fields/date/hide_epoch`).
    Return false to show value. Use `__return_false` callback.

--- templates/fields/date.php
+++ templates/fields/date.php
@@ -4,6 +4,22 @@ $gravityview_view = GravityView_View::getInstance();
 
 extract( $gravityview_view->getCurrentField() );
 
+/**
+ * Unix Epoch probably isn't what you're looking for.
+ * @since 1.7
+ */
+if( $value === '1970-01-01' ) {
+
+	/**
+	 * Return false to show value. Use `__return_false` callback.
+	 */
+	$hide_epoch = apply_filters( 'gravityview/fields/date/hide_epoch', true );
+
+	if( $hide_epoch ) {
+		return;
+	}
+}
+
 if( !empty( $field_settings ) && !empty( $field_settings['date_display'] ) && !empty( $value )) {
 
 	// If there is a custom PHP date format passed via the date_display setting,
