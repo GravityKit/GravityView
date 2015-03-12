2cd55a511787e7e7b9efe9578ae47a8c888f5bc7
Zack Katz <zack@katzwebservices.com>
2015-03-12 14:57:06 -0600

    Rewrite the JS to be cleaner

--- assets/js/admin-entries-list.js
+++ assets/js/admin-entries-list.js
@@ -8,14 +8,109 @@
  * @copyright Copyright 2014, Katz Web Services, Inc.
  *
  * @since 1.0.0
+ *
+ * global gvGlobals
  */
 
 
 (function( $ ) {
 
-	function displayMessage( message, messageClass, container ) {
+	"use strict";
 
-		hideMessage( container, true );
+	var self = {};
+
+	/**
+	 * Enable Approve/Reject functionality on the Gravity Forms Entries page
+	 */
+	self.init = function() {
+
+		self.maybeDisplayMessages();
+
+		self.addApprovedColumn();
+
+		self.addBulkAction();
+
+		self.setInitialApprovedEntries();
+
+		$( '.toggleApproved' ).click( self.toggleApproved );
+	};
+
+	/**
+	 * If there are messages, display them
+	 */
+	self.maybeDisplayMessages = function() {
+		// display update message if any
+		if ( gvGlobals.bulk_message.length > 0 ) {
+			self.displayMessage( gvGlobals.bulk_message, 'updated', '#lead_form' );
+		}
+	};
+
+	/**
+	 * Mark the entries that are approved as approved on load
+	 */
+	self.setInitialApprovedEntries = function() {
+		$( 'tr:has(input.entry_approved)' ).find( 'a.toggleApproved' ).addClass( 'entry_approved' ).prop( 'title', gvGlobals.unapprove_title );
+	};
+
+	/**
+	 * Add approve/reject options to bulk edit dropdown
+	 */
+	self.addBulkAction = function() {
+		$( "#bulk_action, #bulk_action2" ).append( '<optgroup label="GravityView"><option value="approve-' + gvGlobals.form_id + '">' + gvGlobals.label_approve + '</option><option value="unapprove-' + gvGlobals.form_id + '">' + gvGlobals.label_disapprove + '</option></optgroup>' );
+	};
+
+	/**
+	 * Add an Approved column and header in the entries table
+	 */
+	self.addApprovedColumn = function() {
+
+		/**
+		 * inject approve/disapprove buttons into the first column of table
+		 */
+		$( 'thead th.check-column:eq(1), tfoot th.check-column:eq(1)' ).after( '<th scope="col" class="manage-column column-cb check-column gv-approve-column"><a href="' + gvGlobals.column_link + '" title="' + gvGlobals.column_title + '"></a></th>' );
+
+		/**
+		 * Add column for each entry
+		 */
+		$( 'td:has(img[src*="star"])' ).after( '<td class="gv-approve-column"><a href="#" class="toggleApproved" title="' + gvGlobals.approve_title + '"></a></td>' );
+
+	};
+
+	/**
+	 * Toggle a specific entry
+	 *
+	 * @param e The clicked entry event object
+	 * @returns {false}
+	 */
+	self.toggleApproved = function ( e ) {
+		e.preventDefault();
+
+		var entryID = $( this ).parent().parent().find( 'th input[type="checkbox"]' ).val();
+
+		$( this ).addClass( 'loading' );
+
+		if ( $( this ).hasClass( 'entry_approved' ) ) {
+			$( this ).prop( 'title', gvGlobals.approve_title );
+			self.updateApproved( entryID, 0, $( this ) );
+		} else {
+			$( this ).prop( 'title', gvGlobals.unapprove_title );
+			self.updateApproved( entryID, 'Approved', $( this ) );
+		}
+
+		return false;
+
+	};
+
+	/**
+	 * Generate a message and prepend it to the container
+	 *
+	 * @param message Text to display
+	 * @param messageClass (default: updated)
+	 * @param container Where to prepend the message (default: #lead_form)
+	 */
+	self.displayMessage = function ( message, messageClass, container ) {
+
+		self.hideMessage( container, true );
 
 		var messageBox = $( '<div class="message ' + messageClass + '" style="display:none;"><p>' + message + '</p></div>' );
 		$( messageBox ).prependTo( container ).slideDown();
@@ -27,9 +122,14 @@
 		}
 
 
-	}
+	};
 
-	function hideMessage( container, messageQueued ) {
+	/**
+	 * Hide a displayed message
+	 * @param container
+	 * @param messageQueued
+	 */
+	self.hideMessage = function ( container, messageQueued ) {
 
 		var messageBox = $( container ).find( '.message' );
 
@@ -41,11 +141,13 @@
 			} );
 		}
 
-	}
+	};
 
 
-	// Request entry approve (ajax)
-	function updateApproved( entryid, approved, $target ) {
+	/**
+	 * Update an entry status via AJAX
+ 	 */
+	self.updateApproved = function ( entryid, approved, $target ) {
 
 		var data = {
 			action: 'gv_update_approved',
@@ -66,51 +168,12 @@
 
 		return true;
 
-	}
-
-
-	$( document ).ready( function () {
-
-		// add actions to bulk select box
-		$( "#bulk_action, #bulk_action2" ).append( '<optgroup label="GravityView"><option value="approve-' + gvGlobals.form_id + '">' + gvGlobals.label_approve + '</option><option value="unapprove-' + gvGlobals.form_id + '">' + gvGlobals.label_disapprove + '</option></optgroup>' );
-
-		// display update message if any
-		if ( gvGlobals.bulk_message.length > 0 ) {
-			displayMessage( gvGlobals.bulk_message, 'updated', '#lead_form' );
-		}
-
-		// inject approve/disapprove buttons into the first column of table
-		$( 'thead th.check-column:eq(1), tfoot th.check-column:eq(1)' ).after( '<th scope="col" class="manage-column column-cb check-column gv-approve-column"><a href="' + gvGlobals.column_link + '" title="' + gvGlobals.column_title + '"></a></th>' );
-
-		$( 'td:has(img[src*="star"])' ).after( '<td class="gv-approve-column"><a href="#" class="toggleApproved" title="' + gvGlobals.approve_title + '"></a></td>' );
-
-		$( 'tr:has(input.entry_approved)' ).find( 'a.toggleApproved' ).addClass( 'entry_approved' ).prop( 'title', gvGlobals.unapprove_title );
-
-
-		$( '.toggleApproved' ).click( function ( e ) {
-			e.preventDefault();
-
-			var entryID = $( this ).parent().parent().find( 'th input[type="checkbox"]' ).val();
-
-			$( this ).addClass( 'loading' );
-
-			if ( $( this ).hasClass( 'entry_approved' ) ) {
-				$( this ).prop( 'title', gvGlobals.approve_title );
-				updateApproved( entryID, 0, $( this ) );
-			} else {
-				$( this ).prop( 'title', gvGlobals.unapprove_title );
-				updateApproved( entryID, 'Approved', $( this ) );
-			}
-
-			return false;
-
-		} );
+	};
 
 	// Only support approve/reject if the column is visible
 	if( gvGlobals.show_column * 1 === 1 ) {
 		$( self.init );
 	}
 
-	} );
 
 } (jQuery) );
