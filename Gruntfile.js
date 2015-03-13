e01832fdd18d1f8343401b4fb2cced008e58d2bf
Zack Katz <zack@katzwebservices.com>
2015-03-13 15:17:59 -0600

    Update Grunt SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 				files: [{
 		          expand: true,
 		          cwd: 'assets/css/scss',
-		          src: ['*.scss','!admin-merge-tags.scss','!admin-tooltips.scss'],
+		          src: ['*.scss','!admin-merge-tags.scss','!admin-tooltips.scss','!admin-metabox-panel.scss','!admin-metabox.scss'],
 		          dest: 'assets/css',
 		          ext: '.css'
 		      }]
