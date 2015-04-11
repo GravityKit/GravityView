0f997d7582227135c2850f9f22441b834d42868a
Zack Katz <zack@katzwebservices.com>
2015-04-10 22:28:36 -0600

    Restored default-widgets to fix A-Z Entries Extension
    It had a require call to the old file location

---
+++ includes/default-widgets.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * GravityView default widgets and generic widget class
+ *
+ * @deprecated 1.7.5
+ */
+
+include_once( GRAVITYVIEW_DIR .'includes/widgets/class-gravityview-widget.php' );
