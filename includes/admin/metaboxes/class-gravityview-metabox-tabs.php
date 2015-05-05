7a4653ed743318c7ac28376a9e70bbf986252b4a
Zack Katz <zack@katzwebservices.com>
2015-05-05 13:44:03 -0600

    Update docblock

--- includes/admin/metaboxes/class-gravityview-metabox-tabs.php
+++ includes/admin/metaboxes/class-gravityview-metabox-tabs.php
@@ -2,20 +2,27 @@
 
 /**
  * Stores each GravityView_Metabox_Tab instance
- * @link https://github.com/WebDevStudios/CMB2/blob/master/includes/CMB2_Boxes.php Thanks for the inspiration
+ * @since 1.8
+ * @see https://gist.github.com/zackkatz/6cc381bcf54849f2ed41 For example of adding a metabox
+ * @see https://github.com/WebDevStudios/CMB2/blob/master/includes/CMB2_Boxes.php Thanks for the inspiration
  */
 class GravityView_Metabox_Tabs {
 
 	/**
 	 * Array of all GravityView_Metabox_Tab objects
 	 * @var   array
-	 * @since 2.0.0
+	 * @since 1.8
 	 */
 	protected static $meta_boxes = array();
 
 	/**
 	 * Add a tab
+	 *
+	 * @since 1.8
+	 *
 	 * @param GravityView_Metabox_Tab $meta_box
+	 *
+	 * @return void
 	 */
 	public static function add( GravityView_Metabox_Tab $meta_box ) {
 		self::$meta_boxes[ $meta_box->id ] = $meta_box;
@@ -24,7 +31,11 @@ class GravityView_Metabox_Tabs {
 	/**
 	 * Remove a tab by tab ID
 	 *
+	 * @since 1.8
+	 *
 	 * @param string $meta_box_id
+	 *
+	 * @return void
 	 */
 	public static function remove( $meta_box_id ) {
 		if ( array_key_exists( $meta_box_id, self::$meta_boxes ) ) {
@@ -34,6 +45,8 @@ class GravityView_Metabox_Tabs {
 
 	/**
 	 * Get a tab by ID
+	 *
+	 * @since 1.8
 	 *
 	 * @param string $id
 	 *
@@ -50,7 +63,7 @@ class GravityView_Metabox_Tabs {
 	/**
 	 * Get array of all registered metaboxes
 	 *
-	 * @return array
+	 * @return array|GravityView_Metabox_Tab[] Empty array if no tabs, otherwise array of `GravityView_Metabox_Tab`s
 	 */
 	public static function get_all() {
 		return self::$meta_boxes;
