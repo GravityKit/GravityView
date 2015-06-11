b4d92a972e03df62bd35aae6a862289b1e772ddf
Luis Godinho <luis@gomo.pt>
2015-06-11 20:08:02 +0100

    Enable the Form Button conditional logic

--- includes/extensions/edit-entry/partials/form-buttons.php
+++ includes/extensions/edit-entry/partials/form-buttons.php
@@ -9,7 +9,7 @@
 	do_action( 'gravityview/edit-entry/publishing-action/before', $this->form, $this->entry, $this->view_id );
 
 	?>
-	<input class="btn btn-lg button button-large button-primary" type="submit" tabindex="4" value="<?php esc_attr_e( 'Update', 'gravityview'); ?>" name="save" />
+	<input id="gform_submit_button_<?php echo esc_attr( $this->form['id'] ); ?>" class="btn btn-lg button button-large gform_button button-primary" type="submit" tabindex="4" value="<?php esc_attr_e( 'Update', 'gravityview'); ?>" name="save" />
 
 	<a class="btn btn-sm button button-small" tabindex="5" href="<?php echo $back_link ?>"><?php esc_attr_e( 'Cancel', 'gravityview' ); ?></a>
 	<?php
