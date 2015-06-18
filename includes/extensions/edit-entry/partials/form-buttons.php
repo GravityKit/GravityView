8be22b66f07120a9b56def939bd398c5148e5171
Zack Katz <zack@katzwebservices.com>
2015-06-18 11:08:38 -0600

    Add CSS class to Edit Entry buttons
    @luistinygod Will this cause :rocket: ?

--- includes/extensions/edit-entry/partials/form-buttons.php
+++ includes/extensions/edit-entry/partials/form-buttons.php
@@ -9,9 +9,9 @@
 	do_action( 'gravityview/edit-entry/publishing-action/before', $this->form, $this->entry, $this->view_id );
 
 	?>
-	<input id="gform_submit_button_<?php echo esc_attr( $this->form['id'] ); ?>" class="btn btn-lg button button-large gform_button button-primary" type="submit" tabindex="4" value="<?php esc_attr_e( 'Update', 'gravityview'); ?>" name="save" />
+	<input id="gform_submit_button_<?php echo esc_attr( $this->form['id'] ); ?>" class="btn btn-lg button button-large gform_button button-primary gv-button-update" type="submit" tabindex="4" value="<?php esc_attr_e( 'Update', 'gravityview'); ?>" name="save" />
 
-	<a class="btn btn-sm button button-small" tabindex="5" href="<?php echo $back_link ?>"><?php esc_attr_e( 'Cancel', 'gravityview' ); ?></a>
+	<a class="btn btn-sm button button-small gv-button-cancel" tabindex="5" href="<?php echo $back_link ?>"><?php esc_attr_e( 'Cancel', 'gravityview' ); ?></a>
 	<?php
 
 	/**
