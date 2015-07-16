5a07e66ebaee320b68247b3298ef999bfb4cfe28
Luis Godinho <luis@gomo.pt>
2015-07-16 18:11:04 +0100

    New hook gravityview/edit_entry/cancel_link
    Added: New filter hook to customise the cancel Edit Entry link:
    "gravityview/edit_entry/cancel_link"

--- includes/extensions/edit-entry/partials/form-buttons.php
+++ includes/extensions/edit-entry/partials/form-buttons.php
@@ -1,7 +1,14 @@
 <div id="publishing-action">
 	<?php
 
-	$back_link = esc_url( remove_query_arg( array( 'page', 'view', 'edit' ) ) );
+    /**
+     * Customise the cancel button link
+     *
+     * @param $back_link string
+     *
+     * since 1.11.1
+     */
+    $back_link = apply_filters( 'gravityview/edit_entry/cancel_link', remove_query_arg( array( 'page', 'view', 'edit' ) ) );
 
 	/**
 	 * @since 1.5.1
@@ -11,7 +18,7 @@
 	?>
 	<input id="gform_submit_button_<?php echo esc_attr( $this->form['id'] ); ?>" class="btn btn-lg button button-large gform_button button-primary gv-button-update" type="submit" tabindex="4" value="<?php esc_attr_e( 'Update', 'gravityview'); ?>" name="save" />
 
-	<a class="btn btn-sm button button-small gv-button-cancel" tabindex="5" href="<?php echo $back_link ?>"><?php esc_attr_e( 'Cancel', 'gravityview' ); ?></a>
+	<a class="btn btn-sm button button-small gv-button-cancel" tabindex="5" href="<?php echo esc_url( $back_link ); ?>"><?php esc_attr_e( 'Cancel', 'gravityview' ); ?></a>
 	<?php
 
 	/**
--- readme.txt
+++ readme.txt
@@ -20,6 +20,9 @@ Beautifully display your Gravity Forms entries. Learn more on [GravityView.co](h
 
 == Changelog ==
 
+= =
+* Added: New filter hook to customise the cancel Edit Entry link: "gravityview/edit_entry/cancel_link"
+
 = 1.11 on July 15 =
 * Added: GravityView now updates WordPress user profiles when an entry is updated while using the Gravity Forms User Registration Add-on
 * Fixed: Removed User Registration Add-on validation when updating an entry
