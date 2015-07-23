9cd51705ea45c7db57b3f8c488572e0e4ff1f2ca
Zack Katz <zack@katzwebservices.com>
2015-07-22 20:13:17 -0600

    Update team

--- includes/class-admin-welcome.php
+++ includes/class-admin-welcome.php
@@ -578,18 +578,11 @@ class GravityView_Welcome {
 					<p><a href="https://katz.co">View Zack&rsquo;s website</a></p>
 				</div>
 
-				<div>
+				<div class="last-feature">
 					<h2>Luis Godinho</h2>
 					<h4 style="font-weight:0; margin-top:0">Developer &amp; Support</h4>
 					<p><img style="margin: 0 15px 10px 0;"  class="alignleft avatar" src="<?php echo plugins_url( 'assets/images/luis.jpg', GRAVITYVIEW_FILE ); ?>" width="94" height="94" />Luis is a WordPress developer passionate about WordPress. He is a co-founder and partner of GOMO, a digital agency located in Lisbon, Portugal.</p>
 					<p><a href="http://tinygod.pt">View Luis&rsquo;s website</a></p>
-				</div>
-
-				<div class="last-feature">
-					<h2>Gustavo Bordoni</h2>
-					<h4 style="font-weight:0; margin-top:0">Developer &amp; Support</h4>
-					<p><img style="margin: 0 15px 10px 0;"  class="alignleft avatar" src="<?php echo plugins_url( 'assets/images/gustavo.jpg', GRAVITYVIEW_FILE ); ?>" width="94" height="94" />Gustavo is a WordPress developer and an active WordPress community member. He is a frequent WordCamp speaker and is based in Rio de Janeiro, Brazil.</p>
-					<p><a href="http://bordoni.me">View Gustavo&rsquo;s website</a></p>
 				</div>
 
 			</div>
