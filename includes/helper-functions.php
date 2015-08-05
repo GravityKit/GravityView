28ecf298f391562877a3841073fa7408a1356494
Zack Katz <zack@katzwebservices.com>
2015-08-05 15:34:55 -0600

    Make gravityview_sanitize_html_class() available everywhere

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -46,6 +46,27 @@ function gravityview_get_permalink_query_args( $id = 0 ) {
 
 	return $args;
 }
+
+/**
+ * sanitize_html_class doesn't handle spaces (multiple classes). We remedy that.
+ * @uses sanitize_html_class
+ * @param  string|array      $classes Text or arrray of classes to sanitize
+ * @return string            Sanitized CSS string
+ */
+function gravityview_sanitize_html_class( $classes ) {
+
+	if( is_string( $classes ) ) {
+		$classes = explode(' ', $classes );
+	}
+
+	// If someone passes something not string or array, we get outta here.
+	if( !is_array( $classes ) ) { return $classes; }
+
+	$classes = array_map( 'sanitize_html_class' , $classes );
+
+	return implode( ' ', $classes );
+}
+
 /**
  * Get an image of our intrepid explorer friend
  * @return string HTML image tag with floaty's cute mug on it
