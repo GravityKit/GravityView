03c896755f012b003c00368aee400f3bc10823a9
Zack Katz <zack@katzwebservices.com>
2015-08-05 15:34:02 -0600

    Move some functions to connector-functions.php
    They didn't make sense in `helper-functions.php`.
    
    We'll need to move them again in the refactor, but for now, they make
    _more_ sense here.

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -1,55 +1,15 @@
 <?php
-
 /**
- * Theme function to get a GravityView view
- *
- * @access public
- * @param string $view_id (default: '')
- * @param array $atts (default: array())
- * @return string HTML of the output. Empty string if $view_id is empty.
+ * Functions that don't require GravityView or Gravity Forms but are used in the plugin to extend PHP and WP functions
+ * @since 1.12
  */
-function get_gravityview( $view_id = '', $atts = array() ) {
-	if( !empty( $view_id ) ) {
-		$atts['id'] = $view_id;
-		$args = wp_parse_args( $atts, GravityView_View_Data::get_default_args() );
-		$GravityView_frontend = GravityView_frontend::getInstance();
-		$GravityView_frontend->setGvOutputData( GravityView_View_Data::getInstance( $view_id ) );
-		$GravityView_frontend->set_context_view_id( $view_id );
-		$GravityView_frontend->set_entry_data();
-		return $GravityView_frontend->render_view( $args );
-	}
-	return '';
-}
-
-/**
- * Theme function to render a GravityView view
- *
- * @access public
- * @param string $view_id (default: '')
- * @param array $atts (default: array())
- * @return void
- */
-function the_gravityview( $view_id = '', $atts = array() ) {
-	echo get_gravityview( $view_id, $atts );
-}
-
-
-/**
- * Theme function to identify if it is a Single Entry View
- *
- * @since  1.5.4
- * @return bool|string False if not, single entry slug if true
- */
-function gravityview_is_single_entry() {
-	return GravityView_frontend::is_single_entry();
-}
 
 /**
  * Check whether a variable is not an empty string
  *
  * @see /templates/fields/product.php Used to check whether the product array is empty or not
  *
- * @since 1.11.3
+ * @since 1.12
  *
  * @param mixed $mixed Variable to check
  *
