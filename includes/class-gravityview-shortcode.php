7945836b11b2b69b5ce245ae6fa41f352d082e40
Zack Katz <zack@katzwebservices.com>
2015-08-14 17:18:00 -0600

    Add support for `detail` parameter passed to the [gravityview] shortcode
    `[gravityview detail="page_size"]` is `25` by default.
    
    @luistinygod what do you think about this?

--- includes/class-gravityview-shortcode.php
+++ includes/class-gravityview-shortcode.php
@@ -47,9 +47,49 @@ class GravityView_Shortcode {
 
 		do_action( 'gravityview_log_debug', __FUNCTION__ . ' $atts: ', $atts );
 
+		// Get details about the current View
+		if( !empty( $atts['detail'] ) ) {
+			return $this->get_view_detail( $atts['detail'] );
+		}
+
 		return GravityView_frontend::getInstance()->render_view( $atts );
 	}
 
+
+	/**
+	 * Display details for the current View
+	 *
+	 * @since 1.13
+	 *
+	 * @param string $detail The information requested about the current View. Accepts `total_entries`, `first_entry` (entry #), `last_entry` (entry #), and `page_size`
+	 *
+	 * @return string Detail information
+	 */
+	private function get_view_detail( $detail = '' ) {
+
+		$gravityview_view = GravityView_View::getInstance();
+		$return = '';
+
+		switch( $detail ) {
+			case 'total_entries':
+				$return = number_format_i18n( $gravityview_view->getTotalEntries() );
+				break;
+			case 'first_entry':
+				$paging = $gravityview_view->getPaginationCounts();
+				$return = empty( $paging ) ? '' : number_format_i18n( $paging['first'] );
+				break;
+			case 'last_entry':
+				$paging = $gravityview_view->getPaginationCounts();
+				$return = empty( $paging ) ? '' : number_format_i18n( $paging['last'] );
+				break;
+			case 'page_size':
+				$paging = $gravityview_view->getPaging();
+				$return = number_format_i18n( $paging['page_size'] );
+				break;
+		}
+
+		return $return;
+	}
 }
 
 new GravityView_Shortcode;
