32d5fa3a0ef13737c51f33a3678db263f654253e
Zack Katz <zack@katzwebservices.com>
2015-08-19 15:01:19 -0600

    Check if cache returned anything
    Fixes #452
    
    Dupe of
    https://github.com/katzwebservices/GravityView/commit/403c08b8a6b817fd74
    4cb212eda8035e8d88b307

--- includes/class-common.php
+++ includes/class-common.php
@@ -475,7 +475,7 @@ class GVCommon {
 			}
 		}
 
-		if ( class_exists( 'GFAPI' ) && ( is_numeric( $form_ids ) || is_array( $form_ids ) ) ) {
+		if ( is_null( $return ) && class_exists( 'GFAPI' ) && ( is_numeric( $form_ids ) || is_array( $form_ids ) ) ) {
 
 			$entries = GFAPI::get_entries( $form_ids, $criteria['search_criteria'], $criteria['sorting'], $criteria['paging'], $total );
 
