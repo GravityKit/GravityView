00b182b8e9ba43f270ec6bd3f921bed1b02e18d8
Zack Katz <zack@katzwebservices.com>
2015-09-15 12:34:54 -0600

    Fix the display of time fields
    See #492

--- includes/fields/time.php
+++ includes/fields/time.php
@@ -16,9 +16,9 @@ class GravityView_Field_Time extends GravityView_Field {
 			return $field_options;
 		}
 
-		add_filter('gravityview_date_format', array( $this, 'date_format' ) );
+		add_filter('gravityview_date_format', array( $this, '_filter_date_display_date_format' ) );
 		$this->add_field_support('date_display', $field_options );
-		remove_filter('gravityview_date_format', array( $this, 'date_format' ) );
+		remove_filter('gravityview_date_format', array( $this, '_filter_date_display_date_format' ) );
 
 		return $field_options;
 	}
@@ -51,14 +51,29 @@ class GravityView_Field_Time extends GravityView_Field {
 	}
 
 	/**
-	 * Get the default date format for a field based on the field ID and the time format setting
-	 * @return string PHP date format for the time
+	 * Modify the default PHP date formats used by the time field based on the field IDs and the field settings
+	 * @since 1.14
+	 * @return string
 	 */
-	function date_format() {
+	public function _filter_date_display_date_format() {
 
 		$time_format = $this->_get_time_format();
+		$field_id = $this->_field_id;
 
-		$field_id_array = explode( '.', $this->_field_id );
+		return self::date_format( $time_format, $field_id );
+	}
+
+	/**
+	 * Get the default date format for a field based on the field ID and the time format setting
+	 *
+	 * @param string $time_format The time format ("12" or "24"). Default: "12" {@since 1.14}
+	 * @param int $field_id The ID of the field. Used to figure out full time/hours/minutes/am/pm {@since 1.14}
+	 *
+	 * @return string PHP date format for the time
+	 */
+	static public function date_format( $time_format = '12', $field_id = 0 ) {
+
+		$field_id_array = explode( '.', $field_id );
 
 		$field_input_id = isset( $field_id_array[1] ) ? intval( $field_id_array[1] ) : 0;
 
