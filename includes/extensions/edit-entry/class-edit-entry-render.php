e5429020ae67b6fa9c48b0e77372ea6e0b0afd18
Luis Godinho <luis@gomo.pt>
2016-02-12 18:45:58 +0000

    support for edit post_image

--- includes/extensions/edit-entry/class-edit-entry-render.php
+++ includes/extensions/edit-entry/class-edit-entry-render.php
@@ -460,7 +460,51 @@ class GravityView_Edit_Entry_Render {
 
                     case 'post_image':
 
-                        $value = '';
+                        $input_name = 'input_' . $field_id;
+
+                        if ( !empty( $_FILES[ $input_name ]['name'] ) ) {
+
+                            $value = RGFormsModel::prepare_value( $form, $field, $value, $input_name, $this->entry['id'] );
+
+                            // is this field set as featured image ?
+                            if ( ! $field->postFeaturedImage ) {
+                                break;
+                            }
+
+                            $ary = ! empty( $value ) ? explode( '|:|', $value ) : array();
+                            $img_url = rgar( $ary, 0 );
+
+                            $img_title       = count( $ary ) > 1 ? $ary[1] : '';
+                            $img_caption     = count( $ary ) > 2 ? $ary[2] : '';
+                            $img_description = count( $ary ) > 3 ? $ary[3] : '';
+
+                            $image_meta = array(
+                                'post_excerpt' => $img_caption,
+                                'post_content' => $img_description,
+                            );
+
+                            //adding title only if it is not empty. It will default to the file name if it is not in the array
+                            if ( ! empty( $title ) ) {
+                                $image_meta['post_title'] = $img_title;
+                            }
+
+                            //todo: As soon as \GFFormsModel::media_handle_upload becomes a public method, move this call to \GFFormsModel::media_handle_upload and remove the hack from this class.
+                            $media_id = self::media_handle_upload( $img_url, $post_id, $image_meta );
+
+                            if ( $media_id ) {
+                                set_post_thumbnail( $post_id, $media_id );
+                            }
+
+                        } else {
+                            $ary = ! empty( $this->entry[ $field_id ] ) ? explode( '|:|', $this->entry[ $field_id ] ) : array();
+                            $img_url = rgar( $ary, 0 );
+
+                            $value = ! empty( $img_url ) ? $img_url . "|:|" . $value[ $field_id .'.1' ] . "|:|" . $value[ $field_id .'.4' ] . "|:|" . $value[ $field_id .'.7' ] : '';
+
+                            //todo: if feature, update the post image details
+                        }
+
+
                         break;
 
                 }
@@ -503,6 +547,145 @@ class GravityView_Edit_Entry_Render {
         }
 
     }
+
+    /**
+     * Copied function from Gravity Forms plugin \GFFormsModel::copy_post_image since the method is private.
+     * @todo: Remove this as soon as the method becomes available
+     * @param $url
+     * @param $post_id
+     * @return array|bool
+     */
+    private static function copy_post_image( $url, $post_id ) {
+        $time = current_time( 'mysql' );
+
+        if ( $post = get_post( $post_id ) ) {
+            if ( substr( $post->post_date, 0, 4 ) > 0 ) {
+                $time = $post->post_date;
+            }
+        }
+
+        //making sure there is a valid upload folder
+        if ( ! ( ( $upload_dir = wp_upload_dir( $time ) ) && false === $upload_dir['error'] ) ) {
+            return false;
+        }
+
+        $form_id = get_post_meta( $post_id, '_gform-form-id', true );
+
+        /**
+         * Filter the media upload location.
+         *
+         * @param array $upload_dir The current upload directory’s path and url.
+         * @param int $form_id The ID of the form currently being processed.
+         * @param int $post_id The ID of the post created from the entry currently being processed.
+         */
+        $upload_dir = gf_apply_filters( 'gform_media_upload_path', $form_id, $upload_dir, $form_id, $post_id );
+
+        if ( ! file_exists( $upload_dir['path'] ) ) {
+            if ( ! wp_mkdir_p( $upload_dir['path'] ) ) {
+                return false;
+            }
+        }
+
+        $name     = basename( $url );
+        $filename = wp_unique_filename( $upload_dir['path'], $name );
+
+        // the destination path
+        $new_file = $upload_dir['path'] . "/$filename";
+
+        // the source path
+        $y                = substr( $time, 0, 4 );
+        $m                = substr( $time, 5, 2 );
+        $target_root      = RGFormsModel::get_upload_path( $form_id ) . "/$y/$m/";
+        $target_root_url  = RGFormsModel::get_upload_url( $form_id ) . "/$y/$m/";
+        $upload_root_info = array( 'path' => $target_root, 'url' => $target_root_url );
+        $upload_root_info = gf_apply_filters( 'gform_upload_path', $form_id, $upload_root_info, $form_id );
+        $path             = str_replace( $upload_root_info['url'], $upload_root_info['path'], $url );
+
+        // copy the file to the destination path
+        if ( ! copy( $path, $new_file ) ) {
+            return false;
+        }
+
+        // Set correct file permissions
+        $stat  = stat( dirname( $new_file ) );
+        $perms = $stat['mode'] & 0000666;
+        @ chmod( $new_file, $perms );
+
+        // Compute the URL
+        $url = $upload_dir['url'] . "/$filename";
+
+        if ( is_multisite() ) {
+            delete_transient( 'dirsize_cache' );
+        }
+
+        $type = wp_check_filetype( $new_file );
+
+        return array( 'file' => $new_file, 'url' => $url, 'type' => $type['type'] );
+
+    }
+
+    /**
+     * Copied function from Gravity Forms plugin \GFFormsModel::media_handle_upload since the method is private.
+     * @todo: Remove this as soon as the method becomes available
+     * @param $url
+     * @param $post_id
+     * @param array $post_data
+     * @return bool|int
+     */
+    private static function media_handle_upload( $url, $post_id, $post_data = array() ) {
+
+        //WordPress Administration API required for the media_handle_upload() function
+        require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+        $name = basename( $url );
+
+        $file = self::copy_post_image( $url, $post_id );
+
+        if ( ! $file ) {
+            return false;
+        }
+
+        $name_parts = pathinfo( $name );
+        $name       = trim( substr( $name, 0, - ( 1 + strlen( $name_parts['extension'] ) ) ) );
+
+        $url     = $file['url'];
+        $type    = $file['type'];
+        $file    = $file['file'];
+        $title   = $name;
+        $content = '';
+
+        // use image exif/iptc data for title and caption defaults if possible
+        if ( $image_meta = @wp_read_image_metadata( $file ) ) {
+            if ( trim( $image_meta['title'] ) && ! is_numeric( sanitize_title( $image_meta['title'] ) ) ) {
+                $title = $image_meta['title'];
+            }
+            if ( trim( $image_meta['caption'] ) ) {
+                $content = $image_meta['caption'];
+            }
+        }
+
+        // Construct the attachment array
+        $attachment = array_merge(
+            array(
+                'post_mime_type' => $type,
+                'guid'           => $url,
+                'post_parent'    => $post_id,
+                'post_title'     => $title,
+                'post_content'   => $content,
+            ), $post_data
+        );
+
+        // Save the data
+        $id = wp_insert_attachment( $attachment, $file, $post_id );
+        if ( ! is_wp_error( $id ) ) {
+            wp_update_attachment_metadata( $id, wp_generate_attachment_metadata( $id, $file ) );
+        }
+
+        return $id;
+    }
+
+
+
 
     /**
      * Perform actions normally performed after updating a lead
@@ -776,7 +959,7 @@ class GravityView_Edit_Entry_Render {
         // If the form has been submitted, then we don't need to pre-fill the values,
         // Except for fileupload type and when a field input is overridden- run always!!
         if(
-            ( $this->is_edit_entry_submission() && 'fileupload' !== $field->type )
+            ( $this->is_edit_entry_submission() && !in_array( $field->type, array( 'fileupload', 'post_image' ) ) )
             && false === ( $gv_field && is_callable( array( $gv_field, 'get_field_input' ) ) )
             || ! empty( $field_content )
             || GFCommon::is_product_field( $field->type ) // Prevent product fields from appearing editable
@@ -815,6 +998,7 @@ class GravityView_Edit_Entry_Render {
         } else {
 	        $return = $field->get_field_input( $this->form, $field_value, $this->entry );
         }
+
 
 	    // If there was output, it's an error
 	    $warnings = ob_get_clean();
@@ -1078,6 +1262,7 @@ class GravityView_Edit_Entry_Render {
 
 
                 case 'fileupload' :
+                case 'post_image':
 
                     // in case nothing is uploaded but there are already files saved
                     if( !empty( $field->failed_validation ) && !empty( $field->isRequired ) && !empty( $value ) ) {
--- includes/fields/class-gravityview-field-post-image.php
+++ includes/fields/class-gravityview-field-post-image.php
@@ -37,6 +37,62 @@ class GravityView_Field_Post_Image extends GravityView_Field {
 		return $field_options;
 	}
 
+	public function get_field_input( $form, $value = '', $entry = null, GF_Field_Post_Image $field ) {
+
+		$form_id         = $form['id'];
+		$id       = (int) $field->id;
+		$field_id = 'input_' . $form_id . "_$id";
+
+		$class        = esc_attr( $field->size );
+
+		//hiding meta fields for admin
+		$hidden_style      = "style='display:none;'";
+		$title_style       = ! $field->displayTitle  ? $hidden_style : '';
+		$caption_style     = ! $field->displayCaption  ? $hidden_style : '';
+		$description_style = ! $field->displayDescription  ? $hidden_style : '';
+		$file_label_style  =  ! ( $field->displayTitle || $field->displayCaption || $field->displayDescription ) ? $hidden_style : '';
+
+		$hidden_class = $preview = '';
+
+		// get image file name if exists
+		$ary = ! empty( $value ) ? explode( '|:|', $value ) : array();
+		$url = count( $ary ) > 0 ? $ary[0] : '';
+		$path = parse_url( $url, PHP_URL_PATH );
+		$path_frags = explode( '/', $path );
+		$img_name = end( $path_frags );
+
+		$title       = count( $ary ) > 1 ? $ary[1] : '';
+		$caption     = count( $ary ) > 2 ? $ary[2] : '';
+		$description = count( $ary ) > 3 ? $ary[3] : '';
+
+		if ( !empty( $img_name ) ) {
+			$hidden_class     = ' gform_hidden';
+			$file_label_style = $hidden_style;
+			$preview          = "<span class='ginput_preview'><strong>" . esc_html( $img_name ) . "</strong> | <a href='javascript:;' onclick='gformDeleteUploadedFile( {$form_id}, {$id});'>" . __( 'delete', 'gravityview' ) . '</a></span>';
+		}
+
+		//in admin, render all meta fields to allow for immediate feedback, but hide the ones not selected
+		$file_label = ( $field->displayTitle || $field->displayCaption || $field->displayDescription ) ? "<label for='$field_id' class='ginput_post_image_file' $file_label_style>" . gf_apply_filters( array( 'gform_postimage_file', $form_id ), __( 'File', 'gravityview' ), $form_id ) . '</label>' : '';
+
+		$tabindex = $field->get_tabindex();
+
+		$upload = sprintf( "<span class='ginput_full'>{$preview}<input name='input_%d' id='%s' type='file' value='' class='%s' $tabindex />$file_label</span>", $id, $field_id,  esc_attr( $class . $hidden_class ) );
+
+		$tabindex = $field->get_tabindex();
+
+		$title_field = $field->displayTitle ? sprintf( "<span class='ginput_full ginput_post_image_title' $title_style><input type='text' name='input_%d.1' id='%s_1' value='%s' $tabindex /><label for='%s_1'>" . gf_apply_filters( array( 'gform_postimage_title', $form_id ), __( 'Title', 'gravityforms' ), $form_id ) . '</label></span>', $id, $field_id, $title,  $field_id ) : '';
+
+		$tabindex = $field->get_tabindex();
+
+		$caption_field = $field->displayCaption ? sprintf( "<span class='ginput_full ginput_post_image_caption' $caption_style><input type='text' name='input_%d.4' id='%s_4' value='%s' $tabindex /><label for='%s_4'>" . gf_apply_filters( array( 'gform_postimage_caption', $form_id ), __( 'Caption', 'gravityforms' ), $form_id ) . '</label></span>', $id, $field_id, $caption,  $field_id ) : '';
+
+		$tabindex = $field->get_tabindex();
+
+		$description_field = $field->displayDescription  ? sprintf( "<span class='ginput_full ginput_post_image_description' $description_style><input type='text' name='input_%d.7' id='%s_7' value='%s' $tabindex /><label for='%s_7'>" . gf_apply_filters( array( 'gform_postimage_description', $form_id ), __( 'Description', 'gravityforms' ), $form_id ) . '</label></span>', $id, $field_id, $description, $field_id ) : '';
+
+		return "<div class='ginput_complex ginput_container ginput_container_post_image'>" . $upload . $title_field . $caption_field . $description_field . '</div>';
+	}
+
 }
 
 new GravityView_Field_Post_Image;
