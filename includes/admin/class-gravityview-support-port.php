5749002f70d58b59c937af5ef6afb67ee2d965fc
Zack Katz <zack@katzwebservices.com>
2016-05-27 18:11:32 -0600

    Fixed: Support Port search not working on SSL
    https://secure.helpscout.net/conversation/207686698/5905/

--- includes/admin/class-gravityview-support-port.php
+++ includes/admin/class-gravityview-support-port.php
@@ -155,7 +155,6 @@ class GravityView_Support_Port {
 
 		$localization_data = array(
 			'contactEnabled' => (int)current_user_can( 'gravityview_contact_support' ), // @todo use GVCommon::has_cap() after merge
-			'protocol' => ( is_ssl() ? 'https' : 'http' ),
 			'data' => $data,
 			'translation' => $translation,
 		);
