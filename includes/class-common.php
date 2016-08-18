c75bdcf313e185dca6509e5201cb17e259c9e0ba
Zack Katz <zack@katzwebservices.com>
2016-08-18 12:56:35 -0600

    Add support for Admin multiselect fields
    This one's for @naomicbush
    
    I wanted to have `GravityView_FieldType_multiselect` extend the
    `GravityView_FieldType_select` class and have it check the class name
    to modify the output, but that file wasn't included yet, and it was all
    more complicated that it was worth.
    
    The multiselect wasn't saving properly, which turned out to be an issue
    with our `array_merge_recursive()` replacement function. Thanks to the
    fix provided here, it was sorted out:
    https://github.com/reduxframework/redux-framework/issues/2631#issuecomme
    nt-147772300

--- includes/class-common.php
+++ includes/class-common.php
@@ -1414,10 +1414,11 @@ class GVCommon {
 	 */
 	public static function array_merge_recursive_distinct( array &$array1, array &$array2 ) {
 		$merged = $array1;
-
-		foreach ( $array2 as $key => &$value )  {
+		foreach ( $array2 as $key => $value ) {
 			if ( is_array( $value ) && isset( $merged[ $key ] ) && is_array( $merged[ $key ] ) ) {
 				$merged[ $key ] = self::array_merge_recursive_distinct( $merged[ $key ], $value );
+			} else if ( is_numeric( $key ) && isset( $merged[ $key ] ) ) {
+				$merged[] = $value;
 			} else {
 				$merged[ $key ] = $value;
 			}
