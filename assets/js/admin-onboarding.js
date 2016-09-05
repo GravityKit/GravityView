85c07bdf7f151c4c990c0ef8559655daefacc30e
Zack Katz <zack@katzwebservices.com>
2016-09-05 12:06:48 -0600

    Add hopscotch.js
    I like it more than intro.js for some reason:
    https://linkedin.github.io/hopscotch/
    
    But intro.js has the "hint" pulsing dots, so I'll try that as well in
    the future.

--- assets/js/admin-onboarding.js
+++ assets/js/admin-onboarding.js
@@ -13,6 +13,7 @@
 
 jQuery( document ).ready( function( $ ) {
 
+	var $tabs = $( "#gv-view-configuration-tabs" );
 	var $directory_fields = $('#directory-active-fields, #directory-header-widgets, #directory-footer-widgets');
 	var $single_fields = $('#single-active-fields');
 
@@ -40,27 +41,84 @@ jQuery( document ).ready( function( $ ) {
 		return $('#gravityview_form_id').val();
 	}
 
+	function switch_to_tab( index ) {
+		$tabs.tabs( 'option', 'active', index );
+	}
+
 	$('#submitdiv').on('click', '#publish', function( e ) {
 
-		var message = [];
+		var active_tab_id = $tabs.tabs( "option", "active" );
+
+		var tour = {
+			id: "gv-configuration-errors"
+		};
+
+		var steps = [];
+
+		var calloutMgr = hopscotch.getCalloutManager();
 
 		if( ! has_active_fields( $directory_fields ) ) {
-			message.push( 'You have not configured any fields in the Multiple Entry layout. Click "+Add Field" to get started.');
+			steps.push( {
+				id: 'configure_multiple_entry',
+				target: 'ui-id-7',
+				title: 'You have not configured any fields in the Multiple Entry layout.',
+				content: '',
+				// When clicking Next, switch to Multiple Entries tab
+				onNext: function() {
+					switch_to_tab( 1 );
+				}
+			});
+
+			steps.push({
+				id: 'add_field_directory',
+				target: $('#directory-active-fields').find('.gv-add-field')[0],
+				title: 'Click "+Add Field" to get started.',
+				content: ''
+			});
 		}
 
 		if( ! has_active_fields( $single_fields ) ) {
-			message.push( 'You have not configured any fields in the Single Entry layout. Click "+Add Field" to get started.');
-			// Todo: Switch to Single Entry tab
+
+			if ( 1 !== active_tab_id ) {
+
+				steps.push( {
+					id: 'configure_single_entry',
+					target: 'ui-id-8',
+					placement: 'bottom',
+					title: 'You have not configured any fields in the Single Entry layout.',
+					content: '',
+					// When clicking Next, switch to Single Entry tab
+					onNext: function() {
+						switch_to_tab( 1 );
+					}
+				} );
+			}
+
+			steps.push({
+				id: 'add_field_single',
+				placement: 'bottom',
+				target: $('#single-active-fields').find('.gv-add-field')[0],
+				title: 'Click "+Add Field" to get started.',
+				content: ''
+			});
 		}
 
 		// Just getting started
-		message.push( has_active_fields( $directory_fields ) ? 'Has directory fields' : 'Does not have directory fields');
-		message.push( has_active_fields( $single_fields ) ? 'Has Single Entry fields' : 'Does not have Single Entry fields');
-		message.push( has_link_to_single_entry() ? 'Has link to single entry' : 'You have not configured a link to the Single Entry screen. Click a field setting "gear" icon, then check "Link to single" to configure.');
-		message.push( has_search_bar() ? 'Has search widget' : 'No search widget');
-		message.push( has_paging_links() ? 'Has pagination widget' : 'No pagination widget');
+		/*tour.steps.push( has_active_fields( $directory_fields ) ? 'Has directory fields' : 'Does not have directory fields');
+		tour.steps.push( has_active_fields( $single_fields ) ? 'Has Single Entry fields' : 'Does not have Single Entry fields');
+		tour.steps.push( has_link_to_single_entry() ? 'Has link to single entry' : 'You have not configured a link to the Single Entry screen. Click a field setting "gear" icon, then check "Link to single" to configure.');
+		tour.steps.push( has_search_bar() ? 'Has search widget' : 'No search widget');
+		tour.steps.push( has_paging_links() ? 'Has pagination widget' : 'No pagination widget');*/
 
-		return confirm( message.join( "\n" ) );
+
+		if( steps.length === 1 ) {
+			calloutMgr.createCallout( steps[0] );
+		} else {
+			tour.steps = steps;
+			hopscotch.startTour( tour );
+		}
+
+		return false; // confirm( message.join( "\n" ) );
 	});
 
 });
