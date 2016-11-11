7bdcf6f7260498dc59eabcc6a82c87428b333d28
Zack Katz <zack@katzwebservices.com>
2016-11-11 15:13:19 -0700

    Update version to 1.19-beta

--- gravityview.php
+++ gravityview.php
@@ -3,7 +3,7 @@
  * Plugin Name:       	GravityView
  * Plugin URI:        	https://gravityview.co
  * Description:       	The best, easiest way to display Gravity Forms entries on your website.
- * Version:          	1.18.1
+ * Version:          	1.19-beta
  * Author:            	GravityView
  * Author URI:        	https://gravityview.co
  * Text Domain:       	gravityview
@@ -61,7 +61,7 @@ register_deactivation_hook( __FILE__, array( 'GravityView_Plugin', 'deactivate' 
  */
 final class GravityView_Plugin {
 
-	const version = '1.18.1';
+	const version = '1.19-beta';
 
 	private static $instance;
 
