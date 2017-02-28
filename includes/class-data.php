5df50d5b4bb037a2491d3c94b87cbb0ca3e2705b
Gennady Kovshenin <gennady@kovshenin.com>
2017-02-28 13:12:01 +0500

    Fix #851 missing "offset" attribute default
    The [gravityview] shortcode uses `shortcode_atts` along with the
    defaults, which "sanitizes" all incoming attributes, limiting keys to
    only those found in the defaults.
    
    Added the missing "offset" default. As an added bonus (or not), this
    will show the offset setting in the Add View shortcode modal. The offset
    setting is not shown in the View Configuration, though.

--- includes/class-data.php
+++ includes/class-data.php
@@ -591,6 +591,14 @@ class GravityView_View_Data {
 				'value' => 25,
 				'show_in_shortcode' => true,
 			),
+			'offset' => array(
+				'label' 	=> __('Offset entries starting from', 'gravityview'),
+				'type' => 'number',
+				'class'	=> 'small-text',
+				'group'	=> 'default',
+				'value' => 0,
+				'show_in_shortcode' => true,
+			),
 			'lightbox' => array(
 				'label' => __( 'Enable lightbox for images', 'gravityview' ),
 				'type' => 'checkbox',
