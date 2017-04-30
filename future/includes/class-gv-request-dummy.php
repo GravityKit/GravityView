f7cf4bc76e2366fec18728b1c62f91a325cf83b6
Gennady Kovshenin <gennady@kovshenin.com>
2017-04-30 19:48:47 +0500

    Add `Entry::get_permalink` functionality
    This is set to replace all the get_entry_link functions exposed by the
    old API. Stateless stuff :metal:
    
    Also set test function visibility, just because.

--- future/includes/class-gv-request-dummy.php
+++ future/includes/class-gv-request-dummy.php
@@ -18,4 +18,20 @@ final class Dummy_Request extends Frontend_Request {
 	public function __construct() {
 		$this->views = new View_Collection();
 	}
+
+	/**
+	 * A quick check against the current $post global.
+	 *
+	 * This is a dummy method and should not be used.
+	 *  The Frontend_Request::is_view method should be used instead
+	 *  and is ready in the `future-render` branch.
+	 *
+	 * @internal
+	 *
+	 * @return boolean The $post is a View or not.
+	 */
+	public function is_view() {
+		global $post;
+		return $post ? get_post_type( $post ) == 'gravityview' : false;
+	}
 }
