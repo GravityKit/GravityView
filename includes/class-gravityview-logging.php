e57dae728cd41fd205e22ec83dcc573ceec45f63
Gennady Kovshenin <gennady@kovshenin.com>
2017-06-25 21:12:46 +0500

    Fix debug logging backtrace position
    Broken in 74708a2b6087b51f2400e951fd6b144b9c6fdebe

--- includes/class-gravityview-logging.php
+++ includes/class-gravityview-logging.php
@@ -104,11 +104,10 @@ final class GravityView_Logging {
 
 		$function = self::get_print_function();
 
-		$data['backtrace'] = function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '';
-
 		$error = array(
 			'message' => $message,
 			'data' => $data,
+			'backtrace' => function_exists( 'wp_debug_backtrace_summary' ) ? wp_debug_backtrace_summary( null, 3 ) : '',
 		);
 
 		if( !in_array( $error, self::$errors ) ) {

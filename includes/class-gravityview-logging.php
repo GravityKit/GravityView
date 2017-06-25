GravityKit/GravityView

commit 74708a2b6087b51f2400e951fd6b144b9c6fdebe

Fix speed issue due to wp_debug_backtrace_summary
Slow! :unamused:

includes/class-gravityview-logging.php
@@ -88,7 +88,6 @@ final class GravityView_Logging {
 		$notice = array(
 			'message' => $function( $message, true ),
 			'data' => $data,
-			'backtrace' => function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '',
 		);
 
 		if( !in_array( $notice, self::$notices ) ) {
@@ -105,10 +104,11 @@ final class GravityView_Logging {
 
 		$function = self::get_print_function();
 
+		$data['backtrace'] = function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '';
+
 		$error = array(
 			'message' => $message,
 			'data' => $data,
-			'backtrace' => function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '',
 		);
 
 		if( !in_array( $error, self::$errors ) ) {

tests/bootstrap.php
@@ -62,8 +62,8 @@ class GV_Unit_Tests_Bootstrap {
 
 		// Log debug if passed to `phpunit` like: `phpunit --debug --verbose`
 		if( in_array( '--debug', (array)$_SERVER['argv'], true ) && in_array( '--verbose', (array)$_SERVER['argv'], true ) ) {
-			tests_add_filter( 'gravityview_log_error', array( $this, 'test_print_log'), 10, 3 );
-			tests_add_filter( 'gravityview_log_debug', array( $this, 'test_print_log' ), 10, 3 );
+			tests_add_filter( 'gravityview_log_error', array( $this, 'test_print_log'), 10, 2 );
+			tests_add_filter( 'gravityview_log_debug', array( $this, 'test_print_log_backtrace' ), 10, 2 );
 		}
 
 		// load the WP testing environment
@@ -79,12 +79,26 @@ class GV_Unit_Tests_Bootstrap {
 		$this->install();
 	}
 
-	public function test_print_log(  $message = '', $data = null  ) {
+	/**
+	 * Alias of test_print_log, with backtrace
+	 * @since 1.21.6
+	 * @param $message
+	 * @param $data
+	 */
+	public function test_print_log_backtrace( $message, $data ) {
+		$this->test_print_log( $message, $data, true );
+	}
+
+	public function test_print_log(  $message = '', $data = null, $backtrace = false  ) {
 		$error = array(
 			'message' => $message,
 			'data' => $data,
-			'backtrace' => function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '',
 		);
+
+		if( $backtrace ) {
+			$error['backtrace'] = function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '';
+		}
+
 		fwrite( STDERR, print_r( $error, true ) );
 		fflush( STDERR );
 	}

commit e57dae728cd41fd205e22ec83dcc573ceec45f63

Fix debug logging backtrace position
Broken in 74708a2b6087b51f2400e951fd6b144b9c6fdebe

includes/class-gravityview-logging.php
@@ -104,11 +104,10 @@ final class GravityView_Logging {
 
 		$function = self::get_print_function();
 
-		$data['backtrace'] = function_exists('wp_debug_backtrace_summary') ? wp_debug_backtrace_summary( null, 3 ) : '';
-
 		$error = array(
 			'message' => $message,
 			'data' => $data,
+			'backtrace' => function_exists( 'wp_debug_backtrace_summary' ) ? wp_debug_backtrace_summary( null, 3 ) : '',
 		);
 
 		if( !in_array( $error, self::$errors ) ) {