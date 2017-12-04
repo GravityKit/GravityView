GravityKit/GravityView

commit 3404ef1dca4ae7800ec09d7fd3b5eb47ef0ed54b

Remove `gvid` from successful entry deletion redirect
Closes #933

includes/extensions/delete-entry/class-delete-entry.php
@@ -370,7 +370,7 @@ final class GravityView_Delete_Entry {
 				);
 			}
 
-			$redirect_to_base = esc_url_raw( remove_query_arg( array( 'action' ) ) );
+			$redirect_to_base = esc_url_raw( remove_query_arg( array( 'action', 'gvid' ) ) );
 			$redirect_to = add_query_arg( $messages, $redirect_to_base );
 
 			wp_safe_redirect( $redirect_to );

commit 12712179a91c586c685df5e55ee78aef6816b9f0

Fix undefined index error
Also fixes check for whether message is assigned to current View. Issue
introduced in 3404ef1

includes/extensions/delete-entry/class-delete-entry.php
@@ -698,7 +698,7 @@ final class GravityView_Delete_Entry {
 		}
 
 		// Entry wasn't deleted from current View
-		if( intval( $_GET['gvid'] ) !== intval( $current_view_id ) ) {
+		if( isset( $_GET['view_id'] ) && intval( $_GET['view_id'] ) !== intval( $current_view_id ) ) {
 			return;
 		}
 