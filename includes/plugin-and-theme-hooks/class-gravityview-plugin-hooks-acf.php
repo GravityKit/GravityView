4a998ffe8462f0b822d0e19a7229343152065413
Zack Katz <zack@katz.co>
2017-12-20 23:29:43 -0700

    Revert ACF code that broke things.
    Reverting the ACF code fixes issues for multiple customers. I probably
    shouldn't have changed anything in 1.22.1.
    
    https://gravityview.slack.com/archives/G0RA23YHE/p1513837683000080

--- includes/plugin-and-theme-hooks/class-gravityview-plugin-hooks-acf.php
+++ includes/plugin-and-theme-hooks/class-gravityview-plugin-hooks-acf.php
@@ -25,6 +25,11 @@ class GravityView_Plugin_Hooks_ACF extends GravityView_Plugin_and_Theme_Hooks {
 	protected $function_name = 'acf';
 
 	/**
+	 * @since 1.22.2.1
+	 */
+	protected $class_name = 'acf';
+
+	/**
 	 * @since 1.16.5
 	 */
 	protected function add_hooks() {
@@ -44,18 +49,11 @@ class GravityView_Plugin_Hooks_ACF extends GravityView_Plugin_and_Theme_Hooks {
 	function add_meta_keys_from_post( $meta_keys = array(), $post_id = 0 ) {
 
 		// Can never be too careful: double-check that ACF is active and the function exists
-		if ( ! function_exists( 'get_field_objects' ) || ! class_exists('acf_field_functions' ) ) {
+		if ( ! function_exists( 'get_field_objects' ) ) {
 			return $meta_keys;
 		}
 
-		$acf_field_functions = new acf_field_functions;
-
-		remove_action('acf/format_value', array( $acf_field_functions, 'format_value'), 5 );
-
 		$acf_keys = get_field_objects( $post_id, array( 'load_value' => false ) );
-
-		// Restore existing filters added by format_value
-		add_action('acf/format_value', array( $acf_field_functions, 'format_value'), 5, 3 );
 
 		if( $acf_keys ) {
 			return array_merge( array_keys( $acf_keys ), $meta_keys );
