GravityKit/GravityView

commit 72e40ea4b6c1f1ba14ab1a9223a97fe87b0a2e1e

Much less rewrite polution for /entry/ endpoint
Just one rewrite is enough, no need to pollute every single post type,
taxonomy, etc.

tests/unit-tests/GravityView_Future_Test.php
@@ -123,8 +123,8 @@ class GVFuture_Test extends GV_UnitTestCase {
 	 * @covers \GV\Entry::get_endpoint_name()
 	 */
 	public function test_entry_endpoint_rewrite_name() {
-		$entry_enpoint = array_filter( $GLOBALS['wp_rewrite']->endpoints, function( $endpoint ) {
-			return $endpoint === array( EP_ALL, 'entry', 'entry' );
+		$entry_enpoint = array_filter( $GLOBALS['wp_rewrite']->extra_rules_top, function( $endpoint ) {
+			return $endpoint === 'index.php?gravityview=$matches[1]&entry=$matches[3]';
 		} );
 
 		$this->assertNotEmpty( $entry_enpoint, 'Single Entry endpoint not registered.' );

future/includes/class-gv-entry.php
@@ -42,14 +42,18 @@ abstract class Entry {
 	public static function add_rewrite_endpoint() {
 		global $wp_rewrite;
 
+		$slug = apply_filters( 'gravityview_slug', 'view' );
 		$endpoint = self::get_endpoint_name();
+		$rule = array( sprintf( '%s/([^/]+)/%s(/(.*))?/?$', $slug, $endpoint ), sprintf( 'index.php?gravityview=$matches[1]&%s=$matches[3]', $endpoint ), 'top' );
 
-		/** Let's make sure the endpoint array is not polluted. */
-		if ( in_array( array( EP_ALL, $endpoint, $endpoint ), $wp_rewrite->endpoints ) ) {
-			return;
+		add_filter( 'query_vars', function( $query_vars ) use ( $endpoint ) {
+			$query_vars[] = $endpoint;
+			return $query_vars;
+		} );
+
+		if ( ! isset( $wp_rewrite->extra_rules_top[ $rule[0] ] ) ) {
+			call_user_func_array( 'add_rewrite_rule', $rule );
 		}
-
-		add_rewrite_endpoint( $endpoint, EP_ALL );
 	}
 
 	/**

commit 9441385b5918cf02ed0367107e610f442b3802d6

Add tests for embedded View entry rewrites
Regression in 72e40ea4b6c1f1ba14ab1a9223a97fe87b0a2e1e

tests/unit-tests/GravityView_Future_Test.php
@@ -7468,6 +7468,34 @@ class GVFuture_Test extends GV_UnitTestCase {
 
 		$this->_reset_context();
 	}
+
+	public function test_entry_rewrite_rule() {
+		$this->_reset_context();
+		$form = $this->factory->form->create_and_get();
+		$entry = $this->factory->entry->create_and_get( array( 'form_id' => $form['id'] ) );
+		$view = $this->factory->view->create_and_get( array( 'form_id' => $form['id'] ) );
+
+		$form = \GV\GF_Form::by_id( $form['id'] );
+		$entry = \GV\GF_Entry::by_id( $entry['id'] );
+		$view = \GV\View::from_post( $view );
+
+		$request = new \GV\Frontend_Request();
+
+		global $post;
+
+		$post = $this->factory->post->create_and_get( array( 'post_content' => '[gravityview id="' . $view->ID . '"]' ) );
+
+		update_option( 'permalink_structure', '/%postname%' );
+		$this->assertEquals( get_permalink( $post->ID ) . '/entry/' . $entry->ID . '/', $url = $entry->get_permalink( $view, $request ) );
+
+		$this->go_to( $url );
+
+		$this->assertEquals( $entry->ID, get_query_var( 'entry' ) );
+
+		update_option( 'permalink_structure', '' );
+
+		$this->_reset_context();
+	}
 }
 
 class GVFutureTest_Extension_Test_BC extends GravityView_Extension {