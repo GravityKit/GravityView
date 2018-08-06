a5c225efd206b3bedd6cc99725c0d8c39c73f683
Zack Katz <zack@katz.co>
2018-08-06 14:31:32 -0600

    build on this!
    @mrcasual

---
+++ tests/unit-tests/GravityView_Admin_Test.php
@@ -0,0 +1,17 @@
+<?php
+
+defined( 'DOING_GRAVITYVIEW_TESTS' ) || exit;
+
+/**
+ * @group cache
+ */
+class GravityView_Admin_Test extends GV_UnitTestCase {
+
+	/**
+	 * Modify here!
+	 */
+	public function test_admin_field_filter() {
+
+		$this->assertTrue( true );
+	}
+}
