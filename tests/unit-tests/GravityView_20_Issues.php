a3f4816b0f6a26d44f693f78c6b203f9f4ffee4e
Gennady Kovshenin <gennady@kovshenin.com>
2018-08-06 03:24:27 +0300

    Test for #1137

--- tests/unit-tests/GravityView_20_Issues.php
+++ tests/unit-tests/GravityView_20_Issues.php
@@ -381,4 +381,89 @@ class GV_20_Issues_Test extends GV_UnitTestCase {
 		$this->assertContains( 'Search Entries', $future );
 		$this->assertContains( 'Here we go again! <b>Now</b>', $future );
 	}
+
+	/**
+	 * https://github.com/gravityview/GravityView/issues/1137
+	 */
+	public function test_view_in_view_embedded() {
+		$this->_reset_context();
+		$form         = $this->factory->form->import_and_get( 'simple.json' );
+		$another_form = $this->factory->form->import_and_get( 'simple.json' );
+
+		$entry = $this->factory->entry->create_and_get( array(
+			'status' => 'active',
+			'form_id' => $form['id'],
+			'1' => 'this is an entry',
+		) );
+
+		$another_entry = $this->factory->entry->create_and_get( array(
+			'status' => 'active',
+			'form_id' => $another_form['id'],
+			'1' => 'this is an another entry',
+		) );
+
+		$view = $this->factory->view->create_and_get( array(
+			'form_id' => $form['id'],
+			'fields' => array(
+				'directory_table-columns' => array(
+					wp_generate_password( 4, false ) => array(
+						'id' => 'custom',
+						'content' => 'Embed this view!',
+					),
+				),
+				'single_table-columns' => array(
+					wp_generate_password( 4, false ) => array(
+						'id' => 'custom',
+						'content' => 'Embed this view!',
+					),
+				),
+			),
+		) );
+
+		$another_view = $this->factory->view->create_and_get( array(
+			'form_id' => $another_form['id'],
+			'fields' => array(
+				'directory_table-columns' => array(
+					wp_generate_password( 4, false ) => array(
+						'id' => 'custom',
+						'content' => '[gravityview id="' . $view->ID . '"]',
+					),
+				),
+				'single_table-columns' => array(
+					wp_generate_password( 4, false ) => array(
+						'id' => 'custom',
+						'content' => '[gravityview id="' . $view->ID . '"]',
+					),
+				),
+			),
+		) );
+
+		$form          = \GV\GF_Form::by_id( $form['id'] );
+		$entry         = \GV\GF_Entry::by_id( $entry['id'] );
+		$view          = \GV\View::from_post( $view );
+		$another_form  = \GV\GF_Form::by_id( $another_form['id'] );
+		$another_entry = \GV\GF_Entry::by_id( $another_entry['id'] );
+		$another_view  = \GV\View::from_post( $another_view );
+
+		$future = new \GV\Shortcodes\gravityview();
+
+		gravityview()->request = new \GV\Mock_Request();
+		gravityview()->request->returns['is_view'] = false;
+
+		$args = array(
+			'id' => $another_view->ID,
+		);
+
+		$this->assertContains( 'Embed this view', $future->callback( $args ) );
+
+		global $post;
+
+		$post = $this->factory->post->create_and_get( array( 'post_content' => '[gravityview id="' . $another_view->ID . '"]' ) );
+
+		gravityview()->request->returns['is_entry'] = $another_entry;
+
+		$this->assertContains( 'Embed this view', $future->callback( $args ) );
+
+		$this->_reset_context();
+	}
 }
