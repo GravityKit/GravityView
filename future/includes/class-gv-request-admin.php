31b7885f3debb4b08f2e52a98f6bf906efadbadb
Zack Katz <zack@katz.co>
2018-08-07 13:30:49 -0600

    Restore is_admin() for extensions, added tests
    Got overwritten in 2928712

--- future/includes/class-gv-request-admin.php
+++ future/includes/class-gv-request-admin.php
@@ -23,6 +23,10 @@ class Admin_Request extends Request {
 			return false;
 		}
 
+		if( ! function_exists( '\get_current_screen' ) ) {
+			return false;
+		}
+
 		/**
 		 * Regular check.
 		 */
@@ -37,15 +41,17 @@ class Admin_Request extends Request {
 		 */
 		$is_page = false;
 
-		if ( function_exists( '\get_current_screen' ) || function_exists( 'get_current_screen' ) ) {
-			if ( ( $current_screen = \get_current_screen() ) && $current_screen->post_type == 'gravityview' ) {
-				if ( $is_gv_edit_list = $current_screen->base == 'edit' ) {
-					$is_page = 'views';
-				} else if ( $is_gv_edit_single = $current_screen->base == 'post' ) {
-					$is_page = 'single';
-				} else if ( $is_gv_settings = $current_screen->id == 'gravityview_page_gravityview_settings' ) {
-					$is_page = 'settings';
-				}
+		$current_screen = \get_current_screen();
+
+		if ( $current_screen && $current_screen->post_type == 'gravityview' ) {
+			if ( $is_gv_edit_list = 'edit' === $current_screen->base ) {
+				$is_page = 'views';
+			} else if ( $is_gv_edit_single = 'post' === $current_screen->base ) {
+				$is_page = 'single';
+			} else if ( $is_gv_settings = 'gravityview_page_gravityview_settings' === $current_screen->id ) {
+				$is_page = 'settings';
+			} else if( $is_extensions = 'gravityview_page_gv-admin-installer' === $current_screen->id ) {
+				$is_page = 'extensions';
 			}
 		}
 
