6e3ee6b42d2d14ef5ffca43a31068a04a5a5121c
Zack Katz <zack@katz.co>
2018-08-13 16:44:09 -0600

    Move the "unapprove" setting next to "user edit"

--- includes/admin/metaboxes/views/view-settings.php
+++ includes/admin/metaboxes/views/view-settings.php
@@ -35,6 +35,11 @@ $current_settings = gravityview_get_template_settings( $post->ID );
 	GravityView_Render_Settings::render_setting_row( 'user_edit', $current_settings );
 
 	/**
+	 * @since 2.1
+	 */
+	GravityView_Render_Settings::render_setting_row( 'unapprove_edit', $current_settings );
+
+	/**
 	 * @since  1.5.1
 	 */
 	GravityView_Render_Settings::render_setting_row( 'user_delete', $current_settings );
@@ -54,11 +59,6 @@ $current_settings = gravityview_get_template_settings( $post->ID );
 	if ( gravityview()->plugin->supports( \GV\Plugin::FEATURE_REST ) && ( gravityview()->plugin->settings->get( 'rest_api' ) !== '1' ) ) {
 		GravityView_Render_Settings::render_setting_row( 'rest_enable', $current_settings );
 	}
-
-	/**
-	 * @since 2.0
-	 */
-	GravityView_Render_Settings::render_setting_row( 'unapprove_edit', $current_settings );
 
 	do_action( 'gravityview_admin_directory_settings', $current_settings );
 
