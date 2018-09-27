256a2d5668ca1ace0b7e386a4483afed26d9b7a9
Gennady Kovshenin <gennady@kovshenin.com>
2018-09-27 17:26:44 +0300

    Fix CSV output for partial field output
    Support for generic subfield output
    See b097db5cade54eaf99003fbca7faf6f4b0be0546

--- templates/fields/field-csv.php
+++ templates/fields/field-csv.php
@@ -5,4 +5,8 @@
  * @global \GV\Template_Context $gravityview
  * @since 2.0
  */
-echo $gravityview->display_value;
+$field_id = $gravityview->field->ID;
+$display_value = $gravityview->display_value;
+$entry = $gravityview->entry->as_entry();
+
+echo gravityview_get_field_value( $entry, $field_id, $display_value );
