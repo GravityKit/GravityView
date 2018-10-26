GravityKit/GravityView

commit 8c6f251b7b54532e940eb9ded5a5d45072828faa

Only generate <img> tag when "Display as Link" is not checked

includes/fields/class-gravityview-field-fileupload.php
@@ -142,7 +142,7 @@ class GravityView_Field_FileUpload extends GravityView_Field {
 		// Process each file path
 		foreach ( $file_paths as $file_path ) {
 
-			$text = $rendered = null;
+			$rendered = null;
 
 			// If the site is HTTPS, use HTTPS
 			if ( function_exists('set_url_scheme') ) {
@@ -163,6 +163,7 @@ class GravityView_Field_FileUpload extends GravityView_Field {
 			$is_secure = false;
 			$insecure_file_path = $file_path;
 			$secure_file_path = $field->get_download_url( $file_path );
+			$text = $basename;
 
 			if ( $secure_file_path !== $file_path ) {
 				$basename = basename( $secure_file_path );
@@ -221,6 +222,7 @@ class GravityView_Field_FileUpload extends GravityView_Field {
 
 			// Video
 			} else if ( in_array( $extension, wp_get_video_extensions() ) ) {
+
 				if ( shortcode_exists( 'video' ) ) {
 
 					/**
@@ -260,46 +262,50 @@ class GravityView_Field_FileUpload extends GravityView_Field {
 
 			// PDF
 			} else if ( $extension === 'pdf' ) {
+
 				// PDF needs to be displayed in an IFRAME
 				$file_path = add_query_arg( array( 'TB_iframe' => 'true' ), $file_path );
 
 			// Images
 			} else if ( in_array( $extension, array( 'jpg', 'jpeg', 'jpe', 'gif', 'png' ) ) ) {
 
-				$image_atts = array(
-					'src' => $file_path,
-					'class' => 'gv-image gv-field-id-'.$field_settings['id'],
-					'alt' => $field_settings['label'],
-					'width' => ( $is_single ? NULL : 250 )
-				);
+				if ( empty( $field_settings['link_to_file'] ) ) {
 
-				if ( $is_secure ) {
-					$image_atts['validate_src'] = false;
-				}
-
-				/**
-				 * Modify the default image attributes for uploaded images
-				 * @since 2.0
-				 * @see GravityView_Image For the available attributes
-				 * @param array $image_atts
-				 */
-				$image_atts = apply_filters( 'gravityview/fields/fileupload/image_atts', $image_atts );
-
-				$image = new GravityView_Image( $image_atts );
-
-				$entry_slug = GravityView_API::get_entry_slug( $entry['id'], $entry );
-
-				$text = $image->html();
-
-				if ( $lightbox && empty( $field_settings['show_as_link'] ) ) {
-					$lightbox_link_atts = array(
-						'rel' => sprintf( "%s-%s", $gv_class, $entry_slug ),
-						'class' => 'thickbox',
+					$image_atts = array(
+						'src'   => $insecure_file_path,
+						'class' => 'gv-image gv-field-id-' . $field_settings['id'],
+						'alt'   => $field_settings['label'],
+						'width' => ( $is_single ? null : 250 )
 					);
 
-					$rendered = gravityview_get_link( $file_path, $image->html(), $lightbox_link_atts );
-				} else {
-					$rendered = $image->html();
+					if ( $is_secure ) {
+						$image_atts['validate_src'] = false;
+					}
+
+					/**
+					 * Modify the default image attributes for uploaded images
+					 *
+					 * @since 2.0
+					 * @see GravityView_Image For the available attributes
+					 *
+					 * @param array $image_atts
+					 */
+					$image_atts = apply_filters( 'gravityview/fields/fileupload/image_atts', $image_atts );
+
+					$image = new GravityView_Image( $image_atts );
+
+					$entry_slug = GravityView_API::get_entry_slug( $entry['id'], $entry );
+
+					if ( $lightbox && empty( $field_settings['show_as_link'] ) ) {
+						$lightbox_link_atts = array(
+							'rel'   => sprintf( "%s-%s", $gv_class, $entry_slug ),
+							'class' => 'thickbox',
+						);
+
+						$rendered = gravityview_get_link( $file_path, $image->html(), $lightbox_link_atts );
+					} else {
+						$rendered = $image->html();
+					}
 				}
 			}
 

commit 091da6c15b98a3a344f4a218656f252a57befa29

Fix the rest of the tests for fileupload
- Addresses 56aed31b7ea2fc154118f9dc295862670c431dd6
- Bug introduced in 8c6f251b7b54532e940eb9ded5a5d45072828faa
- And other issues from the comprehensive tests

includes/fields/class-gravityview-field-fileupload.php
@@ -261,7 +261,7 @@ class GravityView_Field_FileUpload extends GravityView_Field {
 			// Images
 			} else if ( in_array( $extension, array( 'jpg', 'jpeg', 'jpe', 'gif', 'png' ) ) ) {
 				$image_atts = array(
-					'src'   => $insecure_file_path,
+					'src'   => $file_path,
 					'class' => 'gv-image gv-field-id-' . $field_settings['id'],
 					'alt'   => $field_settings['label'],
 					'width' => ( $is_single ? null : 250 )

tests/unit-tests/GravityView_20_Issues.php
@@ -592,7 +592,7 @@ class GV_20_Issues_Test extends GV_UnitTestCase {
 
 		$this->assertContains( $expected, $output );
 		$this->assertContains( '<audio class="wp-audio-shortcode', $output );
-		$this->assertContains( '<source type="audio/mpeg" src="' . esc_attr( $files[1] ) . '?_=', $output );
+		$this->assertContains( '<source type="audio/mpeg" src="' . esc_attr( $files[1] ) . '&_=', $output );
 		$this->assertContains( '" /><a href="' . esc_attr( $files[1] ). '">' . esc_html( $files[1] ) .  '</a></audio></li></ul>', $output );
 	}
 

tests/unit-tests/GravityView_Future_Test.php
@@ -3716,7 +3716,7 @@ class GVFuture_Test extends GV_UnitTestCase {
 		$field->update_configuration( array( $display_as_url => false ) );
 		$field->update_configuration( array( $link_to_entry => false ) );
 
-		$expected = '<a class="thickbox" href="http://one.jpg" rel="gv-field-' . $form->ID . '-5-' . $form->ID . '">';
+		$expected = '<a class="thickbox" href="http://one.jpg" rel="gv-field-' . $form->ID . '-5-' . $entry->ID . '">';
 			$expected .= '<img src="http://one.jpg" width="250" class="gv-image gv-field-id-5" />';
 		$expected .= '</a>';
 		$this->assertEquals( $expected, $renderer->render( $field, $view, $form, $entry, $request ) );
@@ -3744,7 +3744,7 @@ class GVFuture_Test extends GV_UnitTestCase {
 		$field->update_configuration( array( $display_as_url => false ) );
 		$field->update_configuration( array( $link_to_entry => false ) );
 
-		$expected = '<a class="thickbox" href="http://one.jpg" rel="gv-field-' . $form->ID . '-5-' . $form->ID . '">';
+		$expected = '<a class="thickbox" href="http://one.jpg" rel="gv-field-' . $form->ID . '-5-' . $entry->ID . '">';
 			$expected .= '<img src="http://one.jpg" width="250" class="gv-image gv-field-id-5" />';
 		$expected .= '</a>';
 		$this->assertEquals( $expected, $renderer->render( $field, $view, $form, $entry, $request ) );
@@ -3752,7 +3752,7 @@ class GVFuture_Test extends GV_UnitTestCase {
 		$field->update_configuration( array( $display_as_url => true ) );
 		$field->update_configuration( array( $link_to_entry => false ) );
 
-		$expected = '<a class="thickbox" href="http://one.jpg" rel="gv-field-' . $form->ID . '-5-' . $form->ID . '"><img src="http://one.jpg" width="250" class="gv-image gv-field-id-5" /></a>';
+		$expected = '<a class="thickbox" href="http://one.jpg" rel="gv-field-' . $form->ID . '-5-' . $entry->ID . '"><img src="http://one.jpg" width="250" class="gv-image gv-field-id-5" /></a>';
 		$this->assertEquals( $expected, $renderer->render( $field, $view, $form, $entry, $request ) );
 
 		$field->update_configuration( array( $display_as_url => false ) );