c4f152c801be226cc1bf759eb6bbe3543e93ea32
Zack Katz <zack@katz.co>
2018-11-27 17:27:05 -0700

    Move settings to View Settings, until we re-arrange tabs

--- includes/admin/metaboxes/views/view-settings.php
+++ includes/admin/metaboxes/views/view-settings.php
@@ -40,6 +40,16 @@ $current_settings = gravityview_get_template_settings( $post->ID );
 	GravityView_Render_Settings::render_setting_row( 'unapprove_edit', $current_settings );
 
 	/**
+	 * @since 2.2
+	 */
+	GravityView_Render_Settings::render_setting_row( 'edit_redirect', $current_settings );
+
+	/**
+	 * @since 2.2
+	 */
+	GravityView_Render_Settings::render_setting_row( 'edit_redirect_url', $current_settings );
+
+	/**
 	 * @since  1.5.1
 	 */
 	GravityView_Render_Settings::render_setting_row( 'user_delete', $current_settings );
