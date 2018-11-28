0cf687bf63b1248c891a1103b2428506ca1a1654
Zack Katz <zack@katz.co>
2018-11-27 17:49:09 -0700

    Start to the View settings
    @soulseekah What am I doing wrong here? I'm getting this error:
    
    ![](https://share.gravityview.co/5lqjf6+)

--- includes/admin/metaboxes/views/sort-filter.php
+++ includes/admin/metaboxes/views/sort-filter.php
@@ -26,11 +26,21 @@ $current_settings = gravityview_get_template_settings( $post->ID );
 	 */
 	GravityView_Render_Settings::render_setting_row( 'sort_columns', $current_settings );
 
-	$sort_fields_input = '<select name="template_settings[sort_field]" id="gravityview_sort_field">'.gravityview_get_sortable_fields( $curr_form, $current_settings['sort_field'] ).'</select>';
+	$sort_fields_input = '<select name="template_settings[sort_field][]" id="gravityview_sort_field">%s</select>';
 
-	GravityView_Render_Settings::render_setting_row( 'sort_field', $current_settings, $sort_fields_input );
+	if ( is_array( $current_settings['sort_field'] ) ) {
+		$primary_sort_fields = gravityview_get_sortable_fields( $curr_form, $current_settings['sort_field'][0] );
+		$secondary_sort_fields = gravityview_get_sortable_fields( $curr_form, $current_settings['sort_field'][1] );
+	} else {
+		$primary_sort_fields = $secondary_sort_fields = gravityview_get_sortable_fields( $curr_form, $current_settings['sort_field'] );
+    }
+
+
+	GravityView_Render_Settings::render_setting_row( 'sort_field', $current_settings, sprintf( $sort_fields_input, $primary_sort_fields ) );
 
 	GravityView_Render_Settings::render_setting_row( 'sort_direction', $current_settings );
+
+	GravityView_Render_Settings::render_setting_row( 'sort_field_2', $current_settings, sprintf( $sort_fields_input, $secondary_sort_fields ) );
 
 
 	// End Sort fields
