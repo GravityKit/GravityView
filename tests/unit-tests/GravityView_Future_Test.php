0c2ed00a811becaa601cbb162316cffd4516a961
Gennady Kovshenin <gennady@kovshenin.com>
2019-03-07 18:51:42 +0300

    Fix bumped version tests
    Regression via 3c27c65b1cbf1a48cbe487db3baf54187471ca0b

--- tests/unit-tests/GravityView_Future_Test.php
+++ tests/unit-tests/GravityView_Future_Test.php
@@ -83,7 +83,7 @@ class GVFuture_Test extends GV_UnitTestCase {
 		/** Simulate various other conditions, including failure conditions. */
 		$GLOBALS['GRAVITYVIEW_TESTS_PHP_VERSION_OVERRIDE'] = '7.0.99-hhvm';
 		$GLOBALS['GRAVITYVIEW_TESTS_WP_VERSION_OVERRIDE'] = '4.8-alpha-39901';
-		$GLOBALS['GRAVITYVIEW_TESTS_GF_VERSION_OVERRIDE'] = '2.1.2.3-alpha';
+		$GLOBALS['GRAVITYVIEW_TESTS_GF_VERSION_OVERRIDE'] = '2.3.2.3-alpha';
 		$this->assertTrue( gravityview()->plugin->is_compatible_php() );
 		$this->assertTrue( gravityview()->plugin->is_compatible_wordpress() );
 		$this->assertTrue( gravityview()->plugin->is_compatible_gravityforms() );
