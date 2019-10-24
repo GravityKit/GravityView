c4d58a994addb8194f5770d01198ee4a93bd3b3e
Gennady Kovshenin <gennady@kovshenin.com>
2019-10-24 18:10:41 +0300

    More 5.6 test fixes

--- templates/fields/field-edit_link-html.php
+++ templates/fields/field-edit_link-html.php
@@ -16,7 +16,8 @@ if ( ! $gravityview->field->form_id || ! ( $form = GFAPI::get_form( $gravityview
 }
 
 if ( $gravityview->entry->is_multi() ) {
-	$entry = $gravityview->entry->from_field( $gravityview->field )->as_entry();
+	$entry = $gravityview->entry->from_field( $gravityview->field );
+	$entry = $entry->as_entry();
 } else {
 	$entry = $gravityview->entry->as_entry();
 }
