69e6a87265491eec52b78e4e208fb1b91c4d6106
Gennady Kovshenin <gennady@kovshenin.com>
2020-01-04 23:50:25 +0300

    Add delete link functionality support for admin views
    https://github.com/gravityview/Admin-Views/issues/1

--- includes/extensions/delete-entry/class-delete-entry.php
+++ includes/extensions/delete-entry/class-delete-entry.php
@@ -44,7 +44,7 @@ final class GravityView_Delete_Entry {
 
 		add_filter( 'gravityview_entry_default_fields', array( $this, 'add_default_field'), 10, 3 );
 
-		add_action( 'gravityview_before', array( $this, 'display_message' ) );
+		add_action( 'gravityview_before', array( $this, 'maybe_display_message' ) );
 
 		// For the Delete Entry Link, you don't want visible to all users.
 		add_filter( 'gravityview_field_visibility_caps', array( $this, 'modify_visibility_caps'), 10, 5 );
@@ -383,7 +383,7 @@ final class GravityView_Delete_Entry {
 
 			if( $entry ) {
 
-				$has_permission = $this->user_can_delete_entry( $entry );
+				$has_permission = $this->user_can_delete_entry( $entry, \GV\Utils::_GET( 'gvid', \GV\Utils::_GET( 'view_id' ) ) );
 
 				if( is_wp_error( $has_permission ) ) {
 
@@ -424,7 +424,7 @@ final class GravityView_Delete_Entry {
 				);
 			}
 
-			$redirect_to_base = esc_url_raw( remove_query_arg( array( 'action', 'gvid' ) ) );
+			$redirect_to_base = esc_url_raw( remove_query_arg( array( 'action', 'gvid', 'entry_id' ) ) );
 			$redirect_to = add_query_arg( $messages, $redirect_to_base );
 
 			wp_safe_redirect( $redirect_to );
@@ -743,14 +743,22 @@ final class GravityView_Delete_Entry {
 	 * @param int $current_view_id The ID of the View being rendered
 	 * @return void
 	 */
-	public function display_message( $current_view_id = 0 ) {
-
+	public function maybe_display_message( $current_view_id = 0 ) {
 		if( empty( $_GET['status'] ) || ! self::verify_nonce() ) {
 			return;
 		}
 
 		// Entry wasn't deleted from current View
 		if( isset( $_GET['view_id'] ) && intval( $_GET['view_id'] ) !== intval( $current_view_id ) ) {
+			return;
+		}
+
+		$this->display_message();
+	}
+
+	public function display_message() {
+
+		if ( empty( $_GET['status'] ) ) {
 			return;
 		}
 
