b5f5441d768428a8ca8734df269021567f2fbd90
Zack Katz <zack@katz.co>
2020-01-09 11:40:37 -0700

    Fix failing tests

--- tests/unit-tests/GravityView_Future_Test.php
+++ tests/unit-tests/GravityView_Future_Test.php
@@ -5998,7 +5998,7 @@ class GVFuture_Test extends GV_UnitTestCase {
 		$settings = gravityview()->plugin->settings;
 		$settings->update( array() );
 		$this->assertSame( \GravityView_Settings::get_instance(), $settings );
-		$this->assertEquals( array_keys( $settings->get_default_settings() ), array( 'license_key', 'license_key_response', 'license_key_status', 'support-email', 'no-conflict-mode', 'support_port', 'flexbox_search', 'rest_api', 'beta' ) );
+		$this->assertEquals( array_keys( $settings->get_default_settings() ), array( 'license_key', 'license_key_response', 'license_key_status', 'support-email', 'no-conflict-mode', 'support_port', 'flexbox_search', 'rest_api', 'beta', 'powered_by' ) );
 
 		$this->assertNull( $settings->get( 'not' ) );
 		$this->assertEquals( $settings->get( 'not', 'default' ), 'default' );
