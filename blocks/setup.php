81a436a25d14b9db6cdeb63ed300c2cc594a8b6f
Vlad <vlad@gravityview.co>
2020-02-17 12:38:46 -0500

    Do not use anonymous functions for filters/actions

--- blocks/setup.php
+++ blocks/setup.php
@@ -1,6 +1,6 @@
 <?php
 
-// Exit if accessed directly or Gutenberg is not enabled
+// Exit if accessed directly or if Gutenberg is not enabled
 if ( ! defined( 'ABSPATH' ) || ! function_exists( 'register_block_type' ) ) {
 	exit;
 }
@@ -8,7 +8,9 @@ if ( ! defined( 'ABSPATH' ) || ! function_exists( 'register_block_type' ) ) {
 /**
  * Enqueue UI assets
  */
-add_action( 'enqueue_block_editor_assets', function () {
+add_action( 'enqueue_block_editor_assets', 'gv_blocks_enqueue_assets' );
+
+function gv_blocks_enqueue_assets() {
 
 	$script = 'assets/js/gv-blocks.js';
 	$style  = 'assets/css/gv-blocks.css';
@@ -17,35 +19,32 @@ add_action( 'enqueue_block_editor_assets', function () {
 		'gv-blocks-js',
 		GV_BLOCKS_PLUGIN_URL . $script,
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components', 'wp-hooks', 'jquery' ),
-		filemtime( GV_BLOCKS_PLUGIN_PATH . $script )
+		filemtime( GV_BLOCKS_PLUGIN_PATH . $script ),
 	);
 
 	wp_enqueue_style(
 		'gv-blocks-css',
 		GV_BLOCKS_PLUGIN_URL . $style,
 		array( 'wp-edit-blocks' ),
-		filemtime( GV_BLOCKS_PLUGIN_PATH . $style )
+		filemtime( GV_BLOCKS_PLUGIN_PATH . $style ),
 	);
 
 	$views_list_array = array();
 
-	if ( class_exists( 'GVCommon' ) ) {
+	$views = GVCommon::get_all_views(
+		array(
+			'orderby' => 'post_title',
+			'order'   => 'ASC',
+		)
+	);
 
-		$views = GVCommon::get_all_views(
-			array(
-				'orderby' => 'post_title',
-				'order'   => 'ASC'
-			)
+	$views_list_array = array_map( function ( $view ) {
+
+		return array(
+			'value' => $view->ID,
+			'label' => $view->post_title,
 		);
-
-		$views_list_array = array_map( function ( $view ) {
-
-			return array(
-				'value' => $view->ID,
-				'label' => $view->post_title
-			);
-		}, $views );
-	}
+	}, $views );
 
 	wp_localize_script(
 		'gv-blocks-js',
@@ -54,11 +53,11 @@ add_action( 'enqueue_block_editor_assets', function () {
 			'home_page' => home_url(),
 			'ajax_url'  => admin_url( 'admin-ajax.php' ),
 			'img_url'   => GV_BLOCKS_PLUGIN_URL . 'assets/img/',
-			'view_list' => $views_list_array
+			'view_list' => $views_list_array,
 		)
 	);
 
-} );
+}
 
 /**
  * Register blocks
@@ -68,7 +67,7 @@ add_filter( 'block_categories', function ( $categories, $post ) {
 	return array_merge(
 		$categories,
 		array(
-			array( 'slug' => 'gravityview', 'title' => __( 'GravityView', 'gv-blocks' ) )
+			array( 'slug' => 'gravityview', 'title' => __( 'GravityView', 'gv-blocks' ) ),
 		)
 	);
 }, 10, 2 );
@@ -78,6 +77,7 @@ add_filter( 'block_categories', function ( $categories, $post ) {
  */
 foreach ( glob( GV_BLOCKS_PLUGIN_PATH . 'blocks/*/render.php' ) as $file ) {
 	include $file;
+
 	$block_path      = dirname( $file );
 	$block_cat       = basename( $block_path );
 	$block_name      = 'gv-blocks/' . $block_cat;
