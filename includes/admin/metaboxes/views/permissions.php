835333aac74c178caf5bd5bae77620dff8084038
Zack Katz <zack@katz.co>
2020-04-30 18:51:23 -0600

    Move Delete Entry to internal extension files
    This is part of "preparing the way"

--- includes/admin/metaboxes/views/permissions.php
+++ includes/admin/metaboxes/views/permissions.php
@@ -14,14 +14,16 @@ $current_settings = gravityview_get_template_settings( $post->ID );
 <table class="form-table striped"><?php
 
 	/**
+	 * Render additional Permissions metabox settings
+	 * @since 2.9
+	 * @param array $current_settings
+	 */
+	do_action( 'gravityview/metaboxes/permissions_before', $current_settings );
+
+	/**
 	 * @since 1.15.2
 	 */
 	GravityView_Render_Settings::render_setting_row( 'embed_only', $current_settings );
-
-	/**
-	 * @since  1.5.1
-	 */
-	GravityView_Render_Settings::render_setting_row( 'user_delete', $current_settings );
 
 	/**
 	 * @since  2.5
@@ -49,5 +51,11 @@ $current_settings = gravityview_get_template_settings( $post->ID );
 	 */
 	GravityView_Render_Settings::render_setting_row( 'csv_nolimit', $current_settings );
 
+	/**
+	 * Render additional Permissions metabox settings, like Delete Entry (if available)
+	 * @since 2.9
+	 * @param array $current_settings
+	 */
+	do_action( 'gravityview/metaboxes/permissions_after', $current_settings );
 	?>
 </table>
