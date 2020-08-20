80d4c7d267ec0c5cb5fadb21ca52e2e057aa3d39
Zack Katz <zack@katz.co>
2020-08-20 13:45:32 -0400

    Standardize capitalization in User Nickname setting

--- includes/fields/class-gravityview-field-created-by.php
+++ includes/fields/class-gravityview-field-created-by.php
@@ -138,7 +138,7 @@ class GravityView_Field_Created_By extends GravityView_Field {
 				'user_registered' => __( 'User Registered (Example: "2019-10-18 08:30:11")', 'gravityview' ),
 
 				// meta
-				'nickname' => __( 'User nickname', 'gravityview' ),
+				'nickname' => ucwords( __( 'User nickname', 'gravityview' ) ),
 				'description' => __( 'Description', 'gravityview' ),
 				'first_name' => __( 'First Name', 'gravityview' ),
 				'last_name' => __( 'Last Name', 'gravityview' ),
