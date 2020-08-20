8dc6fd0adf54b9324048a34ff159da103c763937
Zack Katz <zack@katz.co>
2020-08-20 12:33:22 -0400

    Add button-group class to make more WP-standard

--- includes/admin/metaboxes/views/view-configuration.php
+++ includes/admin/metaboxes/views/view-configuration.php
@@ -39,8 +39,10 @@
 
 			<?php // list of available widgets to be shown in the popup ?>
             <div id="directory-available-widgets" class="hide-if-js gv-tooltip">
-                <span class="close" role="button" aria-label="<?php esc_html_e( 'Close', 'gravityview' ); ?>"><i class="dashicons dashicons-dismiss"></i></span>
-				<?php do_action('gravityview_render_available_widgets' ); ?>
+				<div aria-live="polite" role="listbox" class="gv-items-picker-container gv-widget-picker-container" data-layout="grid">
+                	<span class="close" role="button" aria-label="<?php esc_html_e( 'Close', 'gravityview' ); ?>"><i class="dashicons dashicons-dismiss"></i></span>
+					<?php do_action('gravityview_render_available_widgets' ); ?>
+				</div>
             </div>
 
 		</div>
