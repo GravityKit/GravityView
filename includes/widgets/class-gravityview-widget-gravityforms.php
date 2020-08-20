cf82a3094b207fc358783f5e79b9d719758c7797
Zack Katz <zack@katz.co>
2020-08-20 16:37:22 -0400

    Fixed: Gravity Forms forms weren't showing when adding a Gravity Forms widget

--- includes/widgets/class-gravityview-widget-gravityforms.php
+++ includes/widgets/class-gravityview-widget-gravityforms.php
@@ -76,11 +76,14 @@ class GravityView_Widget_Gravity_Forms extends \GV\Widget {
 			return $choices;
 		}
 
+		// Inside GV's widget AJAX request
+		$doing_ajax = defined( 'DOING_AJAX' ) && 'gv_field_options' === \GV\Utils::_POST( 'action' );
+
 		/**
 		 * gravityview_get_forms() is currently running too early as widgets_init runs before init and
 		 * when most Gravity Forms plugins register their own fields like GP Terms of Service.
 		 */
-		if( \GV\Admin_Request::is_admin() && ! GFForms::is_gravity_page() ) {
+		if( $doing_ajax || ( \GV\Admin_Request::is_admin() && ! GFForms::is_gravity_page() ) ) {
 
 			// check for available gravity forms
 			$forms = gravityview_get_forms();
