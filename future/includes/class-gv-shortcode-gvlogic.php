529b24841372dadf32659c91456707098aaf9c97
Zack Katz <zack@katz.co>
2020-08-24 15:55:31 -0400

    Get rid of postbox container for layout tabs

--- future/includes/class-gv-shortcode-gvlogic.php
+++ future/includes/class-gv-shortcode-gvlogic.php
@@ -141,7 +141,7 @@ class gvlogic extends \GV\Shortcode {
 	}
 
 	/**
-	 * Get the ouput content.
+	 * Get the output content.
 	 *
 	 * @param bool $match if or else?
 	 * @param array $atts The attributes.
