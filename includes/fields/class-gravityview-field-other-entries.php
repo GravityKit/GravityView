10dd91a9882f051b3771089c524654f65e63ea5e
Zack Katz <zack@katz.co>
2020-10-19 11:22:28 -0400

    Fix width on Other Entries setting

--- includes/fields/class-gravityview-field-other-entries.php
+++ includes/fields/class-gravityview-field-other-entries.php
@@ -82,6 +82,7 @@ class GravityView_Field_Other_Entries extends GravityView_Field {
 			'label' => __( 'No Entries Text', 'gravityview' ),
 			'desc'  => __( 'The text that is shown if the entry creator has no other entries (and "Hide if no entries" is disabled).', 'gravityview' ),
 			'value' => __( 'This user has no other entries.', 'gravityview' ),
+			'class' => 'widefat',
 		);
 
 		return $new_options + $field_options;
