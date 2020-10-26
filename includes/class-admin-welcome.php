1a700afca09bb984017e25b1417d3208937b43f8
Zack Katz <zack@katz.co>
2020-10-26 17:00:21 -0400

    Version bump

--- includes/class-admin-welcome.php
+++ includes/class-admin-welcome.php
@@ -265,6 +265,13 @@ class GravityView_Welcome {
                     <h2 style="border-bottom: 1px solid #ccc; padding-bottom: 1em; margin-bottom: 0; margin-top: 0"><?php esc_html_e( 'What&rsquo;s New', 'gravityview' ); ?></h2>
                 </div>
 
+				<h3>2.9.2.1 on October 26, 2020</h3>
+
+				<ul>
+					<li>Improved: Plugin license information layout when running Gravity Forms 2.5</li>
+					<li>Fixed: View Settings overflow their container (introduced in 2.9.2)</li>
+				</ul>
+
 				<h3>2.9.2 on October 21, 2020</h3>
 
 				<ul>
