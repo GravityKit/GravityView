1fe6b8c4041c693cb5bf3a8ed169332df32a7e44
Zack Katz <zack@katz.co>
2020-12-04 14:55:53 -0500

    Show warning buttons as links instead of buttons

--- includes/admin/metaboxes/views/view-configuration.php
+++ includes/admin/metaboxes/views/view-configuration.php
@@ -60,10 +60,10 @@
 
 			<div class="notice notice-warning inline is-dismissible">
 				<h3><?php printf( esc_html__( 'Note: %s', 'gravityview' ), sprintf( esc_html__( 'No fields link to the %s layout.', 'gravityview'), esc_html__( 'Single Entry', 'gravityview' ) ) ); ?></h3>
-				<p><a class="button button-primary button-large" data-beacon-article-modal="54c67bbae4b0512429885516" href="https://docs.gravityview.co/article/70-linking-to-a-single-entry"><?php printf( esc_html__( 'Learn how to link to %s', 'gravityview' ), esc_html__( 'Single Entry', 'gravityview' ) ); ?></a></p>
+				<p><a data-beacon-article-modal="54c67bbae4b0512429885516" href="https://docs.gravityview.co/article/70-linking-to-a-single-entry"><?php printf( esc_html__( 'Learn how to link to %s', 'gravityview' ), esc_html__( 'Single Entry', 'gravityview' ) ); ?></a></p>
 			</div>
 
-			<h4><?php esc_html_e( 'These fields will be shown in Single Entry view.', 'gravityview'); ?></h4>
+			<h4><?php esc_html_e( 'These fields will be shown in Single Entry layout.', 'gravityview'); ?></h4>
 
 			<div id="single-active-fields" class="gv-grid">
 				<?php
@@ -85,7 +85,7 @@
 
 			<div class="notice notice-warning inline is-dismissible">
 				<h3><?php printf( esc_html__( 'Note: %s', 'gravityview' ), sprintf( esc_html__( 'No fields link to the %s layout.', 'gravityview'), esc_html__( 'Edit Entry', 'gravityview' ) ) ); ?></h3>
-				<p><a class="button button-primary button-large" data-beacon-article-modal="54c67bb9e4b0512429885513" href="https://docs.gravityview.co/article/67-configuring-the-edit-entry-screen"><?php printf( esc_html__( 'Learn how to link to %s', 'gravityview' ), esc_html__( 'Edit Entry', 'gravityview' ) ); ?></a></p>
+				<p><a data-beacon-article-modal="54c67bb9e4b0512429885513" href="https://docs.gravityview.co/article/67-configuring-the-edit-entry-screen"><?php printf( esc_html__( 'Learn how to link to %s', 'gravityview' ), esc_html__( 'Edit Entry', 'gravityview' ) ); ?></a></p>
 			</div>
 
 			<h4><?php esc_html_e( 'Fields shown when editing an entry.', 'gravityview'); ?> <span><?php esc_html_e('If not configured, all form fields will be displayed.', 'gravityview'); ?></span></h4>
