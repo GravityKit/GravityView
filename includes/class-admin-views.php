2913e877b86dc7035d7a6f9930dfee227176ec1e
Zack Katz <zack@katz.co>
2020-12-11 13:54:08 -0500

    Fix tooltip not appearing in tooltip

--- includes/class-admin-views.php
+++ includes/class-admin-views.php
@@ -943,11 +943,13 @@ class GravityView_Admin_Views {
 					<?php foreach( $areas as $area ) : 	?>
 
 						<div class="gv-droppable-area" data-areaid="<?php echo esc_attr( $zone .'_'. $area['areaid'] ); ?>" data-context="<?php echo esc_attr( $zone ); ?>">
-                            <p class="gv-droppable-area-title"><strong <?php if ( 'widget' === $type ) {
-									echo 'class="screen-reader-text"';
-								} ?>><?php echo esc_html( $area['title'] ); ?></strong><?php if ( ! empty( $area['subtitle'] ) ) { ?>
-                                    <span class="gv-droppable-area-subtitle"><span class="gf_tooltip gv_tooltip tooltip">
-                                    &ndash; <?php echo esc_html( $area['subtitle'] ); ?></span></span><?php } ?></p>
+                            <p class="gv-droppable-area-title">
+								<strong <?php if ( 'widget' === $type ) { echo 'class="screen-reader-text"'; } ?>><?php echo esc_html( $area['title'] ); ?></strong>
+
+                                <?php if ( ! empty( $area['subtitle'] ) ) { ?>
+									<span class="gv-droppable-area-subtitle"><span class="gf_tooltip gv_tooltip tooltip" title="<?php echo esc_attr( $area['subtitle'] ); ?>"></span></span>
+								<?php } ?>
+							</p>
 							<div class="active-drop-container active-drop-container-<?php echo esc_attr( $type ); ?>">
 							<div class="active-drop active-drop-<?php echo esc_attr( $type ); ?>" data-areaid="<?php echo esc_attr( $zone .'_'. $area['areaid'] ); ?>"><?php // render saved fields
 								if( ! empty( $values[ $zone .'_'. $area['areaid'] ] ) ) {
