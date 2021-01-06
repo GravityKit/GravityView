0b6d837f714ffff2fc3059bcbb9028a6011299d2
Zack Katz <zack@katz.co>
2021-01-06 10:46:14 -0500

    Fix failing test when plugin name isn't lowercase

--- tests/unit-tests/GravityView_Future_Test.php
+++ tests/unit-tests/GravityView_Future_Test.php
@@ -51,10 +51,11 @@ class GVFuture_Test extends GV_UnitTestCase {
      * @covers \GV\Plugin::relpath()
 	 */
 	public function test_plugin_dir_and_url_and_relpath() {
-		$plugin_folder_name = basename(GRAVITYVIEW_DIR);
+		$plugin_folder_name = basename( GRAVITYVIEW_DIR );
+		$plugin_folder_name_lower = strtolower( $plugin_folder_name );
 	    $this->assertEquals( GRAVITYVIEW_DIR, gravityview()->plugin->dir() );
-		$this->assertStringEndsWith( "/{$plugin_folder_name}/test/this.php", strtolower( gravityview()->plugin->dir( 'test/this.php' ) ) );
-		$this->assertStringEndsWith( "/{$plugin_folder_name}/and/this.php", strtolower( gravityview()->plugin->dir( '/and/this.php' ) ) );
+		$this->assertStringEndsWith( "/{$plugin_folder_name_lower}/test/this.php", strtolower( gravityview()->plugin->dir( 'test/this.php' ) ) );
+		$this->assertStringEndsWith( "/{$plugin_folder_name_lower}/and/this.php", strtolower( gravityview()->plugin->dir( '/and/this.php' ) ) );
 
 		$dirname = trailingslashit( dirname( plugin_basename( GRAVITYVIEW_FILE ) ) );
 
@@ -64,9 +65,9 @@ class GVFuture_Test extends GV_UnitTestCase {
 
 		/** Due to how WP_PLUGIN_DIR is different in test mode, we are only able to check bits of the URL */
 		$this->assertStringStartsWith( 'http', strtolower( gravityview()->plugin->url() ) );
-		$this->assertStringEndsWith( "/{$plugin_folder_name}/", strtolower( gravityview()->plugin->url() ) );
-		$this->assertStringEndsWith( "/{$plugin_folder_name}/test/this.php", strtolower( gravityview()->plugin->url( 'test/this.php' ) ) );
-		$this->assertStringEndsWith( "/{$plugin_folder_name}/and/this.php", strtolower( gravityview()->plugin->url( '/and/this.php' ) ) );
+		$this->assertStringEndsWith( "/{$plugin_folder_name_lower}/", strtolower( gravityview()->plugin->url() ) );
+		$this->assertStringEndsWith( "/{$plugin_folder_name_lower}/test/this.php", strtolower( gravityview()->plugin->url( 'test/this.php' ) ) );
+		$this->assertStringEndsWith( "/{$plugin_folder_name_lower}/and/this.php", strtolower( gravityview()->plugin->url( '/and/this.php' ) ) );
 	}
 
 	/**
