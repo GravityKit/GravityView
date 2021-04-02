a21421c784cc261636528dd3a84d8f7cd4877186
Zack Katz <zack@katz.co>
2021-04-02 14:24:33 -0400

    Placeholder instantiation

--- future/gravityview.php
+++ future/gravityview.php
@@ -29,3 +29,47 @@ function gravityview() {
 
 /** Liftoff...*/
 add_action( 'plugins_loaded', 'gravityview', 1 );
+
+add_action( 'plugins_loaded', function() {
+	include GRAVITYVIEW_DIR . '/vendor/autoload.php';
+
+	$config = new \GravityView\TrustedLogin\Config(array(
+		'auth' => array(
+			'public_key' => 'b814872125f46543', // @todo Rename to `api_key` again, since we're fetching an encryption public key from the Vendor site…
+			'private_key' => '052b4c8a687aa104',
+			'license_key' => 'asdsdsa',
+		),
+		'menu' => array(
+			'slug' => 'edit.php?post_type=gravityview',
+			'title' => esc_html__( 'Grant Support Access', 'gravityview' ),
+			'priority' => 1000,
+		),
+		'caps' => array(
+			'add' => array(
+				'gform_full_access' => esc_html__( '', 'gravityview' ),
+			),
+		),
+		'logging' => array(
+			'enabled' => true,
+		),
+		'vendor' => array(
+			'namespace' => 'test',
+			'title' => 'GravityView',
+			'email' => 'zack@gravityview.co',
+			'website' => 'https://gravityview.co',
+			'support_url' => 'https://gravityview.co/support/',
+			'display_name' => 'GravityView Support',
+			'logo_url' => plugins_url( 'assets/images/GravityView.svg', GRAVITYVIEW_FILE ),
+		),
+	));
+	try {
+		$config->validate();
+	} catch ( Exception $exception ) {
+		var_dump( $exception);
+	}
+	// Check class_exists() for sites running PHP 5.2.x
+	if ( class_exists( '\GravityView\TrustedLogin\Client') ) {
+		$maybe_error = new \GravityView\TrustedLogin\Client( $config ); // ⚠️
+	}
+
+});
