GravityKit/GravityView

commit 6d1660be0e5baf1675ed3ed72eb2e93362892271

Remove back-compat code for older versions of WP

future/includes/class-gv-plugin.php
@@ -303,16 +303,6 @@ final class Plugin {
 			return;
 		}
 
-		// Pre-4.6 loading
-		// TODO: Remove when GV minimum version is WordPress 4.6.0
-		$locale = apply_filters( 'plugin_locale', ( ( function_exists( 'get_user_locale' ) && is_admin() ) ? get_user_locale() : get_locale() ), 'gravityview' );
-
-		$loaded = load_textdomain( 'gravityview', sprintf( '%s/%s-%s.mo', $this->dir( 'languages' ), $domain, $locale ) );
-
-		if ( $loaded ) {
-			return;
-		}
-
 		gravityview()->log->error( sprintf( 'Unable to load textdomain for %s locale.', $locale ) );
 	}
 

future/_mocks.php
@@ -773,18 +773,6 @@ add_action( 'gravityview/template/after', function( $gravityview = null ) {
 			return;
 		}
 
-		/** WordPress 4.6 and lower compatibility, when WP_Hook classes were still absent. */
-		if ( is_array( $wp_filter['gravityview_after'] ) ) {
-			if ( ! empty( $wp_filter['gravityview_after'][10] ) ) {
-				foreach ( $wp_filter['gravityview_after'][10] as $function_key => $callback ) {
-					if ( strpos( $function_key, 'context_not_configured_warning' ) ) {
-						unset( $wp_filter['gravityview_after'][10][ $function_key ] );
-					}
-				}
-			}
-			return;
-		}
-
 		foreach ( $wp_filter['gravityview_after']->callbacks[10] as $function_key => $callback ) {
 			if ( strpos( $function_key, 'context_not_configured_warning' ) ) {
 				unset( $wp_filter['gravityview_after']->callbacks[10][ $function_key ] );

future/includes/class-gv-collection-view.php
@@ -109,17 +109,12 @@ class View_Collection extends Collection {
 			 */
 			$meta_keys = apply_filters_ref_array( 'gravityview/view_collection/from_post/meta_keys', array( array(), $post, &$views ) );
 
-			if ( function_exists( 'apply_filters_deprecated' ) ) {
-				$meta_keys = (array) apply_filters_deprecated( 'gravityview/data/parse/meta_keys', array( $meta_keys, $post->ID ), '2.0.7', 'gravityview/view_collection/from_post/meta_keys' );
-			} else {
-				/**
-				 * @filter `gravityview/data/parse/meta_keys`
-				 * @deprecated
-				 * @todo Require WP 4.6.0 so that `apply_filters_deprecated` is always available
-				 * @see The `gravityview/view_collection/from_post/meta_keys` filter.
-				 */
-				$meta_keys = (array) apply_filters( 'gravityview/data/parse/meta_keys', $meta_keys, $post->ID );
-			}
+			/**
+			 * @filter `gravityview/data/parse/meta_keys`
+			 * @deprecated
+			 * @see The `gravityview/view_collection/from_post/meta_keys` filter.
+			 */
+			$meta_keys = (array) apply_filters_deprecated( 'gravityview/data/parse/meta_keys', array( $meta_keys, $post->ID ), '2.0.7', 'gravityview/view_collection/from_post/meta_keys' );
 
 			/** What about inside post meta values? */
 			foreach ( $meta_keys as $meta_key ) {

future/includes/class-gv-template-legacy-override.php
@@ -142,24 +142,13 @@ class Legacy_Override_Template extends \GV\Gamajo_Template_Loader {
 		 */
 		global $wp_filter;
 		foreach ( array( 'gravityview_before', 'gravityview_after' ) as $hook ) {
-			/** WordPress 4.6 and lower compatibility, when WP_Hook classes were still absent. */
-			if ( is_array( $wp_filter[ $hook ] ) ) {
-				if ( ! empty( $wp_filter[ $hook ][10] ) ) {
-					foreach ( $wp_filter[ $hook ][10] as $function_key => $callback ) {
-						if ( strpos( $function_key, 'render_widget_hooks' ) ) {
-							unset( $wp_filter[ $hook ][10][ $function_key ] );
-						}
-					}
-				}
-			} else {
-				foreach ( $wp_filter[ $hook ]->callbacks[10] as $function_key => $callback ) {
-					if ( strpos( $function_key, 'render_widget_hooks' ) ) {
-						unset( $wp_filter[ $hook ]->callbacks[10][ $function_key ] );
-					}
+			foreach ( $wp_filter[ $hook ]->callbacks[10] as $function_key => $callback ) {
+				if ( strpos( $function_key, 'render_widget_hooks' ) ) {
+					unset( $wp_filter[ $hook ]->callbacks[10][ $function_key ] );
 				}
 			}
 		}
-		
+
 		/**
 		 * Single entry view.
 		 */

includes/class-cache.php
@@ -410,12 +410,7 @@ class GravityView_Cache {
 
 			$key = '_transient_gv-cache-';
 
-			// WordPress 4.0+
-			if ( is_callable( array( $wpdb, 'esc_like' ) ) ) {
-				$key = $wpdb->esc_like( $key );
-			} else {
-				$key = like_escape( $key );
-			}
+			$key = $wpdb->esc_like( $key );
 
 			$form_id = intval( $form_id );
 

includes/helper-functions.php
@@ -7,10 +7,10 @@
 
 /**
  * Get the URL for a CSS file
- * 
+ *
  * If there's a CSS file with the same name as a GravityView CSS file in the current theme directory, it will be used.
- * Place the CSS file in a `/gravityview/css/` sub-directory. 
- * 
+ * Place the CSS file in a `/gravityview/css/` sub-directory.
+ *
  * Example: /twentysixteen/gravityview/css/gv-default-styles.css
  *
  * Will use, in order:
@@ -42,7 +42,7 @@ function gravityview_css_url( $css_file = '', $dir_path = '' ) {
 		if( '' === $dir_path ) {
 			$dir_path = GRAVITYVIEW_DIR . 'templates/css/';
 		}
-		
+
 		// plugins_url() expects a path to a file, not directory. We append a file to be stripped.
 		$path = plugins_url( $css_file, trailingslashit( $dir_path )  . 'stripped-by-plugin_basename.php' );
 	}
@@ -509,28 +509,7 @@ function gv_maybe_json_decode( $value, $assoc = false, $depth = 512, $options = 
  * @return mixed The value with the callback applied to all non-arrays and non-objects inside it.
  */
 function gv_map_deep( $value, $callback ) {
-
-	// Use the original function, if exists.
-	// Requires WP 4.4+
-	if( function_exists( 'map_deep') ) {
-		return map_deep( $value, $callback );
-	}
-
-	// Exact copy of map_deep() code below:
-	if ( is_array( $value ) ) {
-		foreach ( $value as $index => $item ) {
-			$value[ $index ] = gv_map_deep( $item, $callback );
-		}
-	} elseif ( is_object( $value ) ) {
-		$object_vars = get_object_vars( $value );
-		foreach ( $object_vars as $property_name => $property_value ) {
-			$value->$property_name = gv_map_deep( $property_value, $callback );
-		}
-	} else {
-		$value = call_user_func( $callback, $value );
-	}
-
-	return $value;
+	return map_deep( $value, $callback );
 }
 
 /**

commit 40685dbbf72fd65f5d72e4b59409668b1bbb2cad

Fix undefined variable error
Regression introduced by 6d1660be0e5baf1675ed3ed72eb2e93362892271

future/includes/class-gv-plugin.php
@@ -304,6 +304,8 @@ final class Plugin {
 			return;
 		}
 
+		$locale = apply_filters( 'plugin_locale', ( ( function_exists('get_user_locale') && is_admin() ) ? get_user_locale() : get_locale() ), 'gravityview' );
+
 		gravityview()->log->error( sprintf( 'Unable to load textdomain for %s locale.', $locale ) );
 	}
 