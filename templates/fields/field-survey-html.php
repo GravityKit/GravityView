e716359e88e5f44ee5dcb963ba30c518231a2f1e
Zack Katz <zack@katz.co>
2021-07-12 22:31:36 -0400

    Remove outputting the HTML of the likert field…this made no sense

--- templates/fields/field-survey-html.php
+++ templates/fields/field-survey-html.php
@@ -15,22 +15,37 @@ $field = $gravityview->field;
 
 $display_value = $gravityview->display_value;
 
-if ( $field->score ) {
-	if ( $display_value = $gravityview->value[ $field->ID ] ) {
-		list( $row, $column ) = explode( ':', $display_value );
-		foreach( $field->field->choices as $choice ) {
-			if ( $column === $choice['value'] ) {
-				$display_value = $choice['score'];
-				break;
-			}
+// An empty single column
+if ( '' === \GV\Utils::get( $gravityview->value, $field->ID ) ) {
+	echo '';
+	return;
+}
+
+$is_single_column = isset( $gravityview->value[ $field->ID ] );
+
+// Multiple Row survey fields are formatted with colon-separated information
+if ( $is_single_column && false !== strpos( $gravityview->value[ $field->ID ], ':' ) ) {
+	list( $row, $column ) = explode( ':', $gravityview->value[ $field->ID ] );
+} else {
+	$column = $gravityview->value;
+}
+
+$display_values = array();
+foreach( $field->field->choices as $choice ) {
+	if ( ! $is_single_column || $column === $choice['value'] ) {
+
+		if ( $field->score ) {
+			$display_values[] = $choice['score'];
+		} else {
+			$display_values[] = RGFormsModel::get_choice_text( $field->field, $choice['value'], $column );
+		}
+
+		if( $is_single_column) {
+			break;
 		}
 	}
-} else {
-	// Make sure the CSS file is enqueued
-	if ( class_exists( 'GFSurvey' ) && is_callable( array('GFSurvey', 'get_instance') ) ) {
-		wp_register_style( 'gsurvey_css', GFSurvey::get_instance()->get_base_url() . '/css/gsurvey.css' );
-		wp_print_styles( 'gsurvey_css' );
-	}
 }
+
+$display_value = implode( '; ', $display_values );
 
 echo $display_value;
