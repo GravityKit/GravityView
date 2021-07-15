37f2ee29add3d343a8c5b423393b0fc84c376d52
Zack Katz <zack@katz.co>
2021-07-15 11:51:08 -0400

    Pass open dialog instead of querying DOM again

--- assets/js/admin-views.js
+++ assets/js/admin-views.js
@@ -175,7 +175,7 @@
 
 					var was_closed = $( '.gv-field-details', $dialog ).hasClass('gv-field-details--closed');
 
-					viewConfiguration.toggleFieldDetails( was_closed );
+					viewConfiguration.toggleFieldDetails( $dialog, was_closed );
 
 					// When toggled, set a new cookie
 					$.cookie( 'gv-field-details-expanded', was_closed, { path: gvGlobals.admin_cookiepath } );
@@ -1010,16 +1010,17 @@
 
 			var show_details = viewConfiguration.getCookieVal( show_details_cookie );
 
-			viewConfiguration.toggleFieldDetails( show_details );
+			viewConfiguration.toggleFieldDetails( dialog, show_details );
 		},
 
 		/**
 		 * Toggle visibility for field details
 		 * @since 2.10
+		 * @param {jQuery}  $dialog The open dialog
 		 * @param {boolean} show_details Whether to show the field details or not
 		 */
-		toggleFieldDetails: function ( show_details ) {
-			$( '.gv-dialog:visible' )
+		toggleFieldDetails: function ( $dialog, show_details ) {
+			$dialog
 				.find( '.gv-field-details' ).toggleClass( 'gv-field-details--closed', ! show_details ).end()
 				.find( '.gv-field-details--toggle .dashicons' )
 				.toggleClass( 'dashicons-arrow-down', !! show_details )
