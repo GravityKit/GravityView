46d32faa7829164b217875a9e6db865a9ed5fff2
Zack Katz <zack@katz.co>
2021-09-06 11:38:27 -0400

    Update TrustedLogin credentials
    Still need to update the namespace, but we're not there yet with CSS generation

--- includes/plugin-and-theme-hooks/class-gravityview-plugin-hooks-trustedlogin.php
+++ includes/plugin-and-theme-hooks/class-gravityview-plugin-hooks-trustedlogin.php
@@ -23,7 +23,7 @@ class GravityView_Plugin_Hooks_TrustedLogin extends GravityView_Plugin_and_Theme
 
 	const TRUSTEDLOGIN_NAMESPACE = 'test';
 
-	const TRUSTEDLOGIN_API_KEY = '6346688830182b64';
+	const TRUSTEDLOGIN_API_KEY = '07abf14e66223832';
 
 	const TRUSTEDLOGIN_CLASS_NAME = '\GravityView\TrustedLogin\Client';
 
