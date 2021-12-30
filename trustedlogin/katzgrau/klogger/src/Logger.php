32764c7c5097a7b856f673f9c52f33fbaaf41ce3
GravityView - CI <support@gravityview.co>
2021-12-30 21:42:47 +0000

    Add updated translations/assets & release v2.14.0.1 [ci skip]

--- trustedlogin/katzgrau/klogger/src/Logger.php
+++ trustedlogin/katzgrau/klogger/src/Logger.php
@@ -2,7 +2,7 @@
 /**
  * @license MIT
  *
- * Modified by gravityview on 22-December-2021 using Strauss.
+ * Modified by gravityview on 30-December-2021 using Strauss.
  * @see https://github.com/BrianHenryIE/strauss
  */
 namespace GravityView\Katzgrau\KLogger;
