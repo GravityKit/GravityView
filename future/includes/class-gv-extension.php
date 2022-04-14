088b15a1188d3ef13cff934b6beabfd08be77595
Zack Katz <zack@katz.co>
2022-04-14 11:47:46 -0400

    Fix PHP 5.6 error

--- future/includes/class-gv-extension.php
+++ future/includes/class-gv-extension.php
@@ -320,7 +320,7 @@ abstract class Extension {
 	            'wp_version' => get_bloginfo( 'version' ),
 	            'gv_version' => \GV\Plugin::$version,
 	            'environment' => function_exists( 'wp_get_environment_type' ) ? wp_get_environment_type() : 'production',
-          	),
+          	)
         );
 	}
 
