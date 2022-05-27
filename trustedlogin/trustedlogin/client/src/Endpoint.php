0f346ace8710ed75e2a43bb4c947455d47e34fe8
GravityView - CI <support@gravityview.co>
2022-05-27 20:41:37 +0000

    Add updated translations/assets & release v2.14.6 [ci skip]

--- trustedlogin/trustedlogin/client/src/Endpoint.php
+++ trustedlogin/trustedlogin/client/src/Endpoint.php
@@ -7,7 +7,7 @@
  * @copyright 2021 Katz Web Services, Inc.
  *
  * @license GPL-2.0-or-later
- * Modified by gravityview on 05-May-2022 using Strauss.
+ * Modified by gravityview on 27-May-2022 using Strauss.
  * @see https://github.com/BrianHenryIE/strauss
  */
 
