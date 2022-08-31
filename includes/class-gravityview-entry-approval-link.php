c22827258eb06945433eda8069a6c6a49f5bc8e1
Paulo Iankoski <me@wpdev.com.br>
2022-08-31 12:35:32 -0300

    Add merge tags for entry moderation

---
+++ includes/class-gravityview-entry-approval-link.php
@@ -0,0 +1,222 @@
+<?php
+/**
+ * @file class-gravityview-entry-approval-link.php
+ * @package   GravityView
+ * @license   GPL2+
+ * @author    GravityView <hello@gravityview.co>
+ * @link      https://gravityview.co
+ * @copyright Copyright 2016, Katz Web Services, Inc.
+ *
+ * @since 2.14.8
+ */
+
+/** If this file is called directly, abort. */
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+/**
+ * Handle approval links
+ *
+ * @since 2.14.8
+ */
+class GravityView_Entry_Approval_Link {
+
+	/**
+	 * The regular expression to use for the matching.
+	 *
+	 * @since 1.7.1-dev
+	 *
+	 * @var string
+	 */
+	protected $regex = '';
+
+	public function __construct() {
+		$this->add_hooks();
+	}
+
+	/**
+	 * Add actions and filters related to entry approval links
+	 *
+	 * @return void
+	 */
+	private function add_hooks() {
+		add_filter( 'gform_custom_merge_tags', array( $this, '_filter_gform_custom_merge_tags' ), 10, 4 );
+		add_filter( 'gform_replace_merge_tags', array( $this, '_filter_gform_replace_merge_tags' ), 10, 7 );
+	}
+
+	/**
+	 * Add custom merge tags to merge tag options. DO NOT OVERRIDE.
+	 *
+	 * @internal Not to be overridden by fields
+	 *
+	 * @since 2.14.8
+	 *
+	 * @param array $custom_merge_tags
+	 * @param int $form_id GF Form ID
+	 * @param GF_Field[] $fields Array of fields in the form
+	 * @param string $element_id The ID of the input that Merge Tags are being used on
+	 *
+	 * @return array Modified merge tags
+	 */
+	public function _filter_gform_custom_merge_tags( $custom_merge_tags = array(), $form_id = 0, $fields = array(), $element_id = '' ) {
+
+		$form = GVCommon::get_form( $form_id );
+
+		$field_merge_tags = $this->custom_merge_tags( $form, $fields );
+
+		return array_merge( $custom_merge_tags, $field_merge_tags );
+	}
+
+	/**
+	 * Add custom Merge Tags to Merge Tag options, if custom Merge Tags exist
+	 *
+	 * @since 2.14.8
+	 *
+	 * @param array $form GF Form array
+	 * @param GF_Field[] $fields Array of fields in the form
+	 *
+	 * @return array Merge tag array with `label` and `tag` keys
+	 */
+	protected function custom_merge_tags( $form = array(), $fields = array() ) {
+
+		$merge_tags = array(
+			array(
+				'label' => __( 'Link to approve an entry', 'gravityview' ),
+				'tag' => '{gv_approve_entry}',
+			),
+			array(
+				'label' => __( 'Link to disapprove an entry', 'gravityview' ),
+				'tag' => '{gv_disapprove_entry}',
+			),
+			array(
+				'label' => __( 'Link to unapprove an entry', 'gravityview' ),
+				'tag' => '{gv_unapprove_entry}',
+			),
+		);
+
+		return $merge_tags;
+	}
+
+	/**
+	 * Match the merge tag in replacement text for the field.  DO NOT OVERRIDE.
+	 *
+	 * @see replace_merge_tag Override replace_merge_tag() to handle any matches
+	 *
+	 * @since 2.14.8
+	 *
+	 * @param string $text Text to replace
+	 * @param array $form Gravity Forms form array
+	 * @param array $entry Entry array
+	 * @param bool $url_encode Whether to URL-encode output
+	 *
+	 * @return string Original text if {_custom_merge_tag} isn't found. Otherwise, replaced text.
+	 */
+	public function _filter_gform_replace_merge_tags( $text, $form = array(), $entry = array(), $url_encode = false, $esc_html = false  ) {
+
+		$matches = array();
+		preg_match_all( '/{gv_(.+)_entry:?([0-9]+)?:?(.+)?}/', $text, $matches, PREG_SET_ORDER );
+
+		// If there are no matches, return original text
+		if ( empty( $matches ) ) {
+			return $text;
+		}
+
+		return $this->replace_merge_tag( $matches, $text, $form, $entry, $url_encode, $esc_html );
+	}
+
+	/**
+	 * Run GravityView filters when using GFCommon::replace_variables()
+	 *
+	 * Instead of adding multiple hooks, add all hooks into this one method to improve speed
+	 *
+	 * @since 2.14.8
+	 *
+	 * @see GFCommon::replace_variables()
+	 *
+	 * @param array $matches Array of Merge Tag matches found in text by preg_match_all
+	 * @param string $text Text to replace
+	 * @param array|bool $form Gravity Forms form array. When called inside {@see GFCommon::replace_variables()} (now deprecated), `false`
+	 * @param array|bool $entry Entry array.  When called inside {@see GFCommon::replace_variables()} (now deprecated), `false`
+	 * @param bool $url_encode Whether to URL-encode output
+	 * @param bool $esc_html Whether to apply `esc_html()` to output
+	 *
+	 * @return mixed
+	 */
+	protected function replace_merge_tag( $matches = array(), $text = '', $form = array(), $entry = array(), $url_encode = false, $esc_html = false ) {
+
+		foreach( $matches as $match ) {
+
+			list( $full_tag, $action, $expiration_hours, $permissions ) = $match;
+
+			$token = $this->get_token( $action, $expiration_hours, $permissions, $entry );
+
+			if ( ! $token ) {
+				continue;
+			}
+
+			$link = sprintf( '<a href="%s">%s</a>', esc_url( $this->get_link_url( $token ) ), ucfirst( $action ) );
+
+			$text = str_replace( $full_tag, $link, $text );
+		}
+
+		return $text;
+	}
+
+	protected function get_token( $action = '', $expiration_hours = 24, $permissions = 'private', $entry = array() ) {
+
+		if ( ! $action || ! $entry['id'] ) {
+			return false;
+		}
+
+		$scopes = array(
+			'entry_timestamp' => $entry['timestamp'],
+			'entry_id'        => $entry['id'],
+			'action'          => $action,
+			'permissions'     => $permissions,
+		);
+
+		$expiration_timestamp = strtotime( '+' . (int) $expiration_hours . ' hours' );
+
+		$jti = uniqid();
+
+		$token_array = array(
+			'iat'    => time(),
+			'exp'    => $expiration_timestamp,
+			'scopes' => $scopes,
+			'jti'    => $jti,
+		);
+
+		$token = rawurlencode( base64_encode( json_encode( $token_array ) ) );
+
+		$secret = get_option( 'gravityview_token_secret' );
+		if ( empty( $secret ) ) {
+			$secret = wp_generate_password( 64 );
+			update_option( 'gravityview_token_secret', $secret );
+		}
+
+		$sig = hash_hmac( 'sha256', $token, $secret );
+
+		$token .= '.' . $sig;
+
+		return $token;
+	}
+
+	protected function get_link_url( $token ) {
+
+		$base_url = '';
+		if ( empty( $base_url ) ) {
+			$base_url = admin_url( 'admin.php' );
+		}
+
+		if ( ! empty( $token ) ) {
+			$query_args['gv_token'] = $token;
+		}
+
+		$url = add_query_arg( $query_args, $base_url );
+
+		return $url;
+	}
+}
+
+new GravityView_Entry_Approval_Link;
