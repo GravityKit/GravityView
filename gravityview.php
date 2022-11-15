b5b19eebceef7cb88fa43ee9ddd4b6d52c5c15d9
Zack Katz <zack@katz.co>
2022-11-14 20:54:55 -0500

    Version bump to hide Foundation warning

--- gravityview.php
+++ gravityview.php
@@ -3,7 +3,7 @@
  * Plugin Name:       	GravityView
  * Plugin URI:        	https://www.gravitykit.com
  * Description:       	The best, easiest way to display Gravity Forms entries on your website.
- * Version:             2.15
+ * Version:             2.16
  * Author:            	GravityKit
  * Author URI:        	https://www.gravitykit.com
  * Text Domain:       	gk-gravityview
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * The plugin version.
  */
-define( 'GV_PLUGIN_VERSION', '2.15' );
+define( 'GV_PLUGIN_VERSION', '2.16' );
 
 /**
  * Full path to the GravityView file
