221f15c2f4a4f0935aafe0d1eaf1c7f0c3cc0d49
Vlad <vlad@gravityview.co>
2022-12-07 18:24:36 -0500

    Add back the legacy Extension class removed in f5d71
    It's required by DataTables

--- future/includes/class-gv-plugin.php
+++ future/includes/class-gv-plugin.php
@@ -201,6 +201,10 @@ final class Plugin {
 	 * @return void
 	 */
 	public function include_legacy_core() {
+		if ( ! class_exists( '\GravityView_Extension' ) ) {
+			include_once $this->dir( 'includes/class-gravityview-extension.php' );
+		}
+
 		if ( ! gravityview()->plugin->is_compatible() ) {
 			return;
 		}
