17dce8e31d57f4fa886c382e036338838e35a8f0
GravityView - CI <support@gravityview.co>
2022-12-15 02:05:07 +0000

    Add updated translations/assets & release v2.16.2 [ci skip]

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit74ceb0bb1ee6988eeedcd0ab8bdbfd31
+class ComposerStaticInit35f153e73e46bfd6da9fc86d2ae4912b
 {
     public static $files = array (
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
@@ -138,10 +138,10 @@ class ComposerStaticInit74ceb0bb1ee6988eeedcd0ab8bdbfd31
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit74ceb0bb1ee6988eeedcd0ab8bdbfd31::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit74ceb0bb1ee6988eeedcd0ab8bdbfd31::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit74ceb0bb1ee6988eeedcd0ab8bdbfd31::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit74ceb0bb1ee6988eeedcd0ab8bdbfd31::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit35f153e73e46bfd6da9fc86d2ae4912b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit35f153e73e46bfd6da9fc86d2ae4912b::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit35f153e73e46bfd6da9fc86d2ae4912b::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit35f153e73e46bfd6da9fc86d2ae4912b::$classMap;
 
         }, null, ClassLoader::class);
     }
