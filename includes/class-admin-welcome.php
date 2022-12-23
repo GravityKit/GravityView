1ee3f31b42e896b2e6e27cc9c7d0344667faa768
Vlad <vlad@gravityview.co>
2022-12-22 22:13:21 -0500

    Update changelog [ci skip]

--- includes/class-admin-welcome.php
+++ includes/class-admin-welcome.php
@@ -277,6 +277,12 @@ class GravityView_Welcome {
 					<h2 style="border-bottom: 1px solid #ccc; padding-bottom: 1em; margin-bottom: 0; margin-top: 0"><?php esc_html_e( 'What&rsquo;s New', 'gk-gravityview' ); ?></h2>
 				</div>
 
+				<h3>2.16.4 on December 22, 2022</h3>
+
+				<ul>
+					<li>Fixed: Prevent possible conflict in the View editor with themes/plugins that use Bootstrap's tooltip library</li>
+				</ul>
+
 				<h3>2.16.3 on December 21, 2022</h3>
 
 				<ul>
