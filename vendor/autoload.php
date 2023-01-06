2d94a3b7438d23c9610be99f0d68b5c88b4153d1
GravityView - CI <support@gravityview.co>
2023-01-06 01:24:06 +0000

    Add updated translations/assets & release v2.16.5 [ci skip]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit7a84ee8299810f66170b3a694d59cf40::getLoader();
+return ComposerAutoloaderInit3005711f0690e37711fc0e22378b19a5::getLoader();
