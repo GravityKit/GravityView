613084bab0714b65f860a32a99b208feab5ee245
Vlad <vlad@gravityview.co>
2023-01-23 19:25:41 -0500

    Add missing prop definition

--- future/includes/gutenberg/build/entry-link.asset.php
+++ future/includes/gutenberg/build/entry-link.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2786fd6919279e341a92');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0bdc79aa1745ba769a31');
