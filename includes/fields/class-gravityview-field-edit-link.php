acc347b6c4621943c28f3c12be02ce1bef99e267
Zack Katz <zack@katz.co>
2023-10-28 17:09:45 -0700

    Remove add_default_field
    Now that we're inserting the featured fields in `GravityView_Admin_Views::get_available_fields()`, we no longer need to add them using this method.

--- includes/fields/class-gravityview-field-edit-link.php
+++ includes/fields/class-gravityview-field-edit-link.php
@@ -33,34 +33,8 @@ class GravityView_Field_Edit_Link extends GravityView_Field {
 	public function __construct() {
 		$this->label = esc_html__( 'Link to Edit Entry', 'gk-gravityview' );
 		$this->description = esc_html__('A link to edit the entry. Visible based on View settings.', 'gk-gravityview');
+
 		parent::__construct();
-	}
-
-	/**
-	 * Add as a default field, outside those set in the Gravity Form form
-	 *
-	 * @since 2.10 Moved here from GravityView_Admin_Views::get_entry_default_fields
-	 *
-	 * @param array $entry_default_fields Existing fields
-	 * @param string|array $form form_ID or form object
-	 * @param string $zone Either 'single', 'directory', 'edit', 'header', 'footer'
-	 *
-	 * @return array
-	 */
-	public function add_default_field( $entry_default_fields, $form = array(), $zone = '' ) {
-
-		if ( 'directory' !== $zone ) {
-			return $entry_default_fields;
-		}
-
-		$entry_default_fields[ $this->name ] = array(
-			'label' => $this->label,
-			'type'  => $this->name,
-			'desc'  => $this->description,
-			'icon'  => $this->icon,
-		);
-
-		return $entry_default_fields;
 	}
 
 	public function field_options( $field_options, $template_id, $field_id, $context, $input_type, $form_id ) {
