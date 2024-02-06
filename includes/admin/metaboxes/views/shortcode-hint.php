b17467ac77dcc8343bd35974057da0c08dd1899e
Doeke Norg <doekenorg@gmail.com>
2024-02-06 15:06:21 +0100

    Add `secret` to shortcode hint.

--- includes/admin/metaboxes/views/shortcode-hint.php
+++ includes/admin/metaboxes/views/shortcode-hint.php
@@ -6,12 +6,21 @@
  */
 
 global $post;
+
+$view   = \GV\View::from_post( $post );
+$secret = $view->get_validation_secret();
+$atts   = [ sprintf( "id='%d'", $post->ID ) ];
+if ( $secret ) {
+	$atts[] = sprintf( "secret='%s'", $secret );
+}
+
+$shortcode = sprintf( '[gravityview %s]', implode( ' ', $atts ) );
 ?>
 <div class="misc-pub-section gv-shortcode misc-pub-section-last">
 	<i class="dashicons dashicons-editor-code"></i>
 	<span><?php esc_html_e( 'Embed Shortcode', 'gk-gravityview' ); ?></span>
 	<div>
-		<input type="text" readonly="readonly" value="[gravityview id='<?php echo $post->ID; ?>']" class="code widefat" />
+		<input type="text" readonly="readonly" value="<?php echo esc_attr($shortcode) ?>" class="code widefat" />
 		<span class="howto"><?php esc_html_e( 'Add this shortcode to a post or page to embed this view.', 'gk-gravityview' ); ?></span>
 	</div>
 </div>
