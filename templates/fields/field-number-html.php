3862885f69f16a4549fd417aa783c485a9af9897
omarkasem <omar.kasem207@gmail.com>
2024-03-27 03:43:19 +0200

    fix comma issue by editing wp_locale decimal point and thousands sep in case of decimals

--- templates/fields/field-number-html.php
+++ templates/fields/field-number-html.php
@@ -15,27 +15,29 @@ $value          = $gravityview->value;
 $form           = $gravityview->view->form->form;
 $display_value  = $gravityview->display_value;
 $field_settings = $gravityview->field->as_configuration();
-$field = GFFormsModel::get_field( $form, $gravityview->field->id );
+$field          = GFFormsModel::get_field( $form, $gravityview->field->id );
 
 if ( '' !== $value ) {
 	$decimals = ( isset( $field_settings['decimals'] ) && '' !== $field_settings['decimals'] ) ? $field_settings['decimals'] : '';
 	if ( empty( $field_settings['number_format'] ) && 'currency' === $gravityview->field->field->numberFormat ) {
 		echo $display_value;
 	} else {
-		if($decimals){
+		if ( $decimals ) {
 			global $wp_locale;
-			if($gravityview->field->field->numberFormat === 'decimal_comma'){
-				$wp_locale->number_format['decimal_point'] = ',';
-				$wp_locale->number_format['thousands_sep'] = '.';
-			}else{
-				$wp_locale->number_format['decimal_point'] = '.';
-				$wp_locale->number_format['thousands_sep'] = ',';
+			if ( $wp_locale ) {
+				if ( $gravityview->field->field->numberFormat === 'decimal_comma' ) {
+					$wp_locale->number_format['decimal_point'] = ',';
+					$wp_locale->number_format['thousands_sep'] = '.';
+				} else {
+					$wp_locale->number_format['decimal_point'] = '.';
+					$wp_locale->number_format['thousands_sep'] = ',';
+				}
 			}
 
 			$value = gravityview_number_format( $value, $decimals, ! empty( $field_settings['number_format'] ) );
 		}
 
-		echo $field->get_value_entry_list( $value, $gravityview->entry, $gravityview->field->id, [], $form );
+		echo $field->get_value_entry_list( $value, $gravityview->entry, $gravityview->field->id, array(), $form );
 
 	}
 } else {
