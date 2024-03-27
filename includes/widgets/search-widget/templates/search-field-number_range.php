5a89e95889a98ce46b3b32f3a16ab2bdb3a45994
Doeke Norg <doekenorg@gmail.com>
2024-03-27 14:15:24 +0100

    Add decimal support for total price.

--- includes/widgets/search-widget/templates/search-field-number_range.php
+++ includes/widgets/search-widget/templates/search-field-number_range.php
@@ -18,6 +18,13 @@ $error = null;
 if ( is_numeric( $min ) && is_numeric( $max ) && $min > $max ) {
 	$error = esc_html__( 'The "from" value is lower than the "to" value.', 'gk-gravityview' );
 }
+
+// Add decimal support for total price.
+$step = apply_filters(
+	'gk/gravityview/search/number_range/step',
+	'total' === $gravityview_view->search_field['type'] ? '0.01' : '1',
+	$gravityview_view
+);
 ?>
 
 <div class="gv-search-box gv-search-number gv-search-number-range">
@@ -25,8 +32,8 @@ if ( is_numeric( $min ) && is_numeric( $max ) && $min > $max ) {
 	<label for="search-box-<?php echo esc_attr( $name ) . '-start'; ?>"><?php echo esc_html( $label ); ?></label>
 	<?php } ?>
 	<p>
-		<input name="<?php echo esc_attr( $name ) . '[min]'; ?>" id="search-box-<?php echo esc_attr( $name ) . '-min'; ?>" type="number" placeholder="<?php esc_attr_e( 'From', 'gk-gravityview' ); ?>" value="<?php echo esc_attr( $min ); ?>">
-		<input name="<?php echo esc_attr( $name ) . '[max]'; ?>" id="search-box-<?php echo esc_attr( $name ) . '-max'; ?>" type="number" placeholder="<?php esc_attr_e( 'To', 'gk-gravityview' ); ?>" value="<?php echo esc_attr( $max ); ?>">
+		<input name="<?php echo esc_attr( $name ) . '[min]'; ?>" id="search-box-<?php echo esc_attr( $name ) . '-min'; ?>" type="number" placeholder="<?php esc_attr_e( 'From', 'gk-gravityview' ); ?>" step="<?php echo esc_attr( $step ); ?>" value="<?php echo esc_attr( $min ); ?>">
+		<input name="<?php echo esc_attr( $name ) . '[max]'; ?>" id="search-box-<?php echo esc_attr( $name ) . '-max'; ?>" type="number" placeholder="<?php esc_attr_e( 'To', 'gk-gravityview' ); ?>" step="<?php echo esc_attr( $step ); ?>" value="<?php echo esc_attr( $max ); ?>">
 	</p>
 	<?php if ( $error ) {
 		printf( '<p class="error">%s</p>', $error );
