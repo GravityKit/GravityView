9990b4b086479970d63f3eada4c03d176591e833
Doeke Norg <doekenorg@gmail.com>
2024-03-28 12:07:34 +0100

    Add currency on field. No decimals for quantity.

--- includes/widgets/search-widget/templates/search-field-number_range.php
+++ includes/widgets/search-widget/templates/search-field-number_range.php
@@ -19,21 +19,41 @@ if ( is_numeric( $min ) && is_numeric( $max ) && $min > $max ) {
 	$error = esc_html__( 'The "from" value is lower than the "to" value.', 'gk-gravityview' );
 }
 
+$is_currency = 'total' === $gravityview_view->search_field['type'];
+if ( ! $is_currency ) {
+	// could still be currency from the field.
+	$field       = GVCommon::get_field( $gravityview_view->getForm() ?? [], $gravityview_view->search_field['key'] );
+	$is_currency = 'currency' === $field->numberFormat;
+}
+
 // Add decimal support for total price.
 $step = apply_filters(
 	'gk/gravityview/search/number_range/step',
-	'total' === $gravityview_view->search_field['type'] ? '0.01' : '1',
+	'quantity' === $gravityview_view->search_field['type'] ? '1' : '0.01',
 	$gravityview_view
 );
 ?>
 
 <div class="gv-search-box gv-search-number gv-search-number-range">
 	<?php if ( ! gv_empty( $label, false, false ) ) { ?>
-	<label for="search-box-<?php echo esc_attr( $name ) . '-start'; ?>"><?php echo esc_html( $label ); ?></label>
+		<label for="search-box-<?php echo esc_attr( $name ) . '-start'; ?>">
+			<?php echo esc_html__( $label ) . ( $is_currency ? ' (' . GFCommon::get_currency() . ')' : '' ); ?>
+		</label>
 	<?php } ?>
 	<p>
-		<input name="<?php echo esc_attr( $name ) . '[min]'; ?>" id="search-box-<?php echo esc_attr( $name ) . '-min'; ?>" type="number" placeholder="<?php esc_attr_e( 'From', 'gk-gravityview' ); ?>" step="<?php echo esc_attr( $step ); ?>" value="<?php echo esc_attr( $min ); ?>">
-		<input name="<?php echo esc_attr( $name ) . '[max]'; ?>" id="search-box-<?php echo esc_attr( $name ) . '-max'; ?>" type="number" placeholder="<?php esc_attr_e( 'To', 'gk-gravityview' ); ?>" step="<?php echo esc_attr( $step ); ?>" value="<?php echo esc_attr( $max ); ?>">
+		<input name="<?php echo esc_attr( $name ) . '[min]'; ?>"
+			   id="search-box-<?php echo esc_attr( $name ) . '-min'; ?>"
+			   type="number"
+			   placeholder="<?php esc_attr_e( 'From', 'gk-gravityview' ); ?>"
+			   step="<?php echo esc_attr( $step ); ?>"
+			   value="<?php echo esc_attr( $min ); ?>">
+
+		<input name="<?php echo esc_attr( $name ) . '[max]'; ?>"
+			   id="search-box-<?php echo esc_attr( $name ) . '-max'; ?>"
+			   type="number"
+			   placeholder="<?php esc_attr_e( 'To', 'gk-gravityview' ); ?>"
+			   step="<?php echo esc_attr( $step ); ?>"
+			   value="<?php echo esc_attr( $max ); ?>">
 	</p>
 	<?php if ( $error ) {
 		printf( '<p class="error">%s</p>', $error );
