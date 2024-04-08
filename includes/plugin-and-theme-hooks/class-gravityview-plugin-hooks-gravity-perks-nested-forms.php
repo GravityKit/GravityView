a0c433f4c9267945d6dd20efdcc203a5e466ca56
Doeke Norg <doekenorg@gmail.com>
2024-04-08 16:09:58 +0200

    Register custom meta tags.

--- includes/plugin-and-theme-hooks/class-gravityview-plugin-hooks-gravity-perks-nested-forms.php
+++ includes/plugin-and-theme-hooks/class-gravityview-plugin-hooks-gravity-perks-nested-forms.php
@@ -23,6 +23,12 @@ final class GravityView_Plugin_Hooks_Gravity_Perks_Nested_Forms extends GravityV
 			Closure::fromCallable( [ $this, 'add_gpnf_merge_tags' ] ),
 			5
 		);
+
+		add_filter(
+			'gform_custom_merge_tags',
+			Closure::fromCallable( [ $this, 'add_custom_gpnf_merge_tags' ] ),
+			10
+		);
 	}
 
 	/**
@@ -30,7 +36,7 @@ final class GravityView_Plugin_Hooks_Gravity_Perks_Nested_Forms extends GravityV
 	 * @since $ver$
 	 * @return bool
 	 */
-	private function add_gpnf_merge_tags($value) {
+	private function add_gpnf_merge_tags( $value ) {
 		add_filter( 'gform_replace_merge_tags', [
 			GPNF_Parent_Merge_Tag::get_instance(),
 			'parse_parent_merge_tag',
@@ -42,6 +48,26 @@ final class GravityView_Plugin_Hooks_Gravity_Perks_Nested_Forms extends GravityV
 		], 6 );
 
 		return $value;
+	}
+
+	/**
+	 * Adds the merge tags for
+	 *
+	 * @param array $tags The registered tags.
+	 *
+	 * @return array
+	 */
+	private function add_custom_gpnf_merge_tags( array $tags ): array {
+		return array_merge( $tags, [
+			[
+				'label' => esc_html__( 'Parent Entry ID', 'gp-nested-forms' ),
+				'tag'   => '{Parent:entry_id}',
+			],
+			[
+				'label' => esc_html__( 'Parent Entry Form ID', 'gp-nested-forms' ),
+				'tag'   => '{Parent:form_id}',
+			],
+		] );
 	}
 }
 
