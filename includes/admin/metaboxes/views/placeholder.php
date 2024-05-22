1c2cb27ba6b9efefffa58a3db697ff2b2fedd4e1
Doeke Norg <doekenorg@gmail.com>
2024-05-22 12:49:27 +0200

    Responsive inline placeholder.

--- includes/admin/metaboxes/views/placeholder.php
+++ includes/admin/metaboxes/views/placeholder.php
@@ -24,45 +24,46 @@ $user_can = current_user_can( $caps );
 	<?php //	<div class="gk-gravityview-placeholder-header"></div> ?>
 	<div class="gk-gravityview-placeholder-content">
 		<div class="gk-gravityview-placeholder-icon"><?php echo $icon; ?></div>
+		<div class="gk-gravityview-placeholder-body">
+			<div class="gk-gravityview-placeholder-summary">
+				<h3><?php echo esc_html( $title ); ?></h3>
+				<div class="howto">
+					<p><?php echo esc_html( $description ); ?></p>
+					<?php if ( $user_can ) {
+						printf(
+							'<p><a href="%s" rel="external noopener noreferrer" target="_blank">%s<span class="screen-reader-text">%s</span></span></a></p>',
+							esc_url( $buy_now_link ),
+							esc_html( sprintf( __( 'Learn more about %s…', 'gk-gravityview' ), $title ) ),
+							esc_html__( 'This link opens in a new window.', 'gk-gravityview' )
+						);
+					} ?>
+				</div>
+			</div>
+			<div class="gk-gravityview-placeholder-actions">
+				<?php
 
-		<div class="gk-gravityview-placeholder-summary">
-			<h3><?php echo esc_html( $title ); ?></h3>
-			<div class="howto">
-				<p><?php echo esc_html( $description ); ?></p>
-				<?php if ( $user_can ) {
+				// Only show the button if the user has ability to take action with the plugin.
+				if ( $user_can && 'read' !== $caps ) {
+					$attributes = array_map( static function ( string $key, string $value ): string {
+						return sprintf( '%s="%s"', esc_html( $key ), esc_attr( $value ) );
+					}, array_keys( $attributes ), array_values( $attributes ) );
+
 					printf(
-						'<p><a href="%s" rel="external noopener noreferrer" target="_blank">%s<span class="screen-reader-text">%s</span></span></a></p>',
+						'<a href="%1$s" %3$s class="gk-gravityview-placeholder-button button button-primary button-hero">%2$s</a>',
+						esc_url( $button_href ),
+						esc_html( $button_text ),
+						implode( ' ', $attributes ),
+				);
+				} else {
+					printf(
+						'<a href="%s" class="gk-gravityview-placeholder-button button button-primary button-hero" rel="external noopener noreferrer" target="_blank">%s<span class="screen-reader-text">%s</span></span></a>',
 						esc_url( $buy_now_link ),
-						esc_html( sprintf( __( 'Learn more about %s…', 'gk-gravityview' ), $title ) ),
+						esc_html( $button_text ),
 						esc_html__( 'This link opens in a new window.', 'gk-gravityview' )
 					);
-				} ?>
+				}
+				?>
 			</div>
-		</div>
-		<div class="gk-gravityview-placeholder-actions">
-			<?php
-
-			// Only show the button if the user has ability to take action with the plugin.
-			if ( $user_can && 'read' !== $caps ) {
-				$attributes = array_map( static function ( string $key, string $value ): string {
-					return sprintf( '%s="%s"', esc_html( $key ), esc_attr( $value ) );
-				}, array_keys( $attributes ), array_values( $attributes ) );
-
-				printf(
-					'<a href="%1$s" %3$s class="gk-gravityview-placeholder-button button button-primary button-hero">%2$s</a>',
-					esc_url( $button_href ),
-					esc_html( $button_text ),
-					implode( ' ', $attributes ),
-				);
-			} else {
-				printf(
-					'<a href="%s" class="gk-gravityview-placeholder-button button button-primary button-hero" rel="external noopener noreferrer" target="_blank">%s<span class="screen-reader-text">%s</span></span></a>',
-					esc_url( $buy_now_link ),
-					esc_html( $button_text ),
-					esc_html__( 'This link opens in a new window.', 'gk-gravityview' )
-				);
-			}
-			?>
 		</div>
 	</div>
 </div>
