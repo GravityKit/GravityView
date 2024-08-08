fd2129b350b03ddc6db2d9b55d7dc69b25a327e9
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-08-08 18:45:51 +0200

    Use single quotes

--- tests/E2E/setup/wp-env.config.js
+++ tests/E2E/setup/wp-env.config.js
@@ -1,30 +1,27 @@
 // This creates a .wp-env.json file using the .env file in the root of the project.
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-require("dotenv").config({ path: `${process.env.INIT_CWD}/.env` });
+require('dotenv').config({ path: `${process.env.INIT_CWD}/.env` });
 
 const wpEnvConfig = {
-	phpVersion: "7.4",
-	plugins: [
-		"../../..",
-		process.env.GRAVITYFORMS_FOLDER || "/tmp/gravityforms",
-	],
-	port: parseInt(process.env.WP_ENV_PORT, 10) || 8888,
-	config: {
-		WP_DEBUG: true,
-		WP_DEBUG_LOG: true,
-		WP_DEBUG_DISPLAY: true,
-		SAVEQUERIES: true,
-		SCRIPT_DEBUG: true,
-	},
+  phpVersion: '7.4',
+  plugins: ['../../..', process.env.GRAVITYFORMS_FOLDER || '/tmp/gravityforms'],
+  port: parseInt(process.env.WP_ENV_PORT, 10) || 8888,
+  config: {
+    WP_DEBUG: true,
+    WP_DEBUG_LOG: true,
+    WP_DEBUG_DISPLAY: true,
+    SAVEQUERIES: true,
+    SCRIPT_DEBUG: true,
+  },
 };
 
 fs.writeFileSync(
-	path.join(__dirname, ".wp-env.json"),
+  path.join(__dirname, '.wp-env.json'),
 
-	JSON.stringify(wpEnvConfig, null, 2),
+  JSON.stringify(wpEnvConfig, null, 2)
 );
 
-console.log("Generated .wp-env.json");
+console.log('Generated .wp-env.json');
