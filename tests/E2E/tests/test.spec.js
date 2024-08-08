112da3280e7b63eb5f8bfc276f2c32d4c0347a90
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-08-08 18:45:51 +0200

    Use single quotes

--- tests/E2E/tests/test.spec.js
+++ tests/E2E/tests/test.spec.js
@@ -1,31 +1,29 @@
 // @ts-check
-import { test, expect } from "@wordpress/e2e-test-utils-playwright";
-const path = require("path");
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+const path = require('path');
 
-require("dotenv").config({ path: `${process.env.INIT_CWD}/.env` });
+require('dotenv').config({ path: `${process.env.INIT_CWD}/.env` });
 
-test("GravityKit menu is available", async ({ admin, page, headless }) => {
-	await page.goto("/wp-admin");
+test('GravityKit menu is available', async ({ admin, page, headless }) => {
+  await page.goto('/wp-admin');
 
-	await page.fill("#user_login", process.env.WP_ENV_USER);
-	await page.fill("#user_pass", process.env.WP_ENV_USER_PASS);
+  await page.fill('#user_login', process.env.WP_ENV_USER);
+  await page.fill('#user_pass', process.env.WP_ENV_USER_PASS);
 
-	await page.click("#wp-submit");
+  await page.click('#wp-submit');
 
-	await page.waitForSelector("#toplevel_page__gk_admin_menu");
-	await page.hover("#toplevel_page__gk_admin_menu");
+  await page.waitForSelector('#toplevel_page__gk_admin_menu');
+  await page.hover('#toplevel_page__gk_admin_menu');
 
-	await page.waitForSelector("#toplevel_page__gk_admin_menu .wp-submenu");
+  await page.waitForSelector('#toplevel_page__gk_admin_menu .wp-submenu');
 
-	const submenus = await page
-		.locator("#toplevel_page__gk_admin_menu .wp-submenu a")
-		.allTextContents();
+  const submenus = await page
+    .locator('#toplevel_page__gk_admin_menu .wp-submenu a')
+    .allTextContents();
 
-	const submenuTitles = submenus.map((item) =>
-		item.replace(/\d+$/, "").trim(),
-	); // Remove the trailing number as it's a hidden <span class="plugin-count">0</span> element.
+  const submenuTitles = submenus.map((item) => item.replace(/\d+$/, '').trim()); // Remove the trailing number as it's a hidden <span class="plugin-count">0</span> element.
 
-	expect(submenuTitles).toContain("Manage Your Kit");
-	expect(submenuTitles).toContain("Settings");
-	expect(submenuTitles).toContain("Grant Support Access");
+  expect(submenuTitles).toContain('Manage Your Kit');
+  expect(submenuTitles).toContain('Settings');
+  expect(submenuTitles).toContain('Grant Support Access');
 });
