558665ad9a041b242716362eded3cadf6aaa4b49
omarkasem <omar.kasem207@gmail.com>
2024-11-12 17:08:31 +0200

    Adds initials modifier, Fixes array raw value for merge tags and Makes convert to initials function to use in multiple places

--- includes/fields/class-gravityview-field-name.php
+++ includes/fields/class-gravityview-field-name.php
@@ -48,6 +48,30 @@ class GravityView_Field_Name extends GravityView_Field {
 		return $field_options;
 	}
 
+	/**
+	 * Convert a full name or string to initials.
+	 *
+	 * @since TBD
+	 *
+	 * @param string $value The full name or string to convert.
+	 * 
+	 * @return string The initials.
+	 */
+	public static function convert_to_initials( $value ) {
+		$names    = explode( ' ', $value );
+
+		$display_value = '';
+	
+		foreach ( $names as $name ) {
+			$first_char = function_exists( 'mb_substr' ) ? mb_substr( $name, 0, 1 ) : substr( $name, 0, 1 );
+			$upper_char = function_exists( 'mb_strtoupper' ) ? mb_strtoupper( $first_char ) : strtoupper( $first_char );
+	
+			$display_value .= trim( $upper_char ) . '.';
+		}
+
+		return $display_value;
+	}
+
 
 }
 
