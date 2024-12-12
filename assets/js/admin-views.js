609641b00a33dcff2d502b45069bbe8f97938ca4
Serhiy Zakharchenko <zaharchenko.dev@gmail.com>
2024-12-12 10:30:19 -0800

    Fix premature calculation of zebra stripes #2224

--- assets/js/admin-views.js
+++ assets/js/admin-views.js
@@ -374,14 +374,16 @@
 		* This prevents two gray rows next to each other.
 		* @since 1.19
 		*/
-	   zebraStripeSettings: function() {
-		   viewGeneralSettings.metaboxObj.find('table').each( function () {
-			   var $trs = $( this ).find('tr').filter(':visible');
+	   zebraStripeSettings: function () {
+		   setTimeout( function () {
+			   viewGeneralSettings.metaboxObj.find( 'table' ).each( function () {
+				   var $trs = $( this ).find( 'tr' ).filter( ':visible' );
 
-			   $trs.removeClass('alternate');
+				   $trs.removeClass( 'alternate' );
 
-			   $trs.filter( ':even' ).addClass( 'alternate' );
-		   });
+				   $trs.filter( ':even' ).addClass( 'alternate' );
+			   } );
+		   }, 50 );
 	   },
 
 	   /**
