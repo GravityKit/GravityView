86a62068865c0cbbf08a754fe78cd1556c303931
Mwale Kalenga <mwale.kalenga@prosites.space>
2025-02-07 17:23:54 +0200

    Include inner catch block in login script

--- tests/E2E/helpers/wp-login.js
+++ tests/E2E/helpers/wp-login.js
@@ -1,45 +1,60 @@
-const path = require( 'path' );
-const fs = require( 'fs' ).promises;
+const path = require("path");
+const fs = require("fs").promises;
 
-require( 'dotenv' ).config( { path: `${ process.env.INIT_CWD }/.env` } );
+require("dotenv").config({ path: `${process.env.INIT_CWD}/.env` });
 
-async function wpLogin( {
+async function wpLogin({
 	page,
 	stateFile,
 	username = process.env.WP_ENV_USER,
 	password = process.env.WP_ENV_USER_PASS,
-} = {} ) {
+} = {}) {
 	const baseUrl =
-		`${ process.env.WP_ENV_URL }:${ process.env.WP_ENV_PORT }` ||
-		'http://localhost';
+		`${process.env.WP_ENV_URL}:${process.env.WP_ENV_PORT}` ||
+		"http://localhost";
 
 	try {
-		await fs.access( stateFile );
+		await fs.access(stateFile);
 
-		const { cookies } = JSON.parse( await fs.readFile( stateFile, 'utf-8' ) );
+		const { cookies } = JSON.parse(await fs.readFile(stateFile, "utf-8"));
 
-		console.log( 'Loading previously saved state…' );
+		console.log("Loading previously saved state…");
 
-		await page.context().addCookies( cookies );
-	} catch ( error ) {
-		console.log( 'Logging in and saving state…' );
+		await page.context().addCookies(cookies);
+	} catch (error) {
+		console.error("An error occurred during login:", error);
 
-		const loginPage = 'wp-login.php';
+		console.log("Logging in and saving state…");
 
-		await page.goto( `${ baseUrl }/${ loginPage }` );
+		try {
+			const loginPage = "wp-login.php";
+			console.log(`Navigating to: ${baseUrl}/${loginPage}`);
 
-		await page.fill( '#user_login', username );
-		await page.fill( '#user_pass', password );
+			await page.goto(`${baseUrl}/${loginPage}`);
 
-		await page.click( '#wp-submit' );
+			console.log("Filling in username and password fields");
+			await page.fill("#user_login", username);
+			await page.fill("#user_pass", password);
 
-		await page.waitForNavigation({ waitUntil: 'networkidle', timeout: 90000 });
+			console.log("Clicking submit button");
+			await page.click("#wp-submit");
 
-		if ( page.url().includes( loginPage ) ) {
-			throw new Error( 'WordPress login failed' );
+			console.log("Waiting for navigation to complete");
+			await page.waitForNavigation({ waitUntil: "networkidle" });
+
+			console.log(`Current URL after login: ${page.url()}`);
+			if (page.url().includes(loginPage)) {
+				throw new Error("WordPress login failed");
+			}
+
+			console.log(`Saving state to: ${stateFile}`);
+			await page.context().storageState({ path: stateFile });
+		} catch (innerError) {
+			console.error(
+				"An error occurred inside the catch block:",
+				innerError,
+			);
 		}
-
-		await page.context().storageState( { path: stateFile } );
 	}
 }
 
