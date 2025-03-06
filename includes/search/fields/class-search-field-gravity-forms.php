3b0fac191f21ac3dfd21522054916d91e283a023
Doeke Norg <doekenorg@gmail.com>
2025-03-06 14:04:38 +0100

    Add choices for choice fields

--- includes/search/fields/class-search-field-gravity-forms.php
+++ includes/search/fields/class-search-field-gravity-forms.php
@@ -2,7 +2,9 @@
 
 namespace GV\Search\Fields;
 
+use GF_Field;
 use GF_Fields;
+use GFAPI;
 
 /**
  * Represents a search field based on a Gravity Forms Field.
@@ -11,7 +13,7 @@ use GF_Fields;
  *
  * @extends Search_Field<string>
  */
-final class Search_Field_Gravity_Forms extends Search_Field {
+final class Search_Field_Gravity_Forms extends Search_Field_Choices {
 	/**
 	 * @inheritDoc
 	 * @since $ver$
@@ -32,6 +34,15 @@ final class Search_Field_Gravity_Forms extends Search_Field {
 	 * @var array
 	 */
 	public array $form_field = [];
+
+	/**
+	 * Microcache of the connected field instance.
+	 *
+	 * @since $ver$
+	 *
+	 * @var GF_Field|null
+	 */
+	private ?GF_Field $field = null;
 
 	/**
 	 * Creates an instance based on a field object.
@@ -104,6 +115,62 @@ final class Search_Field_Gravity_Forms extends Search_Field {
 	 * @since $ver$
 	 */
 	protected function get_input_name(): string {
-		return sprintf( 'filter_%s', $this->form_field['id'] );
+		$field_id = $this->get_field() ? $this->get_field()->id : 0;
+		return sprintf( 'filter_%s', $field_id );
+	}
+
+	/**
+	 * @inheritDoc
+	 * @since $ver$
+	 */
+	protected function has_choices(): bool {
+		$field = $this->get_field();
+		if ( ! $field ) {
+			return false;
+		}
+
+		return count( $field->choices ?? [] ) > 0;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @since $ver$
+	 */
+	protected function get_field_type(): string {
+		return $this->has_choices() ? 'select' : parent::get_field_type();
+	}
+
+	/**
+	 * @inheritDoc@
+	 * @since     $ver$
+	 */
+	public function get_choices(): array {
+		$field = $this->get_field();
+		if ( ! $field ) {
+			return [];
+		}
+
+		return $field->choices ?? [];
+	}
+
+	/**
+	 * Retrieve the Gravity Forms field connected to this search field.
+	 *
+	 * @since $ver$
+	 *
+	 * @return GF_Field|null The Gravity Forms field.
+	 */
+	private function get_field(): ?GF_Field {
+		if ( $this->field ) {
+			return $this->field;
+		}
+		$parts = explode( '::', (string) ( $this->id ?? '' ) );
+		if ( count( $parts ) !== 3 ) {
+			return null;
+		}
+
+		$field = GFAPI::get_field( $parts[1], $parts[2] );
+
+		return $this->field = ( $field ?: null );
 	}
 }
