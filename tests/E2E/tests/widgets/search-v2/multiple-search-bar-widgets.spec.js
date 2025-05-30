56c98ff1a62988bc2ade633e76db956f6f386810
Mwale Kalenga <mwale.kalenga@prosites.space>
2025-05-30 13:16:30 +0200

    Add multiple search bar widget test

---
+++ tests/E2E/tests/widgets/search-v2/multiple-search-bar-widgets.spec.js
@@ -0,0 +1,63 @@
+const { test, expect } = require('@playwright/test');
+const {
+	templates,
+	createView,
+	publishView,
+	checkViewOnFrontEnd,
+	clickFirstVisible,
+  viewTemplatesMap
+} = require('../../../helpers/test-helpers');
+
+// This test creates a View with multiple search bar widgets and tests searching for entries.
+test('Multiple Search Bar Widgets', async ({ page }) => {
+	await page.goto('/wp-admin/edit.php?post_type=gravityview');
+
+	await createView(page, {
+		formTitle: 'A Simple Form',
+		viewName: 'Search Test with Multiple Search Bar Widgets',
+		template: viewTemplatesMap.table
+	});
+
+	await page
+		.locator('#directory-header-widgets')
+		.getByRole('link', { name: /Add Widget/ })
+		.first()
+		.click();
+	await page.locator('#ui-id-15').getByText('Add "Search Bar" Search').click();
+
+	await page
+		.getByRole('heading', { name: /Configure Search Bar Settings/ })
+		.getByLabel('Configure Search Bar Settings')
+		.nth(1)
+		.click();
+	await page
+		.getByLabel('Search Bar Settings', { exact: true })
+		.locator('#search-search-general-fields')
+		.getByRole('link', { name: /Add Search Field/ })
+		.click();
+	await page
+		.locator('#ui-id-19')
+		.getByText('Add "Search Everything" Search EverythingSearch across all entry fields')
+		.click();
+	await clickFirstVisible(page, page.getByRole('button', { name: /Close/ }));
+
+	await publishView(page);
+
+	await checkViewOnFrontEnd(page);
+
+	const form1 = page.locator('form.gv-widget-search').nth(0);
+	const form2 = page.locator('form.gv-widget-search').nth(1);
+
+	const searchInput1 = form1.locator('input[name="gv_search"]');
+	const searchInput2 = form2.locator('input[name="gv_search"]');
+
+	await searchInput1.click();
+	await searchInput1.fill('Bob');
+	await form1.getByRole('button', { name: /Search/ }).click();
+	await expect(page.getByRole('link', { name: 'Bob' })).toBeVisible();
+
+	await expect(searchInput2).toBeVisible();
+	await searchInput2.fill('Charlie');
+	await form2.getByRole('button', { name: /Search/ }).click();
+	await expect(page.getByRole('link', { name: 'Charlie' })).toBeVisible();
+});
