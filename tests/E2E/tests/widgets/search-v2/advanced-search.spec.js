2d41e7b07be8b6108a9179e933db62b1084e10d6
Mwale Kalenga <mwale.kalenga@prosites.space>
2025-06-02 13:19:35 +0200

    Format tests

--- tests/E2E/tests/widgets/search-v2/advanced-search.spec.js
+++ tests/E2E/tests/widgets/search-v2/advanced-search.spec.js
@@ -1,30 +1,42 @@
 const { test, expect } = require('@playwright/test');
-const { createView, publishView, checkViewOnFrontEnd, viewTemplatesMap, clickFirstVisible } = require('../../../helpers/test-helpers');
+const {
+	createView,
+	publishView,
+	checkViewOnFrontEnd,
+	viewTemplatesMap,
+	clickFirstVisible
+} = require('../../../helpers/test-helpers');
 
 /*
-* Verifies that the advanced search panel works as expected. Particularly, after performing 
-* a search with regular search fields first.
-*/
+ * Verifies that the advanced search panel works as expected. Particularly, after performing
+ * a search with regular search fields first.
+ */
 
 test('advanced search panel remains open after search', async ({ page }) => {
-  await page.goto('/wp-admin/edit.php?post_type=gravityview');
-  await createView(page, {
-    formTitle: 'Training Feedback',
-    viewName: 'Verify Adv Search Remains Open',
-    template: viewTemplatesMap.table,
-  });
-  await page.getByRole('button', { name: 'Configure Search Bar Settings' }).click();
-  await page.locator('#search-search-advanced-fields').getByRole('link', { name: ' Add Search Field' }).click();
-  await clickFirstVisible(page, page.getByTitle('Search Field: Is Starred\nFilter on starred entries'));
-  await page.getByRole('button', { name: ' Close' }).click();
-  await publishView(page);
-  await checkViewOnFrontEnd(page);
-  
-// TO DO: Add a regular search field and perform a search with it first.
+	await page.goto('/wp-admin/edit.php?post_type=gravityview');
+	await createView(page, {
+		formTitle: 'Training Feedback',
+		viewName: 'Verify Adv Search Remains Open',
+		template: viewTemplatesMap.table
+	});
+	await page.getByRole('button', { name: 'Configure Search Bar Settings' }).click();
+	await page
+		.locator('#search-search-advanced-fields')
+		.getByRole('link', { name: ' Add Search Field' })
+		.click();
+	await clickFirstVisible(
+		page,
+		page.getByTitle('Search Field: Is Starred\nFilter on starred entries')
+	);
+	await page.getByRole('button', { name: ' Close' }).click();
+	await publishView(page);
+	await checkViewOnFrontEnd(page);
 
-  await page.getByLabel('Toggle Advanced Search').click();
-  await page.getByLabel('Is Starred').check();
-  await page.getByRole('button', { name: 'Search', exact: true }).click();
+	// TO DO: Add a regular search field and perform a search with it first.
 
-  await expect(page.getByLabel('Is Starred')).toBeVisible();
+	await page.getByLabel('Toggle Advanced Search').click();
+	await page.getByLabel('Is Starred').check();
+	await page.getByRole('button', { name: 'Search', exact: true }).click();
+
+	await expect(page.getByLabel('Is Starred')).toBeVisible();
 });
