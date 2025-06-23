96b8b32b66aeb35a227c9e0375b3786045d455ad
Doeke Norg <doekenorg@gmail.com>
2025-06-23 11:38:08 +0200

    Fix DT E2E test

--- tests/E2E/tests/widgets/search-v2/showExistingChoicesDT.spec.js
+++ tests/E2E/tests/widgets/search-v2/showExistingChoicesDT.spec.js
@@ -70,7 +70,7 @@ test('Search Bar Only Shows Choices That Exist in Submitted Entries', async ({ p
 			.getByLabel('Configure Settings')
 			.click();
 		await clickFirstVisible(page, page.getByText('Only show choices that exist'));
-		await page.getByRole('button', { name: 'Close' }).click();
+		await page.locator('button[data-close-settings]').click();
 
 		await page
 			.getByLabel('Search Bar Settings', { exact: true })
