94d770128c7e97cdb6541dffddf57e64c7f551fc
Zack Katz <zack@katz.co>
2025-08-11 17:22:29 -0400

    Fix template code being verbose

--- includes/widgets/search-widget/templates/search-field-date_range.php
+++ includes/widgets/search-widget/templates/search-field-date_range.php
@@ -18,20 +18,16 @@ $custom_class     = \GV\Utils::get( $search_field, 'custom_class', '' );
 $start_value = $value['start'] ?? '';
 $end_value   = $value['end'] ?? '';
 
-// Build input attributes.
-$start_input_name = esc_attr( $name ) . '[start]';
-$start_input_id   = 'search-box-' . esc_attr( $name ) . '-start';
-$end_input_name   = esc_attr( $name ) . '[end]';
-$end_input_id     = 'search-box-' . esc_attr( $name ) . '-end';
+$custom_class     = gravityview_sanitize_html_class( $custom_class );
 $input_class      = gravityview_sanitize_html_class( $datepicker_class );
 ?>
 
 <div class="gv-search-box gv-search-date gv-search-date-range <?php echo esc_attr( $custom_class ); ?>">
 	<?php if ( ! gv_empty( $label, false, false ) ) { ?>
-		<label for="<?php echo $start_input_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already escaped on line 23. ?>"><?php echo esc_html( $label ); ?></label>
+		<label for="<?php echo esc_attr( 'search-box-' . $name . '-start' ); ?>"><?php echo esc_html( $label ); ?></label>
 	<?php } ?>
 	<p>
-		<input name="<?php echo $start_input_name; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already escaped. ?>" id="<?php echo $start_input_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already escaped. ?>" type="text" class="<?php echo $input_class; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already sanitized. ?>" placeholder="<?php esc_attr_e( 'Start date', 'gk-gravityview' ); ?>" value="<?php echo esc_attr( $start_value ); ?>">
-		<input name="<?php echo $end_input_name; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already escaped. ?>" id="<?php echo $end_input_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already escaped. ?>" type="text" class="<?php echo $input_class; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Already sanitized. ?>" placeholder="<?php esc_attr_e( 'End date', 'gk-gravityview' ); ?>" value="<?php echo esc_attr( $end_value ); ?>">
+		<input name="<?php echo esc_attr( $name . '[start]' ); ?>" id="<?php echo esc_attr( 'search-box-' . $name . '-start' ); ?>" type="text" class="<?php echo esc_attr( $input_class ); ?>" placeholder="<?php esc_attr_e( 'Start date', 'gk-gravityview' ); ?>" value="<?php echo esc_attr( $start_value ); ?>">
+		<input name="<?php echo esc_attr( $name . '[end]' ); ?>" id="<?php echo esc_attr( 'search-box-' . $name . '-end' ); ?>" type="text" class="<?php echo esc_attr( $input_class ); ?>" placeholder="<?php esc_attr_e( 'End date', 'gk-gravityview' ); ?>" value="<?php echo esc_attr( $end_value ); ?>">
 	</p>
 </div>
