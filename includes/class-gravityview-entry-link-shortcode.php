fe028220c63f670e6afd546ad3b6c5a4b90c7807
Vlad <site.github@mrcasual.com>
2025-08-28 12:50:23 -0400

    Ensure backward compatibility for the old `gv_entry_link` shortcode

--- includes/class-gravityview-entry-link-shortcode.php
+++ includes/class-gravityview-entry-link-shortcode.php
@@ -1,39 +1,48 @@
 <?php
+
+use GV\Shortcodes\gv_entry_link;
+
 /**
  * Shortcode to handle showing/hiding content in merge tags. Works great with GravityView Custom Content fields
  *
  * @deprecated 2.45
- * @see \GV\Shortcodes\gv_entry_link
+ * @see        gv_entry_link
  */
 class GravityView_Entry_Link_Shortcode {
 
 	/**
 	 * @deprecated 2.45
-	 * @see \GV\Shortcodes\gv_entry_link
+	 * @see        gv_entry_link
 	 */
-	public function read_shortcode( $atts, $content = '', $tag = '' ) {
+	public function read_shortcode( $atts, $content = '', $tag = 'gv_entry_link' ) {
 		_deprecated_function( __FUNCTION__, '2.45', '\GV\Shortcodes\gv_entry_link' );
-		$shortcode = new \GV\Shortcodes\gv_entry_link();
+
+		$shortcode = new gv_entry_link();
+
 		return $shortcode->callback( $atts, $content, $tag );
 	}
 
 	/**
 	 * @deprecated 2.45
-	 * @see \GV\Shortcodes\gv_entry_link
+	 * @see        gv_entry_link
 	 */
-	public function edit_shortcode( $atts, $content = '', $tag = '' ) {
+	public function edit_shortcode( $atts, $content = '', $tag = 'gv_edit_entry_link' ) {
 		_deprecated_function( __FUNCTION__, '2.45', '\GV\Shortcodes\gv_entry_link' );
-		$shortcode = new \GV\Shortcodes\gv_entry_link();
+
+		$shortcode = new gv_entry_link();
+
 		return $shortcode->callback( $atts, $content, $tag );
 	}
 
 	/**
 	 * @deprecated 2.45
-	 * @see \GV\Shortcodes\gv_entry_link
+	 * @see        gv_entry_link
 	 */
-	public function delete_shortcode( $atts, $content = '', $tag = '' ) {
+	public function delete_shortcode( $atts, $content = '', $tag = 'gv_delete_entry_link' ) {
 		_deprecated_function( __FUNCTION__, '2.45', '\GV\Shortcodes\gv_entry_link' );
-		$shortcode = new \GV\Shortcodes\gv_entry_link();
-		return $shortcode->callback( $atts, $content, $tag );
+
+		$shortcode = new gv_entry_link();
+
+		return $shortcode->callback( $atts, $content );
 	}
 }
