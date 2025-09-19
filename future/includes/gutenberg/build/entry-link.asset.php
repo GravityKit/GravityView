7e635fbe0ebf02fe04600b76d1e40940be4a464c
Vlad <site.github@mrcasual.com>
2025-09-18 21:34:12 -0400

    Update Foundation to v1.4.0
    Ref: https://github.com/GravityKit/Foundation/releases/tag/v1.4.0

--- future/includes/gutenberg/build/entry-link.asset.php
+++ future/includes/gutenberg/build/entry-link.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a22025b496600375e0d8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1911d07ed02458c6539a');
