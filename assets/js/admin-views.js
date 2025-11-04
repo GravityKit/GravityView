19ebe895dbf32578bd597e6a929e8dcf451ff2b6
Zack Katz <zack@katz.co>
2025-11-04 14:04:46 -0500

    Increases toast notification duration from 3s to 7.5s for better user visibility.

--- assets/js/admin-views.js
+++ assets/js/admin-views.js
@@ -3431,7 +3431,7 @@
 		   show: function( message, type, duration ) {
 			   this.init();
 
-			   duration = duration || 3000;
+			   duration = duration || 7500;
 
 			   var iconMap = {
 				   success: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>',
