e3cd9ba870e9e78da4eeac80b61116da48aa3623
Zack Katz <zack@katz.co>
2025-12-18 18:47:26 -0500

    Fix hook documentation in class-gv-template-view-layout-builder.php
    - Add @since 2.46.2 to both hooks
    - Fix @deprecated message to reference correct filter
    - Remove non-standard @return tags from filters
    - Add periods to descriptions
    - Align @param types
    - Fix variable name from $this to $view

--- future/includes/class-gv-template-view-layout-builder.php
+++ future/includes/class-gv-template-view-layout-builder.php
@@ -43,20 +43,23 @@ final class View_Layout_Builder_Template extends View_Template {
         /**
          * Modify the class applied to the entry row.
          *
-         * @param string $class Existing class.
-         * @param array $entry Current entry being displayed
-         * @param \GravityView_View $this Current GravityView_View object
-         * @deprecated Use `gravityview/template/table/entry/class`
-         * @return string The modified class.
+         * @since 2.46.2
+         *
+         * @deprecated Use `gravityview/template/layout-builder/entry/class` instead.
+         *
+         * @param string           $class Existing class.
+         * @param array            $entry Current entry being displayed.
+         * @param \GravityView_View $view  Current GravityView_View object.
          */
         $class = apply_filters( 'gravityview_entry_class', $class, $entry->as_entry(), \GravityView_View::getInstance() );
 
         /**
          * Modify the class applied to the entry row.
          *
-         * @param string $class The existing class.
+         * @since 2.46.2
+         *
+         * @param string               $class   The existing class.
          * @param \GV\Template_Context $context The context.
-         * @return string The modified class.
          */
         return apply_filters( 'gravityview/template/layout-builder/entry/class', $class, Template_Context::from_template( $context->template, compact( 'entry' ) ) );
     }
