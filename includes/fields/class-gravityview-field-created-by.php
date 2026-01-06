55e82fb7838491b2be4d8536c94b3f30e1fa2469
omarkasem <omar.kasem207@gmail.com>
2026-01-06 10:47:04 +0200

    Refactor inline styles for selectWoo in GravityView_Field_Created_By
    - Replaced inline styles in the `get_field_input` method with `wp_add_inline_style` for better performance and maintainability.
    - This change enhances the loading of selectWoo assets on the frontend, improving the user interface consistency during entry editing.

--- includes/fields/class-gravityview-field-created-by.php
+++ includes/fields/class-gravityview-field-created-by.php
@@ -168,21 +168,18 @@ class GravityView_Field_Created_By extends GravityView_Field {
 	 */
 	public function get_field_input( array $form, $value, array $entry, GF_Field $field ): string {
 		GravityView_Change_Entry_Creator::enqueue_selectwoo_assets_frontend();
-		$inline_styles = '
-			<style>
-				.ginput_container .select2-container .select2-selection--single,
-				.ginput_container .select2-container--default .select2-selection--single .select2-selection__arrow {
-					height: 40px;
-				}
-				.ginput_container .select2-container--default .select2-selection--single .select2-selection__rendered {
-					line-height: 40px;
-				}
-			</style>
-		';
+		wp_add_inline_style( 'gravityview_selectwoo', '
+		.ginput_container .select2-container .select2-selection--single,
+		.ginput_container .select2-container--default .select2-selection--single .select2-selection__arrow {
+			height: 40px;
+		}
+		.ginput_container .select2-container--default .select2-selection--single .select2-selection__rendered {
+			line-height: 40px;
+		}
+		');
 
 		return sprintf(
-			'<div class="ginput_container">%s%s</div>',
-			$inline_styles,
+			'<div class="ginput_container">%s</div>',
 			GravityView_Change_Entry_Creator::get_select_field( $entry )
 		);
 	}
