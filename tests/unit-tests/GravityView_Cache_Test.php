88c8f0aa591e59b5497ae3d5a975d3b5e001c7bf
Zack Katz <zack@katz.co>
2026-01-14 20:13:53 -0500

    Fix cache invalidation after Edit Entry

--- tests/unit-tests/GravityView_Cache_Test.php
+++ tests/unit-tests/GravityView_Cache_Test.php
@@ -49,4 +49,40 @@ class GravityView_Cache_Test extends GV_UnitTestCase {
 
 		$this->assertNull( $cache->get(), 'Cache was not deleted by invalidation (transients object cache)' );
 	}
+
+	public function test_edit_entry_after_update_adds_form_to_blocklist() {
+		$form  = $this->factory->form->create_and_get();
+		$entry = $this->factory->entry->create_and_get(
+			array(
+				'form_id' => $form['id'],
+			)
+		);
+
+		$cache = new GravityView_Cache();
+		$cache->blocklist_remove( $form['id'] );
+
+		$this->assertFalse( $cache->in_blocklist( $form['id'] ) );
+
+		$hook_name = 'gravityview/edit_entry/after_update';
+
+		global $wp_filter;
+		$original_hook = $wp_filter[ $hook_name ] ?? null;
+
+		remove_all_actions( $hook_name );
+		add_action( $hook_name, array( $cache, 'entry_updated' ), 10, 2 );
+
+		try {
+			do_action( $hook_name, $form, $entry['id'], null, null );
+		} finally {
+			if ( null === $original_hook ) {
+				unset( $wp_filter[ $hook_name ] );
+			} else {
+				$wp_filter[ $hook_name ] = $original_hook;
+			}
+		}
+
+		$this->assertTrue( $cache->in_blocklist( $form['id'] ) );
+
+		$cache->blocklist_remove( $form['id'] );
+	}
 }
